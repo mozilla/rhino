24f7d07adde4d12c457ac03544a3e98e7220a1d9
Attila Szegedi <szegedia@gmail.com>
2006-11-06 10:20:14 +0000

    Relicensing Rhino under MPL, see:http://groups.google.com/group/mozilla.dev.tech.js-engine/browse_thread/thread/12b1279e97d1f8a

--- xmlimplsrc/org/mozilla/javascript/xmlimpl/XML.java
+++ xmlimplsrc/org/mozilla/javascript/xmlimpl/XML.java
@@ -1,39 +1,44 @@
 /* -*- Mode: java; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
  *
- * The contents of this file are subject to the Netscape Public
- * License Version 1.1 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of
- * the License at http://www.mozilla.org/NPL/
+ * ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
  *
- * Software distributed under the License is distributed on an "AS
- * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
- * implied. See the License for the specific language governing
- * rights and limitations under the License.
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
  * The Original Code is Rhino code, released
  * May 6, 1999.
  *
- * The Initial Developer of the Original Code is Netscape
- * Communications Corporation.  Portions created by Netscape are
- * Copyright (C) 1997-2000 Netscape Communications Corporation. All
- * Rights Reserved.
+ * The Initial Developer of the Original Code is
+ * Netscape Communications Corporation.
+ * Portions created by the Initial Developer are Copyright (C) 1997-2000
+ * the Initial Developer. All Rights Reserved.
  *
  * Contributor(s):
- * Ethan Hugg
- * Terry Lucas
- * Milen Nankov
+ *   Ethan Hugg
+ *   Terry Lucas
+ *   Milen Nankov
  *
- * Alternatively, the contents of this file may be used under the
- * terms of the GNU Public License (the "GPL"), in which case the
- * provisions of the GPL are applicable instead of those above.
- * If you wish to allow use of your version of this file only
- * under the terms of the GPL and not to allow others to use your
- * version of this file under the NPL, indicate your decision by
- * deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL.  If you do not delete
- * the provisions above, a recipient may use your version of this
- * file under either the NPL or the GPL.
- */
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
 
 package org.mozilla.javascript.xmlimpl;
 
@@ -988,7 +993,7 @@ todo need to handle namespace prefix not found in XML look for namespace type in
             {
                 if (xmlValue instanceof XMLList)
                 {
-                    xml = ((XMLList) xmlValue).item(i);
+                    xml = (XML)((XMLList) xmlValue).item(i);
                 }
                 else
                 {
@@ -1052,7 +1057,7 @@ todo need to handle namespace prefix not found in XML look for namespace type in
             throw ScriptRuntime.typeError(e.getMessage());
         }
 
-        result = createTextElement(lib, qname, value);
+        result = createTextElement(lib, qname, value.toString());
 
         return result;
     }
@@ -1306,7 +1311,7 @@ todo need to handle namespace prefix not found in XML look for namespace type in
                     {
                         for (int i = 0; i < xmlValue.length(); i++)
                         {
-                            XML xml = ((XMLList) xmlValue).item(i);
+                            XML xml = (XML)((XMLList) xmlValue).item(i);
 
                             if (xml.tokenType() == XmlCursor.TokenType.ATTR)
                             {
@@ -1320,7 +1325,7 @@ todo need to handle namespace prefix not found in XML look for namespace type in
                     xmlValue = makeXmlFromString(lib, xmlName, ScriptRuntime.toString(value));
                 }
 
-                XMLList matches = getPropertyList(xmlName);
+                XMLList matches = (XMLList)getPropertyList(xmlName);
 
                 if (matches.length() == 0)
                 {
@@ -1335,7 +1340,7 @@ todo need to handle namespace prefix not found in XML look for namespace type in
                     }
 
                     // Replace first match with new value.
-                    doPut(xmlName, matches.item(0), xmlValue);
+                    doPut(xmlName, (XML)matches.item(0), xmlValue);
                 }
             }
         }
@@ -1389,7 +1394,7 @@ todo need to handle namespace prefix not found in XML look for namespace type in
         }
         else
         {
-            XMLList matches = getPropertyList(name);
+            XMLList matches = (XMLList)getPropertyList(name);
 
             matches.remove();
         }
@@ -2715,11 +2720,11 @@ todo need to handle namespace prefix not found in XML look for namespace type in
 
     XML replace(long index, Object xml)
     {
-        XMLList xlChildToReplace = child(index);
+        XMLList xlChildToReplace = (XMLList)child(index);
         if (xlChildToReplace.length() > 0)
         {
             // One exists an that index
-            XML childToReplace = xlChildToReplace.item(0);
+            XML childToReplace = (XML)xlChildToReplace.item(0);
             insertChildAfter(childToReplace, xml);
             removeChild(index);
         }
@@ -2746,7 +2751,7 @@ todo need to handle namespace prefix not found in XML look for namespace type in
     {
         // remove all children
         XMLName xmlName = XMLName.formStar();
-        XMLList matches = getPropertyList(xmlName);
+        XMLList matches = (XMLList)getPropertyList(xmlName);
         matches.remove();
 
         // append new children
--- xmlimplsrc/org/mozilla/javascript/xmlimpl/XMLList.java
+++ xmlimplsrc/org/mozilla/javascript/xmlimpl/XMLList.java
@@ -1,39 +1,44 @@
 /* -*- Mode: java; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
  *
- * The contents of this file are subject to the Netscape Public
- * License Version 1.1 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of
- * the License at http://www.mozilla.org/NPL/
+ * ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
  *
- * Software distributed under the License is distributed on an "AS
- * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
- * implied. See the License for the specific language governing
- * rights and limitations under the License.
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
  * The Original Code is Rhino code, released
  * May 6, 1999.
  *
- * The Initial Developer of the Original Code is Netscape
- * Communications Corporation.  Portions created by Netscape are
- * Copyright (C) 1997-2000 Netscape Communications Corporation. All
- * Rights Reserved.
+ * The Initial Developer of the Original Code is
+ * Netscape Communications Corporation.
+ * Portions created by the Initial Developer are Copyright (C) 1997-2000
+ * the Initial Developer. All Rights Reserved.
  *
  * Contributor(s):
- * Ethan Hugg
- * Terry Lucas
- * Milen Nankov
+ *   Ethan Hugg
+ *   Terry Lucas
+ *   Milen Nankov
  *
- * Alternatively, the contents of this file may be used under the
- * terms of the GNU Public License (the "GPL"), in which case the
- * provisions of the GPL are applicable instead of those above.
- * If you wish to allow use of your version of this file only
- * under the terms of the GPL and not to allow others to use your
- * version of this file under the NPL, indicate your decision by
- * deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL.  If you do not delete
- * the provisions above, a recipient may use your version of this
- * file under either the NPL or the GPL.
- */
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
 
 package org.mozilla.javascript.xmlimpl;
 
@@ -156,7 +161,7 @@ class XMLList extends XMLObjectImpl implements Function
             XML orgXML = XML.createFromJS(lib, frag);
 
             // Now orphan the children and add them to our XMLList.
-            XMLList children = orgXML.children();
+            XMLList children = (XMLList)orgXML.children();
 
             _annos = new AnnotationList();
 
@@ -391,11 +396,11 @@ class XMLList extends XMLObjectImpl implements Function
                 }
                 else
                 {
-                    XML xml = item(0);
+                    XML xml = (XML)item(0);
                     xml.putXMLProperty(xmlName, value);
 
                     // Update the list with the new item at location 0.
-                    replace(0, item(0));
+                    replace(0, (XML)item(0));
                 }
 
                 // Now add us to our parent
@@ -413,11 +418,11 @@ class XMLList extends XMLObjectImpl implements Function
         }
         else
         {
-            XML xml = item(0);
+            XML xml = (XML)item(0);
             xml.putXMLProperty(xmlName, value);
 
             // Update the list with the new item at location 0.
-            replace(0, item(0));
+            replace(0, (XML)item(0));
         }
     }
 
@@ -502,14 +507,14 @@ class XMLList extends XMLObjectImpl implements Function
                     if (list.length() > 0)
                     {
                         int lastIndexAdded = xmlNode.childIndex();
-                        xmlNode.replaceAll(list.item(0));
-                        replace(index, list.item(0));
+                        xmlNode.replaceAll((XML)list.item(0));
+                        replace(index, (XML)list.item(0));
 
                         for (int i = 1; i < list.length(); i++)
                         {
                             xmlParent.insertChildAfter(xmlParent.getXmlChild(lastIndexAdded), list.item(i));
                             lastIndexAdded++;
-                            insert(index + i, list.item(i));
+                            insert(index + i, (XML)list.item(i));
                         }
                     }
                 }
@@ -540,12 +545,12 @@ class XMLList extends XMLObjectImpl implements Function
 
                     if (list.length() > 0)
                     {
-                        xmlNode.replaceAll(list.item(0));
-                        replace(index, list.item(0));
+                        xmlNode.replaceAll((XML)list.item(0));
+                        replace(index, (XML)list.item(0));
 
                         for (int i = 1; i < list.length(); i++)
                         {
-                            insert(index + i, list.item(i));
+                            insert(index + i, (XML)list.item(i));
                         }
                     }
                 }
@@ -686,7 +691,7 @@ class XMLList extends XMLObjectImpl implements Function
             XMLList xmlSrc = (XMLList)toAdd;
             for (int i = 0; i < xmlSrc.length(); i++)
             {
-                _annos.add((xmlSrc.item(i)).getAnnotation());
+                _annos.add(((XML)xmlSrc.item(i)).getAnnotation());
             }
         }
         else if (toAdd instanceof XML)
@@ -1261,7 +1266,7 @@ class XMLList extends XMLObjectImpl implements Function
         } else if (name instanceof Number) {
             double x = ((Number)name).doubleValue();
             index = (long)x;
-            if (index != x) {
+            if ((double)index != x) {
                 return false;
             }
             if (index == 0 && 1.0 / x < 0) {
