7e9743f74df19bed9607e73fb1aaf5e1710ba2d5
Norris Boyd <norrisboyd@gmail.com>
2008-06-11 13:02:13 +0000

    Fix bug 437988. Rhino must discover the abstract methods in the interface of a base class in JavaAdapter

--- src/org/mozilla/javascript/JavaAdapter.java
+++ src/org/mozilla/javascript/JavaAdapter.java
@@ -452,30 +452,38 @@ public final class JavaAdapter implements IdFunctionCall
         ArrayList<Method> list = new ArrayList<Method>();
         HashSet<String> skip = new HashSet<String>();
         while (c != null) {
-            Method[] methods = c.getDeclaredMethods();
-            for (int i = 0; i < methods.length; i++) {
-                String methodKey = methods[i].getName() + 
-                    getMethodSignature(methods[i],
-                            methods[i].getParameterTypes());
-                if (skip.contains(methodKey))
-                    continue; // skip this method
-                int mods = methods[i].getModifiers();
-                if (Modifier.isStatic(mods))
-                    continue;
-                if (Modifier.isFinal(mods)) {
-                    // Make sure we don't add a final method to the list
-                    // of overridable methods.
-                    skip.add(methodKey);
-                    continue;
-                }
-                if (Modifier.isPublic(mods) || Modifier.isProtected(mods)) {
-                    list.add(methods[i]);
-                    skip.add(methodKey);
-                }
-            }
+            appendOverridableMethods(c, list, skip);
+            for (Class<?> intf: c.getInterfaces())
+                appendOverridableMethods(intf, list, skip);
             c = c.getSuperclass();
         }
         return list.toArray(new Method[list.size()]);
+    }
+    
+    private static void appendOverridableMethods(Class<?> c, 
+            ArrayList<Method> list, HashSet<String> skip)
+    {
+        Method[] methods = c.getDeclaredMethods();
+        for (int i = 0; i < methods.length; i++) {
+            String methodKey = methods[i].getName() + 
+                getMethodSignature(methods[i],
+                        methods[i].getParameterTypes());
+            if (skip.contains(methodKey))
+                continue; // skip this method
+            int mods = methods[i].getModifiers();
+            if (Modifier.isStatic(mods))
+                continue;
+            if (Modifier.isFinal(mods)) {
+                // Make sure we don't add a final method to the list
+                // of overridable methods.
+                skip.add(methodKey);
+                continue;
+            }
+            if (Modifier.isPublic(mods) || Modifier.isProtected(mods)) {
+                list.add(methods[i]);
+                skip.add(methodKey);
+            }
+        }
     }
 
     static Class<?> loadAdapterClass(String className, byte[] classBytes)
