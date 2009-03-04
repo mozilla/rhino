55a35758ea2e25db31382c80b77aa30c671cb5bd
Attila Szegedi <szegedia@gmail.com>
2009-03-04 14:58:47 +0000

    Make sure we can lazily load our standard objects even when invoked from code that wouldn't have privilege to access those classes

--- src/org/mozilla/javascript/LazilyLoadedCtor.java
+++ src/org/mozilla/javascript/LazilyLoadedCtor.java
@@ -40,6 +40,8 @@
 package org.mozilla.javascript;
 
 import java.lang.reflect.*;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
 
 /**
  * Avoid loading classes unless they are used.
@@ -57,21 +59,29 @@ public final class LazilyLoadedCtor implements java.io.Serializable {
     private final String propertyName;
     private final String className;
     private final boolean sealed;
+    private final boolean privileged;
     private Object initializedValue;
     private int state;
 
-    public LazilyLoadedCtor(ScriptableObject scope, String propertyName,
-                            String className, boolean sealed)
+    public LazilyLoadedCtor(ScriptableObject scope, String propertyName, 
+            String className, boolean sealed)
+    {
+        this(scope, propertyName, className, sealed, false);
+    }
+
+    LazilyLoadedCtor(ScriptableObject scope, String propertyName,
+            String className, boolean sealed, boolean privileged)
     {
 
         this.scope = scope;
         this.propertyName = propertyName;
         this.className = className;
         this.sealed = sealed;
+        this.privileged = privileged;
         this.state = STATE_BEFORE_INIT;
 
         scope.addLazilyInitializedValue(propertyName, 0, this,
-                                        ScriptableObject.DONTENUM);
+                ScriptableObject.DONTENUM);
     }
 
     void init()
@@ -103,6 +113,24 @@ public final class LazilyLoadedCtor implements java.io.Serializable {
     }
 
     private Object buildValue()
+    {
+        if(privileged)
+        {
+            return AccessController.doPrivileged(new PrivilegedAction<Object>()
+            {
+                public Object run()
+                {
+                    return buildValue0();
+                }
+            });
+        }
+        else
+        {
+            return buildValue0();
+        }
+    }
+    
+    private Object buildValue0()
     {
         Class<? extends Scriptable> cl = cast(Kit.classOrNull(className));
         if (cl != null) {
