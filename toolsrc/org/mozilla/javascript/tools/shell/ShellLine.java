258fed8eb5f31e70272e1efac9119439ac468892
Norris Boyd <norrisboyd@gmail.com>
2009-03-06 13:37:13 +0000

    Various fixes for autocompletion behavior.

--- toolsrc/org/mozilla/javascript/tools/shell/ShellLine.java
+++ toolsrc/org/mozilla/javascript/tools/shell/ShellLine.java
@@ -119,7 +119,7 @@ class FlexibleCompletor implements java.lang.reflect.InvocationHandler {
         if (method.equals(this.completeMethod)) {
             int result = complete((String)args[0], ((Integer) args[1]).intValue(),
                     (List<String>) args[2]);
-            return new Integer(result);
+            return Integer.valueOf(result);
         }
         throw new NoSuchMethodError(method.toString());
     }
@@ -133,12 +133,13 @@ class FlexibleCompletor implements java.lang.reflect.InvocationHandler {
         // fragment as a prefix and return those for autocompletion.
         int m = cursor - 1;
         while (m >= 0) {
-            char c = buffer.charAt(m--);
+            char c = buffer.charAt(m);
             if (!Character.isJavaIdentifierPart(c) && c != '.')
                 break;
+            m--;
         }
         String namesAndDots = buffer.substring(m+1, cursor);
-        String[] names = namesAndDots.split("\\.");
+        String[] names = namesAndDots.split("\\.", -1);
         Scriptable obj = this.global;
         for (int i=0; i < names.length - 1; i++) {
             Object val = obj.get(names[i], global);
