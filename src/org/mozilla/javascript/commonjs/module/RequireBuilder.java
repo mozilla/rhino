8f623d32f7e999ba67efa278a9ee8030c50f8619
Attila Szegedi <szegedia@gmail.com>
2010-02-19 09:44:52 +0000

    "Fluidized" the builder setters, so they can be chained

--- src/org/mozilla/javascript/commonjs/module/RequireBuilder.java
+++ src/org/mozilla/javascript/commonjs/module/RequireBuilder.java
@@ -11,8 +11,10 @@ import org.mozilla.javascript.Scriptable;
  * instances of {@link Require} that are identical except for their top-level
  * scope and current {@link Context}. Also useful if you prefer configuring it
  * using named setters instead of passing many parameters in a constructor.
+ * Every setter returns "this", so you can easily chain their invocations for 
+ * additional convenience.
  * @author Attila Szegedi
- * @version $Id: RequireBuilder.java,v 1.1 2010/02/15 19:31:14 szegedia%freemail.hu Exp $
+ * @version $Id: RequireBuilder.java,v 1.2 2010/02/19 09:44:52 szegedia%freemail.hu Exp $
  */
 public class RequireBuilder implements Serializable
 {
@@ -28,29 +30,35 @@ public class RequireBuilder implements Serializable
      * that this builder builds.
      * @param moduleScriptProvider the module script provider for the 
      * {@link Require} instances that this builder builds. 
+     * @return this, so you can chain ("fluidize") setter invocations
      */
-    public void setModuleScriptProvider(
+    public RequireBuilder setModuleScriptProvider(
             ModuleScriptProvider moduleScriptProvider)
     {
         this.moduleScriptProvider = moduleScriptProvider;
+        return this;
     }
     
     /**
      * Sets the script that should execute in every module's scope after the
      * module's own script has executed.
      * @param postExec the post-exec script.
+     * @return this, so you can chain ("fluidize") setter invocations
      */
-    public void setPostExec(Script postExec) {
+    public RequireBuilder setPostExec(Script postExec) {
         this.postExec = postExec;
+        return this;
     }
 
     /**
      * Sets the script that should execute in every module's scope before the
      * module's own script has executed.
      * @param preExec the pre-exec script.
+     * @return this, so you can chain ("fluidize") setter invocations
      */
-    public void setPreExec(Script preExec) {
+    public RequireBuilder setPreExec(Script preExec) {
         this.preExec = preExec;
+        return this;
     }
     
     /**
@@ -59,9 +67,11 @@ public class RequireBuilder implements Serializable
      * Script, Script, boolean)} for explanation.
      * @param sandboxed true if the created require() instances will be 
      * sandboxed.
+     * @return this, so you can chain ("fluidize") setter invocations
      */
-    public void setSandboxed(boolean sandboxed) {
+    public RequireBuilder setSandboxed(boolean sandboxed) {
         this.sandboxed = sandboxed;
+        return this;
     }
  
     /**
