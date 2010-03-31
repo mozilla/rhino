3eba3d1adc9ed9ce410c85d763e681ff0f6d96d2
Hannes Wallnöfer <hannesw@gmail.com>
2010-03-31 15:12:43 +0000

    Patch for bug 549604 - Add (non ECMA) property "stack" to NativeError
    This adds a new getScriptStack() method to RhinoException, returning an
    array of instances of the also new ScriptStackElement class.
    This is used to allow applying different formatting styles when generating
    the `stack` property on NativeError objects.
    
    By default, the "traditional" Rhino script stack format is used. There
    is a static useMozillaStackStyle(boolean) in RhinoException to use
    the Mozilla/Firefox script stack rendring format instead.
    
    Also, function names are now included in script stacks from
    compiled code, and the stack property is also generated on Errors
    constructed and thrown by script code if the
    FEATURE_LOCATION_INFORMATION_IN_ERROR is enabled (the latter is now
    done in JavaScriptException constructor instead of NativeError to
    avoid duplication of effort).
    
    Original patch and tests by Marc Guillemot.

--- testsrc/org/mozilla/javascript/tests/StackTraceTest.java
+++ testsrc/org/mozilla/javascript/tests/StackTraceTest.java
@@ -8,23 +8,29 @@ import junit.framework.TestCase;
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.ContextAction;
 import org.mozilla.javascript.JavaScriptException;
+import org.mozilla.javascript.RhinoException;
 import org.mozilla.javascript.Scriptable;
 
 /**
  * @author Marc Guillemot
  */
 public class StackTraceTest extends TestCase {
-    
+    final static String LS = System.getProperty("line.separator");
+
 	/**
 	 * As of CVS head on May, 11. 2009, stacktrace information is lost when a call to some
 	 * native function has been made.
 	 */
     public void testFailureStackTrace() {
+        RhinoException.useMozillaStackStyle(false);
         final String source1 = "function f2() { throw 'hello'; }; f2();";
         final String source2 = "function f2() { 'H'.toLowerCase(); throw 'hello'; }; f2();";
-        
-        runWithExpectedStackTrace(source1, "	at test.js (f2)\n\tat test.js\n"); // works
-        runWithExpectedStackTrace(source2, "	at test.js (f2)\n\tat test.js\n"); // fails
+        final String source3 = "function f2() { new java.lang.String('H').toLowerCase(); throw 'hello'; }; f2();";
+        final String result = "\tat test.js (f2)" + LS + "\tat test.js" + LS;
+
+        runWithExpectedStackTrace(source1, result);
+        runWithExpectedStackTrace(source2, result);
+        runWithExpectedStackTrace(source3, result);
     }
 
 	private void runWithExpectedStackTrace(final String _source, final String _expectedStackTrace)
--- testsrc/org/mozilla/javascript/tests/Utils.java
+++ testsrc/org/mozilla/javascript/tests/Utils.java
@@ -3,6 +3,8 @@ package org.mozilla.javascript.tests;
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.ContextAction;
 import org.mozilla.javascript.ContextFactory;
+import org.mozilla.javascript.Scriptable;
+
 
 /**
  * Misc utilities to make test code easier.
@@ -21,11 +23,29 @@ public class Utils
 	}
 
 	/**
+	 * Runs the action successively with all available optimization levels
+	 */
+	public static void runWithAllOptimizationLevels(final ContextFactory contextFactory, final ContextAction action)
+	{
+		runWithOptimizationLevel(contextFactory, action, -1);
+		runWithOptimizationLevel(contextFactory, action, 0);
+		runWithOptimizationLevel(contextFactory, action, 1);
+	}
+
+	/**
 	 * Runs the provided action at the given optimization level
 	 */
 	public static void runWithOptimizationLevel(final ContextAction action, final int optimizationLevel)
 	{
-    	final Context cx = new ContextFactory().enterContext();
+		runWithOptimizationLevel(new ContextFactory(), action, optimizationLevel);
+	}
+
+	/**
+	 * Runs the provided action at the given optimization level
+	 */
+	public static void runWithOptimizationLevel(final ContextFactory contextFactory, final ContextAction action, final int optimizationLevel)
+	{
+    	final Context cx = contextFactory.enterContext();
     	try
     	{
     		cx.setOptimizationLevel(optimizationLevel);
@@ -35,5 +55,20 @@ public class Utils
     	{
     		Context.exit();
     	}
+	}
+
+	/**
+	 * Execute the provided script in a fresh context as "myScript.js".
+	 * @param script the script code
+	 */
+	static void executeScript(final String script, final int optimizationLevel) {
+		final ContextAction action = new ContextAction()
+		{
+			public Object run(Context cx) {
+				final Scriptable scope = cx.initStandardObjects();
+				return cx.evaluateString(scope, script, "myScript.js", 1, null);
+			}
+		};
+		Utils.runWithOptimizationLevel(action, optimizationLevel);
 	}
 }
