23696754f72933326c16c064c188d2ea5644f99c
Ian Sollars <ian.sollars@gmail.com>
2011-10-28 13:46:00 +0200

    fix for prototype modifications, inline functions and continuations

--- testsrc/org/mozilla/javascript/tests/ContinuationsApiTest.java
+++ testsrc/org/mozilla/javascript/tests/ContinuationsApiTest.java
@@ -51,6 +51,7 @@ import org.mozilla.javascript.WrappedException;
 import org.mozilla.javascript.serialize.ScriptableInputStream;
 import org.mozilla.javascript.serialize.ScriptableOutputStream;
 
+
 /**
  * Test of new API functions for running and resuming scripts containing
  * continuations, and for suspending a continuation from a Java method
@@ -81,6 +82,16 @@ public class ContinuationsApiTest extends TestCase {
         try {
             ContinuationPending pending = cx.captureContinuation();
             pending.setApplicationState(2*a);
+            throw pending;
+        } finally {
+            Context.exit();
+        }
+    }
+    public String h() {
+        Context cx = Context.enter();
+        try {
+            ContinuationPending pending = cx.captureContinuation();
+            pending.setApplicationState("2*3");
             throw pending;
         } finally {
             Context.exit();
@@ -260,4 +271,114 @@ public class ContinuationsApiTest extends TestCase {
           Context.exit();
       }
   }
+
+  public void testContinuationsPrototypesAndSerialization() throws IOException, ClassNotFoundException {
+
+      byte[] serializedData = null;
+
+      {
+          Scriptable globalScope;
+          Context cx = Context.enter();
+          try {
+              globalScope = cx.initStandardObjects();
+              cx.setOptimizationLevel(-1); // must use interpreter mode
+              globalScope.put("myObject", globalScope, Context.javaToJS(new MyClass(), globalScope));
+          } finally {
+              Context.exit();
+          }
+
+          cx = Context.enter();
+          try {
+              cx.setOptimizationLevel(-1); // must use interpreter mode
+              cx.evaluateString(
+                      globalScope,
+                      "function f(a) { Number.prototype.blargh = function() {return 'foo';}; var k = myObject.f(a); var t = []; return new Number(8).blargh(); }",
+                      "function test source", 1, null);
+              Function f = (Function) globalScope.get("f", globalScope);
+              Object[] args = { 7 };
+              cx.callFunctionWithContinuations(f, globalScope, args);
+              fail("Should throw ContinuationPending");
+          } catch (ContinuationPending pending) {
+              // serialize
+              ByteArrayOutputStream baos = new ByteArrayOutputStream();
+              ObjectOutputStream sos = new ObjectOutputStream(baos);
+              sos.writeObject(globalScope);
+              sos.writeObject(pending.getContinuation());
+              sos.close();
+              baos.close();
+              serializedData = baos.toByteArray();
+          } finally {
+              Context.exit();
+          }
+      }
+
+      {
+          try {
+              Context cx = Context.enter();
+
+              Scriptable globalScope;
+
+              // deserialize
+              ByteArrayInputStream bais = new ByteArrayInputStream(serializedData);
+              ObjectInputStream sis = new ObjectInputStream(bais);
+              globalScope = (Scriptable) sis.readObject();
+              Object continuation = sis.readObject();
+              sis.close();
+              bais.close();
+
+              Object result = cx.resumeContinuation(continuation, globalScope, 8);
+              assertEquals("foo", result);
+          } finally {
+              Context.exit();
+          }
+      }
+
+  }
+
+  public void testContinuationsInlineFunctionsSerialization() throws IOException, ClassNotFoundException {
+
+      Scriptable globalScope;
+      Context cx = Context.enter();
+      try {
+          globalScope = cx.initStandardObjects();
+          cx.setOptimizationLevel(-1); // must use interpreter mode
+          globalScope.put("myObject", globalScope, Context.javaToJS(new MyClass(), globalScope));
+      } finally {
+          Context.exit();
+      }
+
+      cx = Context.enter();
+      try {
+          cx.setOptimizationLevel(-1); // must use interpreter mode
+          cx.evaluateString(globalScope, "function f(a) { var k = eval(myObject.h()); var t = []; return k; }",
+                  "function test source", 1, null);
+          Function f = (Function) globalScope.get("f", globalScope);
+          Object[] args = { 7 };
+          cx.callFunctionWithContinuations(f, globalScope, args);
+          fail("Should throw ContinuationPending");
+      } catch (ContinuationPending pending) {
+          // serialize
+          ByteArrayOutputStream baos = new ByteArrayOutputStream();
+          ScriptableOutputStream sos = new ScriptableOutputStream(baos, globalScope);
+          sos.writeObject(globalScope);
+          sos.writeObject(pending.getContinuation());
+          sos.close();
+          baos.close();
+          byte[] serializedData = baos.toByteArray();
+
+          // deserialize
+          ByteArrayInputStream bais = new ByteArrayInputStream(serializedData);
+          ScriptableInputStream sis = new ScriptableInputStream(bais, globalScope);
+          globalScope = (Scriptable) sis.readObject();
+          Object continuation = sis.readObject();
+          sis.close();
+          bais.close();
+
+          Object result = cx.resumeContinuation(continuation, globalScope, "2+3");
+          assertEquals(5, ((Number) result).intValue());
+      } finally {
+          Context.exit();
+      }
+  }
+
 }
