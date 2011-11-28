6761a962463eb49701cadfa63f8428b3ed17ac6b
Hannes Wallnöfer <hannesw@gmail.com>
2011-11-28 11:47:24 +0100

    Make Arguments and NativeCall non-final and not extend IdScriptableObject
    This is a major rewrite of the Arguments class. The main goals are to
    make NativeCall and Arguments subclassable, fast to instantiate and not
    extend IdScriptableObject.

--- src/org/mozilla/javascript/Arguments.java
+++ src/org/mozilla/javascript/Arguments.java
@@ -24,6 +24,7 @@
  * Contributor(s):
  *   Norris Boyd
  *   Igor Bukanov
+ *   Hannes Wallnoefer
  *
  * Alternatively, the contents of this file may be used under the terms of
  * the GNU General Public License Version 2 or later (the "GPL"), in which
@@ -47,360 +48,279 @@ package org.mozilla.javascript;
  * @see org.mozilla.javascript.NativeCall
  * @author Norris Boyd
  */
-final class Arguments extends IdScriptableObject
+public class Arguments extends ScriptableObject 
 {
     static final long serialVersionUID = 4275508002492040609L;
 
-    private static final String FTAG = "Arguments";
-
-    public Arguments(NativeCall activation)
-    {
-        this.activation = activation;
-
+    public Arguments(NativeCall activation) {
         Scriptable parent = activation.getParentScope();
         setParentScope(parent);
-        setPrototype(ScriptableObject.getObjectPrototype(parent));
+        Scriptable scope = ScriptableObject.getTopLevelScope(parent);
+        setPrototype(TopLevel.getBuiltinPrototype(scope, TopLevel.Builtins.Object));
 
+        this.activation = activation;
         args = activation.originalArgs;
+        constructor = TopLevel.getBuiltinCtor(scope, TopLevel.Builtins.Object);
         lengthObj = Integer.valueOf(args.length);
-
-        NativeFunction f = activation.function;
-        calleeObj = f;
-
-        Scriptable topLevel = getTopLevelScope(parent);
-        constructor = getProperty(topLevel, "Object");
-
-        int version = f.getLanguageVersion();
-        if (version <= Context.VERSION_1_3
-            && version != Context.VERSION_DEFAULT)
-        {
-            callerObj = null;
-        } else {
-            callerObj = NOT_FOUND;
-        }
+        calleeObj = activation.function;
     }
 
-    @Override
-    public String getClassName()
-    {
-        return FTAG;
+    public String getClassName() {
+        return "Arguments";
     }
 
-    private Object arg(int index) {
-      if (index < 0 || args.length <= index) return NOT_FOUND;
-      return args[index];
+    protected boolean hasArg(int index) {
+        return index >= 0
+                && index < args.length
+                && args[index] != NOT_FOUND;
     }
 
     // the following helper methods assume that 0 < index < args.length
 
-    private void putIntoActivation(int index, Object value) {
-        String argName = activation.function.getParamOrVarName(index);
-        activation.put(argName, activation, value);
-    }
-
-    private Object getFromActivation(int index) {
-        String argName = activation.function.getParamOrVarName(index);
-        return activation.get(argName, activation);
-    }
-
-    private void replaceArg(int index, Object value) {
-      if (sharedWithActivation(index)) {
-        putIntoActivation(index, value);
-      }
-      synchronized (this) {
-        if (args == activation.originalArgs) {
-          args = args.clone();
+    protected void setArg(int index, Object value) {
+        synchronized (this) {
+            if (args == activation.originalArgs) {
+                args = args.clone();
+            }
+            args[index] = value;
         }
-        args[index] = value;
-      }
+        String argName = getActivationName(index);
+        if (argName != null) {
+            activation.put(argName, activation, value);
+        }
+    }
+
+    protected Object getArg(int index) {
+        String argName = getActivationName(index);
+        if (argName != null) {
+            return activation.get(argName, activation);
+        } else {
+            return args[index];
+        }
     }
 
     private void removeArg(int index) {
-      synchronized (this) {
         if (args[index] != NOT_FOUND) {
-          if (args == activation.originalArgs) {
-            args = args.clone();
-          }
-          args[index] = NOT_FOUND;
+            synchronized (this) {
+                if (args == activation.originalArgs) {
+                    args = args.clone();
+                }
+                args[index] = NOT_FOUND;
+            }
         }
-      }
     }
 
     // end helpers
 
     @Override
-    public boolean has(int index, Scriptable start)
-    {
-        if (arg(index) != NOT_FOUND) {
-          return true;
-        }
-        return super.has(index, start);
+    public boolean has(int index, Scriptable start) {
+        return hasArg(index) || super.has(index, start);
     }
 
     @Override
-    public Object get(int index, Scriptable start)
-    {
-      final Object value = arg(index);
-      if (value == NOT_FOUND) {
-        return super.get(index, start);
-      } else {
-        if (sharedWithActivation(index)) {
-          return getFromActivation(index);
-        } else {
-          return value;
+    public Object get(int index, Scriptable start) {
+        if (hasArg(index)) {
+            return getArg(index);
         }
-      }
+        return super.get(index, start);
     }
-
-    private boolean sharedWithActivation(int index)
-    {
+    
+    /**
+     * Return the activation name of the given parameter, or null if the
+     * parameter does not have an activation name, either because it is
+     * shadowed by a later parameter or is not a defined parameter.
+     * @param index the parameter index
+     * @return the activation name, or null
+     */
+    private String getActivationName(int index) {
         NativeFunction f = activation.function;
         int definedCount = f.getParamCount();
         if (index < definedCount) {
             // Check if argument is not hidden by later argument with the same
             // name as hidden arguments are not shared with activation
-            if (index < definedCount - 1) {
-                String argName = f.getParamOrVarName(index);
-                for (int i = index + 1; i < definedCount; i++) {
-                    if (argName.equals(f.getParamOrVarName(i))) {
-                        return false;
-                    }
+            String argName = f.getParamOrVarName(index);
+            for (int i = index + 1; i < definedCount; i++) {
+                if (argName.equals(f.getParamOrVarName(i))) {
+                    // argument is shadowed by later argument
+                    return null;
                 }
             }
+            return argName;
+        }
+        // not a defined parameter
+        return null;
+    }
+
+    @Override
+    public void put(int index, Scriptable start, Object value) {
+        if (hasArg(index)) {
+            setArg(index, value);
+        } else {
+            super.put(index, start, value);
+        }
+    }
+
+    @Override
+    public void delete(int index) {
+        if (0 <= index && index < args.length) {
+            removeArg(index);
+        }
+        super.delete(index);
+    }
+
+    @Override
+    public boolean has(String name, Scriptable start) {
+        if (super.has(name, start)) {
             return true;
+        } else if (name.equals(LENGTH)) {
+            return lengthObj != NOT_FOUND;
+        } else if (name.equals(CONSTRUCTOR)) {
+            return constructor != NOT_FOUND;
+        } else if (name.equals(CALLEE)) {
+            return calleeObj != NOT_FOUND;
         }
         return false;
     }
 
     @Override
-    public void put(int index, Scriptable start, Object value)
-    {
-        if (arg(index) == NOT_FOUND) {
-          super.put(index, start, value);
-        } else {
-          replaceArg(index, value);
-        }
-    }
-
-    @Override
-    public void delete(int index)
-    {
-        if (0 <= index && index < args.length) {
-          removeArg(index);
-        }
-        super.delete(index);
-    }
-
-// #string_id_map#
-
-    private static final int
-        Id_callee           = 1,
-        Id_length           = 2,
-        Id_caller           = 3,
-        Id_constructor      = 4,
-
-        MAX_INSTANCE_ID     = Id_constructor;
-
-    @Override
-    protected int getMaxInstanceId()
-    {
-        return MAX_INSTANCE_ID;
-    }
-
-    @Override
-    protected int findInstanceIdInfo(String s)
-    {
-        int id;
-// #generated# Last update: 2010-01-06 05:48:21 ARST
-        L0: { id = 0; String X = null; int c;
-            int s_length = s.length();
-            if (s_length==6) {
-                c=s.charAt(5);
-                if (c=='e') { X="callee";id=Id_callee; }
-                else if (c=='h') { X="length";id=Id_length; }
-                else if (c=='r') { X="caller";id=Id_caller; }
-            }
-            else if (s_length==11) { X="constructor";id=Id_constructor; }
-            if (X!=null && X!=s && !X.equals(s)) id = 0;
-            break L0;
-        }
-// #/generated#
-
-        if (id == 0) return super.findInstanceIdInfo(s);
-
-        int attr;
-        switch (id) {
-          case Id_callee:
-          case Id_caller:
-          case Id_length:
-          case Id_constructor:
-            attr = DONTENUM;
-            break;
-          default: throw new IllegalStateException();
-        }
-        return instanceIdInfo(attr, id);
-    }
-
-// #/string_id_map#
-
-    @Override
-    protected String getInstanceIdName(int id)
-    {
-        switch (id) {
-            case Id_callee: return "callee";
-            case Id_length: return "length";
-            case Id_caller: return "caller";
-            case Id_constructor: return "constructor";
-        }
-        return null;
-    }
-
-    @Override
-    protected Object getInstanceIdValue(int id)
-    {
-        switch (id) {
-            case Id_callee: return calleeObj;
-            case Id_length: return lengthObj;
-            case Id_caller: {
-                Object value = callerObj;
-                if (value == UniqueTag.NULL_VALUE) { value = null; }
-                else if (value == null) {
-                    NativeCall caller = activation.parentActivationCall;
-                    if (caller != null) {
-                        value = caller.get("arguments", caller);
-                    }
-                }
-                return value;
-            }
-            case Id_constructor:
-                return constructor;
-        }
-        return super.getInstanceIdValue(id);
-    }
-
-    @Override
-    protected void setInstanceIdValue(int id, Object value)
-    {
-        switch (id) {
-            case Id_callee: calleeObj = value; return;
-            case Id_length: lengthObj = value; return;
-            case Id_caller:
-                callerObj = (value != null) ? value : UniqueTag.NULL_VALUE;
-                return;
-            case Id_constructor: constructor = value; return;
-        }
-        super.setInstanceIdValue(id, value);
-    }
-
-    @Override
-    Object[] getIds(boolean getAll)
-    {
-        Object[] ids = super.getIds(getAll);
-        if (args.length != 0) {
-            boolean[] present = new boolean[args.length];
-            int extraCount = args.length;
-            for (int i = 0; i != ids.length; ++i) {
-                Object id = ids[i];
-                if (id instanceof Integer) {
-                    int index = ((Integer)id).intValue();
-                    if (0 <= index && index < args.length) {
-                        if (!present[index]) {
-                            present[index] = true;
-                            extraCount--;
-                        }
-                    }
-                }
-            }
-            if (!getAll) { // avoid adding args which were redefined to non-enumerable
-              for (int i = 0; i < present.length; i++) {
-                if (!present[i] && super.has(i, this)) {
-                  present[i] = true;
-                  extraCount--;
-                }
-              }
-            }
-            if (extraCount != 0) {
-                Object[] tmp = new Object[extraCount + ids.length];
-                System.arraycopy(ids, 0, tmp, extraCount, ids.length);
-                ids = tmp;
-                int offset = 0;
-                for (int i = 0; i != args.length; ++i) {
-                    if (present == null || !present[i]) {
-                        ids[offset] = Integer.valueOf(i);
-                        ++offset;
-                    }
-                }
-                if (offset != extraCount) Kit.codeBug();
+    public Object get(String name, Scriptable start) {
+        Object value = super.get(name, start);
+        if (value == NOT_FOUND) {
+            if (name.equals(LENGTH)) {
+                value = lengthObj;
+            } else if (name.equals(CONSTRUCTOR)) {
+                value = constructor;
+            } else if (name.equals(CALLEE)) {
+                value = calleeObj;
             }
         }
-        return ids;
+        return value;
+    }
+
+    @Override
+    public void put(String name, Scriptable start, Object value) {
+        super.put(name, start, value);
+        if (name.equals(LENGTH)) {
+            lengthObj = NOT_FOUND;
+        } else if (name.equals(CONSTRUCTOR)) {
+            constructor = NOT_FOUND;
+        } else if (name.equals(CALLEE)) {
+            calleeObj = NOT_FOUND;
+        }
+    }
+
+    @Override
+    public void delete(String name) {
+        super.delete(name);
+        if (name.equals(LENGTH)) {
+            lengthObj = NOT_FOUND;
+        } else if (name.equals(CONSTRUCTOR)) {
+            constructor = NOT_FOUND;
+        } else if (name.equals(CALLEE)) {
+            calleeObj = NOT_FOUND;
+        }
+    }
+
+    @Override
+    Object[] getIds(boolean getAll) {
+        int length = args.length;
+        Object[] superIds = super.getIds(getAll);
+        if (length == 0 && !getAll) {
+            return superIds;
+        }
+        int extraLength = getAll ? length + 3 : length;
+        Object[] ownIds = new Object[extraLength];
+        int count = 0;
+        for (int i = 0; i < length; i++) {
+            // avoid adding args which are already present in superIds
+            // (or would be if they were enumerable)
+            if (hasArg(i) && !super.has(i, this)) {
+                ownIds[count++] = Integer.valueOf(i);
+            }
+        }
+        if (getAll) {
+            if (lengthObj != NOT_FOUND && !super.has(LENGTH, this))
+                ownIds[count++] = LENGTH;
+            if (calleeObj != NOT_FOUND && !super.has(CALLEE, this))
+                ownIds[count++] = CALLEE;
+            if (constructor != NOT_FOUND && !super.has(CONSTRUCTOR, this))
+                ownIds[count++] = CONSTRUCTOR;
+        }
+        if (superIds.length == 0 && count == extraLength) {
+            // common case
+            return ownIds;
+        }
+        if (count > 0) {
+            int mergedLength = superIds.length + count;
+            Object[] merged = new Object[mergedLength];
+            System.arraycopy(ownIds, 0, merged, 0, count);
+            System.arraycopy(superIds, 0, merged, count, superIds.length);
+            return merged;
+        }
+        return superIds;
     }
 
     @Override
     protected ScriptableObject getOwnPropertyDescriptor(Context cx, Object id) {
-      double d = ScriptRuntime.toNumber(id);
-      int index = (int) d;
-      if (d != index) {
-        return super.getOwnPropertyDescriptor(cx, id);
-      }
-      Object value = arg(index);
-      if (value == NOT_FOUND) {
-        return super.getOwnPropertyDescriptor(cx, id);
-      }
-      if (sharedWithActivation(index)) {
-        value = getFromActivation(index);
-      }
-      if (super.has(index, this)) { // the descriptor has been redefined
-        ScriptableObject desc = super.getOwnPropertyDescriptor(cx, id);
-        desc.put("value", desc, value);
-        return desc;
-      } else {
-        Scriptable scope = getParentScope();
-        if (scope == null) scope = this;
-        return buildDataDescriptor(scope, value, EMPTY);
-      }
+        double d = ScriptRuntime.toNumber(id);
+        int index = (int) d;
+        if (d != index || !hasArg(index) || super.has(index, this)) {
+            ScriptableObject desc = super.getOwnPropertyDescriptor(cx, id);
+            if (desc == null) {
+                if (id.equals(LENGTH) && lengthObj != NOT_FOUND) {
+                    desc = buildDataDescriptor(this, lengthObj, DONTENUM);
+                } else if (id.equals(CONSTRUCTOR) && constructor != NOT_FOUND) {
+                    desc = buildDataDescriptor(this, constructor, DONTENUM);
+                } else if (id.equals(CALLEE) && calleeObj != NOT_FOUND) {
+                    desc = buildDataDescriptor(this, calleeObj, DONTENUM);
+                }
+            }
+            return desc;
+        }
+        return buildDataDescriptor(this, getArg(index), EMPTY);
     }
 
     @Override
     public void defineOwnProperty(Context cx, Object id, ScriptableObject desc) {
-      super.defineOwnProperty(cx, id, desc);
+        super.defineOwnProperty(cx, id, desc);
 
-      double d = ScriptRuntime.toNumber(id);
-      int index = (int) d;
-      if (d != index) return;
+        double d = ScriptRuntime.toNumber(id);
+        int index = (int) d;
+        if (d != index) return;
 
-      Object value = arg(index);
-      if (value == NOT_FOUND) return;
+        if (!hasArg(index)) return;
 
-      if (isAccessorDescriptor(desc)) {
-        removeArg(index);
-        return;
-      }
+        if (isAccessorDescriptor(desc)) {
+          removeArg(index);
+          return;
+        }
 
-      Object newValue = getProperty(desc, "value");
-      if (newValue == NOT_FOUND) return;
+        Object newValue = getProperty(desc, "value");
 
-      replaceArg(index, newValue);
+        if (newValue == NOT_FOUND) return;
 
-      if (isFalse(getProperty(desc, "writable"))) {
-        removeArg(index);
-      }
+        setArg(index, newValue);
+        if (isFalse(getProperty(desc, "writable"))) {
+            removeArg(index);
+        }
+
     }
 
-// Fields to hold caller, callee and length properties,
-// where NOT_FOUND value tags deleted properties.
-// In addition if callerObj == NULL_VALUE, it tags null for scripts, as
-// initial callerObj == null means access to caller arguments available
-// only in JS <= 1.3 scripts
-    private Object callerObj;
+    private static final String
+            CALLEE = "callee",
+            CONSTRUCTOR = "constructor",
+            LENGTH = "length";
+    
+    // Fields to hold caller, callee and length properties,
+    // where NOT_FOUND value tags deleted properties.
     private Object calleeObj;
     private Object lengthObj;
     private Object constructor;
 
-    private NativeCall activation;
+    private final NativeCall activation;
 
-// Initially args holds activation.getOriginalArgs(), but any modification
-// of its elements triggers creation of a copy. If its element holds NOT_FOUND,
-// it indicates deleted index, in which case super class is queried.
+    // Initially args holds activation.getOriginalArgs(), but any modification
+    // of its elements triggers creation of a copy. If its element holds NOT_FOUND,
+    // it indicates deleted index, in which case super class is queried.
     private Object[] args;
 }
