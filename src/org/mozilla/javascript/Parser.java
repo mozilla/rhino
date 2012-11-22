bb8528165aefdf3b0b868fc26756ea67c6a4e830
tntim96 <tntim96@gmail.com>
2012-11-23 07:39:43 +1100

    Bug 800616 - Fix Hex/Oct literal toSource

--- src/org/mozilla/javascript/Parser.java
+++ src/org/mozilla/javascript/Parser.java
@@ -2796,6 +2796,12 @@ public class Parser
               if (this.inUseStrictDirective && ts.isNumberOctal()) {
                   reportError("msg.no.octal.strict");
               }
+              if (ts.isNumberOctal()) {
+                  s = "0"+s;
+              }
+              if (ts.isNumberHex()) {
+                  s = "0x"+s;
+              }
               return new NumberLiteral(ts.tokenBeg,
                                        s,
                                        ts.getNumber());
