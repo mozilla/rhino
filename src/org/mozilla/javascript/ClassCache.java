5f1ad11dfbf23b3afcc8caa6c230fa3533b5ffd6
Hannes Wallnoefer <hannesw@gmail.com>
2013-02-23 16:55:37 +0100

    Reduce concurrency level / memory footprint in concurrent class cache hash maps

--- src/org/mozilla/javascript/ClassCache.java
+++ src/org/mozilla/javascript/ClassCache.java
@@ -23,9 +23,9 @@ public class ClassCache implements Serializable
     private static final long serialVersionUID = -8866246036237312215L;
     private static final Object AKEY = "ClassCache";
     private volatile boolean cachingIsEnabled = true;
-    private transient ConcurrentHashMap<Class<?>,JavaMembers> classTable;
-    private transient ConcurrentHashMap<JavaAdapter.JavaAdapterSignature,Class<?>> classAdapterCache;
-    private transient ConcurrentHashMap<Class<?>,Object> interfaceAdapterCache;
+    private transient Map<Class<?>,JavaMembers> classTable;
+    private transient Map<JavaAdapter.JavaAdapterSignature,Class<?>> classAdapterCache;
+    private transient Map<Class<?>,Object> interfaceAdapterCache;
     private int generatedClassSerial;
     private Scriptable associatedScope;
 
@@ -128,7 +128,9 @@ public class ClassCache implements Serializable
      */
     Map<Class<?>,JavaMembers> getClassCacheMap() {
         if (classTable == null) {
-            classTable = new ConcurrentHashMap<Class<?>,JavaMembers>();
+            // Use 1 as concurrency level here and for other concurrent hash maps
+            // as we don't expect high levels of sustained concurrent writes.
+            classTable = new ConcurrentHashMap<Class<?>,JavaMembers>(16, 0.75f, 1);
         }
         return classTable;
     }
@@ -136,7 +138,7 @@ public class ClassCache implements Serializable
     Map<JavaAdapter.JavaAdapterSignature,Class<?>> getInterfaceAdapterCacheMap()
     {
         if (classAdapterCache == null) {
-            classAdapterCache = new ConcurrentHashMap<JavaAdapter.JavaAdapterSignature,Class<?>>();
+            classAdapterCache = new ConcurrentHashMap<JavaAdapter.JavaAdapterSignature,Class<?>>(16, 0.75f, 1);
         }
         return classAdapterCache;
     }
@@ -184,7 +186,7 @@ public class ClassCache implements Serializable
     {
         if (cachingIsEnabled) {
             if (interfaceAdapterCache == null) {
-                interfaceAdapterCache = new ConcurrentHashMap<Class<?>,Object>();
+                interfaceAdapterCache = new ConcurrentHashMap<Class<?>,Object>(16, 0.75f, 1);
             }
             interfaceAdapterCache.put(cl, iadapter);
         }
