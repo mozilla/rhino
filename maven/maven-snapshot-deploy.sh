d05d93f2835d95896ea08ed30f9d6f45f2e581ac
Ievgenii.Shepeliuk <I.Shepeliuk@external.oberthur.com>
2015-05-07 14:43:12 +0300

    Final Gradle adoption

--- maven/maven-snapshot-deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-mvn deploy:deploy-file \
-  -Dfile=../build/rhino1_7R5pre/js.jar \
-  -DpomFile=maven-pom.xml \
-  -DrepositoryId=sonatype-nexus-snapshots \
-  -Durl=https://oss.sonatype.org/content/repositories/snapshots/
