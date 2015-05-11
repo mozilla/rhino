e985ecd7bcba25c706d8db5e1813c33536859812
Raimund Jacob <jacob@pinuts.de>
2015-05-11 10:52:16 +0200

    176: Actually Working Tests

--- testsrc/org/mozilla/javascript/tests/Issue176Test.java
+++ testsrc/org/mozilla/javascript/tests/Issue176Test.java
@@ -8,20 +8,13 @@ import java.io.InputStreamReader;
 
 import junit.framework.TestCase;
 
-import org.mozilla.javascript.Callable;
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.Script;
 import org.mozilla.javascript.ScriptRuntime;
 import org.mozilla.javascript.Scriptable;
-import org.mozilla.javascript.ScriptableObject;
 
 public class Issue176Test extends TestCase {
 
-    public static Issue176Test singleton;
-
-    public Issue176Test() {
-        this.singleton = this;
-    }
 
     Context cx;
     Scriptable scope;
@@ -31,22 +24,23 @@ public class Issue176Test extends TestCase {
         try {
             Script script = cx.compileReader(new InputStreamReader(
                     Bug482203Test.class.getResourceAsStream("Issue176.js")),
-                    "", 1, null);
+                    "Issue176.js", 1, null);
             scope = cx.initStandardObjects();
-            script.exec(cx, scope); // calls our static methods
+            scope.put("host", scope, this);
+            script.exec(cx, scope); // calls our methods
         } finally {
             Context.exit();
         }
     }
 
 
-    public static void throwError(String msg) {
-        ScriptRuntime.throwError(singleton.cx, singleton.scope, msg);
+    public void throwError(String msg) {
+        throw ScriptRuntime.throwError(cx, scope, msg);
     }
 
 
-    public static void throwCustomg(String constr, String msg) {
-        ScriptRuntime.throwCustomError(singleton.cx, singleton.scope, constr, msg);
+    public void throwCustomError(String constr, String msg) {
+        throw ScriptRuntime.throwCustomError(cx, scope, constr, msg);
     }
 
 }
