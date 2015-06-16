46a73a9d6ffb163886378c3b166b6c8632b576a1
Gregory Brail <greg@apigee.com>
2015-06-16 16:47:26 -0700

    Add "readline" and "write" to console.

--- maven/maven-snapshot-deploy.sh
+++ maven/maven-snapshot-deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mvn deploy:deploy-file \
-  -Dfile=../build/rhino1_7R5pre/js.jar \
+  -Dfile=../build/rhino1.7.7/js.jar \
   -DpomFile=maven-pom.xml \
   -DrepositoryId=sonatype-nexus-snapshots \
   -Durl=https://oss.sonatype.org/content/repositories/snapshots/
