5022ae8ccc72e5547125cac59d9d763b023a5869
Ievgenii.Shepeliuk <I.Shepeliuk@external.oberthur.com>
2015-06-17 16:16:23 +0300

    Add flag to indiicate if pre ES5 behaviour needed when processing undefined / null as this in Function.prototype.call Function.prototype.apply

--- src/org/mozilla/javascript/ContextFactory.java
+++ src/org/mozilla/javascript/ContextFactory.java
@@ -282,6 +282,8 @@ public class ContextFactory
 
           case Context.FEATURE_V8_EXTENSIONS:
             return true;
+          case Context.FEATURE_OLD_UNDEF_NULL_THIS:
+              return cx.getLanguageVersion() <= Context.VERSION_1_7;
         }
         // It is a bug to call the method with unknown featureIndex
         throw new IllegalArgumentException(String.valueOf(featureIndex));
