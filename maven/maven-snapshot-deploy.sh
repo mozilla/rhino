0bd43139e53db425f3e665662986fa2141521a6a
Gregory Brail <greg@apigee.com>
2015-07-02 10:42:51 -0700

    Update maven deployment script.

--- maven/maven-snapshot-deploy.sh
+++ maven/maven-snapshot-deploy.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
+deployFile=`ls ../buildGradle/libs/rhino*.jar`
+
+if [ ! -f $deployFile ]
+then
+  echo "File cannot be found in $deployFile"
+  exit 2
+fi
+
 mvn deploy:deploy-file \
-  -Dfile=../build/rhino1.7.7/js.jar \
+  -Dfile=${deployFile} \
   -DpomFile=maven-pom.xml \
   -DrepositoryId=sonatype-nexus-snapshots \
   -Durl=https://oss.sonatype.org/content/repositories/snapshots/
