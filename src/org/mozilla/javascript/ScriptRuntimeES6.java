bb267efd240c6a0120ae8ceebe55871b76ae10e0
Evgeny Shepelyuk <eshepelyuk@gmail.com>
2015-09-02 08:10:33 +0300

    Better `undefined` detection

--- src/org/mozilla/javascript/ScriptRuntimeES6.java
+++ src/org/mozilla/javascript/ScriptRuntimeES6.java
@@ -9,7 +9,7 @@ package org.mozilla.javascript;
 public class ScriptRuntimeES6 {
 
     public static Scriptable requireObjectCoercible(Context cx, Scriptable val, IdFunctionObject idFuncObj) {
-        if (val == null || val == Undefined.SCRIPTABLE_UNDEFINED) {
+        if (val == null || Undefined.isUndefined(val)) {
             throw ScriptRuntime.typeError2("msg.called.null.or.undefined", idFuncObj.getTag(), idFuncObj.getFunctionName());
         }
         return val;
