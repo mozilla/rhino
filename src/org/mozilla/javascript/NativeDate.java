6b9b60b0d24030cf85747066d48f2c12757299e4
Gregory Brail <greg@apigee.com>
2016-01-15 11:20:16 -0800

    Replace YearFromTime with code from jsdate.cpp that does not result in such high CPU usage for extreme values.

--- src/org/mozilla/javascript/NativeDate.java
+++ src/org/mozilla/javascript/NativeDate.java
@@ -16,6 +16,9 @@ import java.util.TimeZone;
  * This class implements the Date native object.
  * See ECMA 15.9.
  * @author Mike McCabe
+ *
+ * Significant parts of this code are adapted from the venerable jsdate.cpp (also Mozilla):
+ * https://dxr.mozilla.org/mozilla-central/source/js/src/jsdate.cpp
  */
 final class NativeDate extends IdScriptableObject
 {
@@ -429,35 +432,25 @@ final class NativeDate extends IdScriptableObject
 
     private static int YearFromTime(double t)
     {
-        int lo = (int) Math.floor((t / msPerDay) / 366) + 1970;
-        int hi = (int) Math.floor((t / msPerDay) / 365) + 1970;
-        int mid;
-
-        /* above doesn't work for negative dates... */
-        if (hi < lo) {
-            int temp = lo;
-            lo = hi;
-            hi = temp;
+        if (Double.isInfinite(t) || Double.isNaN(t)) {
+            return 0;
         }
 
-        /* Use a simple binary search algorithm to find the right
-           year.  This seems like brute force... but the computation
-           of hi and lo years above lands within one year of the
-           correct answer for years within a thousand years of
-           1970; the loop below only requires six iterations
-           for year 270000. */
-        while (hi > lo) {
-            mid = (hi + lo) / 2;
-            if (TimeFromYear(mid) > t) {
-                hi = mid - 1;
-            } else {
-                lo = mid + 1;
-                if (TimeFromYear(lo) > t) {
-                    return mid;
-                }
-            }
+        double y = Math.floor(t / (msPerDay * 365.2425)) + 1970;
+        double t2 = TimeFromYear(y);
+
+        /*
+         * Adjust the year if the approximation was wrong.  Since the year was
+         * computed using the average number of ms per year, it will usually
+         * be wrong for dates within several hours of a year transition.
+         */
+        if (t2 > t) {
+            y--;
+        } else {
+            if (t2 + msPerDay * DaysInYear(y) <= t)
+                y++;
         }
-        return lo;
+        return (int)y;
     }
 
     private static double DayFromMonth(int m, int year)
@@ -471,6 +464,14 @@ final class NativeDate extends IdScriptableObject
         if (m >= 2 && IsLeapYear(year)) { ++day; }
 
         return day;
+    }
+
+    private static double DaysInYear(double year)
+    {
+        if (Double.isInfinite(year) || Double.isNaN(year)) {
+            return ScriptRuntime.NaN;
+        }
+        return IsLeapYear((int)year) ? 366.0 : 365.0;
     }
 
     private static int DaysInMonth(int year, int month)
