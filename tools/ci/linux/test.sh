86479fb0a9da4a639290ba61ca39fad8d84726b5
Gregory Brail <gregbrail@google.com>
2017-03-10 18:12:55 -0800

    Make test log format work with CI. Ensure submodule is checked out.

--- tools/ci/linux/test.sh
+++ tools/ci/linux/test.sh
@@ -2,4 +2,11 @@
 
 BUILDROOT=${BUILDROOT:-github/rhino}
 
+(cd $BUILDROOT; git submodule init; git submodule update)
 (cd $BUILDROOT; ./gradlew clean test)
+
+for n in ${BUILDROOT}/buildGradle/test-results/*.xml
+do
+  bn=`basename $n .xml`
+  mv $n ${BUILDROOT}/buildGradle/test-results/${bn}_sponge_log.xml
+done
