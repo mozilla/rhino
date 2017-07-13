03b2c8e2274b376abc1aafaabb30a818cd820d7b
Gregory Brail <gregbrail@google.com>
2017-07-13 14:46:34 -0700

    Fix CI test script to return correct exit status.

--- tools/ci/linux/test.sh
+++ tools/ci/linux/test.sh
@@ -4,9 +4,13 @@ BUILDROOT=${BUILDROOT:-github/rhino}
 
 (cd $BUILDROOT; git submodule init; git submodule update)
 (cd $BUILDROOT; ./gradlew clean test)
+testStatus=$?
 
 for n in ${BUILDROOT}/buildGradle/test-results/*.xml
 do
   bn=`basename $n .xml`
   mv $n ${BUILDROOT}/buildGradle/test-results/${bn}_sponge_log.xml
 done
+
+exit ${testStatus}
+
