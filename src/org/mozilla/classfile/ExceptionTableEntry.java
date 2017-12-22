51da82a4f71c7a2891857bde2728069a7c9b8834
Gregory Brail <gregbrail@google.com>
2017-12-22 11:00:29 -0800

    Add ability to insert invokeDynamic instructions.
    Cherry-pick code from @hns from many years ago that allows us
    to use invokeDynamic instructions in bytecode. However, do not
    use it yet.
    
    Now we will have the ability to experiment with this new bytecode.
    However, I have no working code yet that uses it AND results in
    any measurable performance benefit.
    
    Also, clean up ClassFileWriter to put top-level Java classes in
    separate .java files like we've been doing in Java since 1999 ;-)

---
+++ src/org/mozilla/classfile/ExceptionTableEntry.java
@@ -0,0 +1,24 @@
+/* -*- Mode: java; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.classfile;
+
+final class ExceptionTableEntry
+{
+  ExceptionTableEntry(int startLabel, int endLabel,
+      int handlerLabel, short catchType)
+  {
+    itsStartLabel = startLabel;
+    itsEndLabel = endLabel;
+    itsHandlerLabel = handlerLabel;
+    itsCatchType = catchType;
+  }
+
+  int itsStartLabel;
+  int itsEndLabel;
+  int itsHandlerLabel;
+  short itsCatchType;
+}
