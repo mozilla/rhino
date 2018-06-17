829c18fb2bbecf3ff4af887d224fea7b4cbf135e
Attila Szegedi <szegedia@gmail.com>
2018-06-17 16:45:26 +0200

    Add workarounds for #437 and #449

--- src/org/mozilla/javascript/EqualObjectGraphs.java
+++ src/org/mozilla/javascript/EqualObjectGraphs.java
@@ -16,6 +16,8 @@ import java.util.Set;
 import java.util.SortedMap;
 import java.util.TreeMap;
 import org.mozilla.javascript.debug.DebuggableObject;
+import org.mozilla.javascript.typedarrays.NativeArrayBuffer;
+import org.mozilla.javascript.typedarrays.NativeTypedArrayView;
 
 /**
  * An object that implements deep equality test of objects, including their 
@@ -298,7 +300,18 @@ final class EqualObjectGraphs  {
     private static Object[] getIds(final Scriptable s) {
         if (s instanceof ScriptableObject) {
             // Grabs symbols too
-            return ((ScriptableObject)s).getIds(true, true);
+            try {
+                return ((ScriptableObject)s).getIds(true, true);
+            } catch (IllegalArgumentException e) {
+                if (s instanceof NativeArrayBuffer) {
+                    // TODO: remove this once https://github.com/mozilla/rhino/issues/437 is fixed
+                    return new String[0];
+                } else if (s instanceof NativeTypedArrayView) {
+                    // TODO: remove this once https://github.com/mozilla/rhino/issues/449 is fixed
+                    return s.getIds();
+                }
+                throw e;
+            }
         } else if (s instanceof DebuggableObject) {
             return ((DebuggableObject)s).getAllIds();
         } else {
