cda902513dcd732cc1833e6321c4514dd0ab5e2f
Gregory Brail <gregbrail@google.com>
2019-05-13 15:15:30 -0700

    Make the "Sorting" helper class a proper singleton.
    I've been made aware of some security scanning tools that don't
    like that "Sorting" is all static. Fix this using a classic
    Singleton pattern.

--- src/org/mozilla/javascript/Sorting.java
+++ src/org/mozilla/javascript/Sorting.java
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 package org.mozilla.javascript;
 
 import java.util.Comparator;
@@ -5,12 +9,20 @@ import java.util.Comparator;
 public final class Sorting {
     private static final int SMALLSORT = 16;
 
-    public static void insertionSort(Object[] a, Comparator<Object> cmp)
+    private static final Sorting sorting = new Sorting();
+
+    private Sorting() {}
+
+    public static Sorting get() {
+        return sorting;
+    }
+
+    public void insertionSort(Object[] a, Comparator<Object> cmp)
     {
         insertionSort(a, 0, a.length - 1, cmp);
     }
 
-    public static void insertionSort(Object[] a, int start, int end, Comparator<Object> cmp)
+    private void insertionSort(Object[] a, int start, int end, Comparator<Object> cmp)
     {
         int i = start;
         while (i <= end) {
@@ -31,12 +43,12 @@ public final class Sorting {
     recursion is too deep, and then use insertion sort for the rest.
     This is the same basic algorithm used by the GNU Standard C++ library.
     */
-    public static void hybridSort(Object[] a, Comparator<Object> cmp)
+    public void hybridSort(Object[] a, Comparator<Object> cmp)
     {
         hybridSort(a, 0, a.length - 1, cmp, log2(a.length) * 2);
     }
 
-    private static void hybridSort(Object[] a, int start, int end, Comparator<Object> cmp, int maxdepth)
+    private void hybridSort(Object[] a, int start, int end, Comparator<Object> cmp, int maxdepth)
     {
         if (start < end) {
             if ((maxdepth == 0) || ((end - start) <= SMALLSORT)) {
@@ -55,7 +67,7 @@ public final class Sorting {
     Use the "median of three" method to determine which index to pivot on, and then
     separate the array into two halves based on the pivot.
     */
-    private static int partition(Object[] a, int start, int end, Comparator<Object> cmp) {
+    private int partition(Object[] a, int start, int end, Comparator<Object> cmp) {
         final int p = median(a, start, end, cmp);
         final Object pivot = a[p];
         a[p] = a[start];
@@ -85,14 +97,14 @@ public final class Sorting {
         return j;
     }
 
-    private static void swap(Object[] a, int l, int h)
+    private void swap(Object[] a, int l, int h)
     {
         final Object tmp = a[l];
         a[l] = a[h];
         a[h] = tmp;
     }
 
-    private static int log2(int n)
+    private int log2(int n)
     {
         return (int)(Math.log10(n) / Math.log10(2.0));
     }
@@ -101,7 +113,7 @@ public final class Sorting {
     Return the index of the median of three elements in the specified array range -- the
     first, the last, and the one in the middle.
     */
-    public static int median(final Object[] a, int start, int end, Comparator<Object> cmp)
+    public int median(final Object[] a, int start, int end, Comparator<Object> cmp)
     {
         final int m = start + ((end - start) / 2);
         int smallest = start;
