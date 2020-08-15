501c13489ed25e59a8aeb271d3136020615fc9aa
Ronald Brill <rbri@rbri.de>
2020-08-15 13:00:42 +0200

    NativeArrayBuffer slice() length is 2

--- src/org/mozilla/javascript/typedarrays/NativeArrayBuffer.java
+++ src/org/mozilla/javascript/typedarrays/NativeArrayBuffer.java
@@ -163,7 +163,7 @@ public class NativeArrayBuffer
         int arity;
         switch (id) {
         case Id_constructor:            arity = 1; s = "constructor"; break;
-        case Id_slice:                  arity = 1; s = "slice"; break;
+        case Id_slice:                  arity = 2; s = "slice"; break;
         default: throw new IllegalArgumentException(String.valueOf(id));
         }
         initPrototypeMethod(CLASS_NAME, id, s, arity);
