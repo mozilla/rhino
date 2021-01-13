b329ef187626596ed2ed61d7320d6999f5e772d7
Ronald Brill <rbri@rbri.de>
2021-01-13 11:52:05 +0100

    more detailed tests

--- testsrc/jstests/es6/prototype-functions.js
+++ testsrc/jstests/es6/prototype-functions.js
@@ -5,17 +5,32 @@ Check for changing the prototype of functions.
 load('testsrc/assert.js');
 
 // new prototype is an object
-var protoObj = { foo: function (n) { return 'foo'; } }
-var target = function () {}
+var protoObj = { foo: function (n) { return 'foo'; } };
+var target = function () {};
+var res;
 
-target.__proto__ = protoObj;
+res = (target.__proto__ = protoObj);
 assertEquals(protoObj, target.__proto__);
 assertEquals('foo', target.foo());
+assertEquals(protoObj, res)
+
 
 // new prototype is a function
 protoFunc = function (n) { }
+target = function () {};
 
-target.__proto__ = protoFunc;
-assertEquals(target.__proto__, protoFunc);
+res = (target.__proto__ = protoFunc);
+assertEquals(protoFunc, target.__proto__);
+assertEquals(protoFunc, res)
+
+
+// new prototype is a number
+target = function () {};
+var proto = target.__proto__
+
+res = (target.__proto__ = 42);
+assertEquals(proto, target.__proto__);
+assertTrue(42 == res)
+
 
 "success";
