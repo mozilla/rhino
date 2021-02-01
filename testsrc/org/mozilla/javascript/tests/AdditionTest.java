a088b11d0c1b1d388e7ef6ceb52bb686429619f8
tuchida <dc578av_adle@yahoo.co.jp>
2021-02-01 20:58:45 +0900

    ref. #832 add addition testcase

---
+++ testsrc/org/mozilla/javascript/tests/AdditionTest.java
@@ -0,0 +1,9 @@
+package org.mozilla.javascript.tests;
+
+import org.mozilla.javascript.drivers.RhinoTest;
+import org.mozilla.javascript.drivers.ScriptTestsBase;
+
+@RhinoTest("testsrc/jstests/addition.js")
+public class AdditionTest extends ScriptTestsBase
+{
+}
