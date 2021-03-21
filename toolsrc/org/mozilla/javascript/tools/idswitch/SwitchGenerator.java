64fb2230004af05324c6c2090605194058e5e6b9
Ronald Brill <rbri@rbri.de>
2021-03-21 10:38:09 +0100

    simplify code

--- toolsrc/org/mozilla/javascript/tools/idswitch/SwitchGenerator.java
+++ toolsrc/org/mozilla/javascript/tools/idswitch/SwitchGenerator.java
@@ -9,19 +9,6 @@ import org.mozilla.javascript.tools.ToolErrorReporter;
 
 public class SwitchGenerator {
 
-    String v_switch_label = "L0";
-    String v_label = "L";
-    String v_s = "s";
-    String v_c = "c";
-    String v_guess = "X";
-    String v_id = "id";
-    String v_length_suffix = "_length";
-
-    int use_if_threshold = 3;
-    int char_tail_test_threshold = 2;
-
-    private IdValuePair[] pairs;
-
     private CodePrinter P;
     private ToolErrorReporter R;
     private String source_file;
@@ -35,38 +22,22 @@ public class SwitchGenerator {
     public String getSourceFileName() { return source_file; }
     public void setSourceFileName(String value) { source_file = value; }
 
-    public void generateSwitch(String[] pairs, String default_value) {
-        int N = pairs.length / 2;
-        IdValuePair[] id_pairs = new IdValuePair[N];
-        for (int i = 0; i != N; ++i) {
-            id_pairs[i] = new IdValuePair(pairs[2 * i], pairs[2 * i + 1]);
-        }
-        generateSwitch(id_pairs, default_value);
+    public void generateSwitch(IdValuePair[] pairs) {
+        if (pairs.length == 0) { return; }
 
-    }
-
-    public void generateSwitch(IdValuePair[] pairs, String default_value) {
-        int begin = 0;
-        int end = pairs.length;
-        if (begin == end) { return; }
-        this.pairs = pairs;
-
-        generate_body(begin, end, 2);
-    }
-
-    private void generate_body(int begin, int end, int indent_level) {
+        int indent_level = 2;
         P.line(indent_level, "switch (s) {");
 
-        for (int i = begin; i < end; i++) {
+        for (int i = 0; i < pairs.length; i++) {
             P.indent(indent_level);
             P.p("case \"");
-            P.p(this.pairs[i].id);
+            P.p(pairs[i].id);
             P.p("\":");
             P.nl();
 
             P.indent(++indent_level);
             P.p("id = ");
-            P.p(this.pairs[i].value);
+            P.p(pairs[i].value);
             P.p(";");
             P.nl();
 
