9cc8a1b861197f9fd7d604d6274f13c2fa887bcb
mulingLHY <46077813+mulingLHY@users.noreply.github.com>
2021-04-09 06:52:58 +0800

    modify the code style
    modify the code style to match the original project

--- src/org/mozilla/javascript/JavaAdapter.java
+++ src/org/mozilla/javascript/JavaAdapter.java
@@ -500,9 +500,7 @@ public final class JavaAdapter implements IdFunctionCall
     private static void appendOverridableMethods(Class<?> c,
             ArrayList<Method> list, HashSet<String> skip)
     {
-        Method[] methods = null;
-        if(c.isInterface())methods=c.getMethods();
-        else methods=c.getDeclaredMethods();
+        Method[] methods=c.isInterface() ? methods=c.getMethods() : c.getDeclaredMethods();
         
         for (int i = 0; i < methods.length; i++) {
             String methodKey = methods[i].getName() +
