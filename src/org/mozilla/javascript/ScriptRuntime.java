7bf362880901b0972284008ab15ff69a8026696b
tuchida <dc578av_adle@yahoo.co.jp>
2021-04-16 22:04:39 +0900

    unary minus operator for BigInt

--- src/org/mozilla/javascript/ScriptRuntime.java
+++ src/org/mozilla/javascript/ScriptRuntime.java
@@ -818,6 +818,13 @@ public class ScriptRuntime {
         }
     }
 
+    public static Number toNumeric(Object val) {
+        if (val instanceof BigInteger) {
+            return (BigInteger) val;
+        }
+        return toNumber(val);
+    }
+
     /**
      * Helper function for builtin objects that use the varargs form. ECMA function formal arguments
      * are undefined if not supplied; this function pads the argument array out to the expected
@@ -3034,6 +3041,13 @@ public class ScriptRuntime {
             return value;
         }
         return result;
+    }
+
+    public static Number negate(Number val) {
+        if (val instanceof BigInteger) {
+            return ((BigInteger) val).negate();
+        }
+        return -val.doubleValue();
     }
 
     public static Object toPrimitive(Object val) {
