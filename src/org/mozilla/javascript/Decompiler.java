7dec7b738a45dd0adf212062498d061bc0b48dbe
Paul Bakker <paul@theorangedots.com>
2021-06-01 14:14:22 +0200

    Add Decompile test

--- src/org/mozilla/javascript/Decompiler.java
+++ src/org/mozilla/javascript/Decompiler.java
@@ -746,18 +746,6 @@ public class Decompiler {
                     result.append(" ** ");
                     break;
 
-                case Token.TEMPLATE_LITERAL:
-                    result.append("`");
-                    break;
-
-                case Token.TEMPLATE_LITERAL_SUBST:
-                    result.append("${");
-                    break;
-
-                case Token.TEMPLATE_CHARS:
-                    i = printSourceString(source, i + 1, false, result);
-                    continue;
-
                 case Token.COLONCOLON:
                     result.append("::");
                     break;
@@ -781,6 +769,18 @@ public class Decompiler {
                 case Token.ARROW:
                     result.append(" => ");
                     break;
+
+                case Token.TEMPLATE_LITERAL:
+                    result.append("`");
+                    break;
+
+                case Token.TEMPLATE_LITERAL_SUBST:
+                    result.append("${");
+                    break;
+
+                case Token.TEMPLATE_CHARS:
+                    i = printSourceString(source, i + 1, false, result);
+                    continue;
 
                 default:
                     // If we don't know how to decompile it, raise an exception.
