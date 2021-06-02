c915eea3ccd681169c11bd89a39844b1e7a5ab37
Roland Praml <roland.praml@foconis.de>
2021-06-02 07:48:13 +0200

    Spotless NativeMap

--- src/org/mozilla/javascript/NativeMap.java
+++ src/org/mozilla/javascript/NativeMap.java
@@ -190,9 +190,13 @@ public class NativeMap extends IdScriptableObject {
         Scriptable proto = ScriptableObject.getClassPrototype(scope, map.getClassName());
         final Callable set = ScriptRuntime.getPropFunctionAndThis(proto, "set", cx, scope);
 
-        ScriptRuntime.loadFromIterable(cx, scope, arg1, (key, value) -> {
-            set.call(cx, scope, map, new Object[] { key, value });
-        });
+        ScriptRuntime.loadFromIterable(
+                cx,
+                scope,
+                arg1,
+                (key, value) -> {
+                    set.call(cx, scope, map, new Object[] {key, value});
+                });
     }
 
     private static NativeMap realThis(Scriptable thisObj, IdFunctionObject f) {
