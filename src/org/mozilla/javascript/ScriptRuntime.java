aa0afca94f3652ac9d9a95bc374b90e9f75ffc54
Roland Praml <roland.praml@foconis.de>
2021-09-07 08:26:25 +0200

    reverted unexplainable change.

--- src/org/mozilla/javascript/ScriptRuntime.java
+++ src/org/mozilla/javascript/ScriptRuntime.java
@@ -3441,12 +3441,6 @@ public class ScriptRuntime {
 
     /** Implement "SameValueZero" from ECMA 7.2.9 */
     public static boolean sameZero(Object x, Object y) {
-        if (x instanceof Wrapper) {
-            x = ((Wrapper) x).unwrap();
-        }
-        if (y instanceof Wrapper) {
-            y = ((Wrapper) y).unwrap();
-        }
         if (!typeof(x).equals(typeof(y))) {
             return false;
         }
@@ -3606,12 +3600,6 @@ public class ScriptRuntime {
     }
 
     public static boolean shallowEq(Object x, Object y) {
-        if (x instanceof Wrapper) {
-            x = ((Wrapper) x).unwrap();
-        }
-        if (y instanceof Wrapper) {
-            y = ((Wrapper) y).unwrap();
-        }
         if (x == y) {
             if (!(x instanceof Number)) {
                 return true;
