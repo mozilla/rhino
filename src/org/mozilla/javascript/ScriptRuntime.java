4c02bc2cb799b4d519bebd6f690f9b4eca0ff8e3
Roland Praml <roland.praml@foconis.de>
2021-11-08 16:59:20 +0100

    Changed unwrap to fix Array.indexOf / lastIndexOf and Array.includes

--- src/org/mozilla/javascript/ScriptRuntime.java
+++ src/org/mozilla/javascript/ScriptRuntime.java
@@ -3446,6 +3446,12 @@ public class ScriptRuntime {
 
     /** Implement "SameValueZero" from ECMA 7.2.9 */
     public static boolean sameZero(Object x, Object y) {
+        if (x instanceof Wrapper) {
+            x = ((Wrapper) x).unwrap();
+        }
+        if (y instanceof Wrapper) {
+            y = ((Wrapper) y).unwrap();
+        }
         if (!typeof(x).equals(typeof(y))) {
             return false;
         }
@@ -3605,6 +3611,13 @@ public class ScriptRuntime {
     }
 
     public static boolean shallowEq(Object x, Object y) {
+        if (x instanceof Wrapper) {
+            x = ((Wrapper) x).unwrap();
+        }
+        if (y instanceof Wrapper) {
+            y = ((Wrapper) y).unwrap();
+        }
+
         if (x == y) {
             if (!(x instanceof Number)) {
                 return true;
@@ -3635,9 +3648,6 @@ public class ScriptRuntime {
                 return x.equals(y);
             }
         } else if (x instanceof Scriptable) {
-            if (x instanceof Wrapper && y instanceof Wrapper) {
-                return ((Wrapper) x).unwrap() == ((Wrapper) y).unwrap();
-            }
             if (x instanceof Delegator) {
                 x = ((Delegator) x).getDelegee();
                 if (y instanceof Delegator) {
@@ -3646,8 +3656,7 @@ public class ScriptRuntime {
                 if (x == y) {
                     return true;
                 }
-            }
-            if (y instanceof Delegator && ((Delegator) y).getDelegee() == x) {
+            } else if (y instanceof Delegator && ((Delegator) y).getDelegee() == x) {
                 return true;
             }
         } else {
