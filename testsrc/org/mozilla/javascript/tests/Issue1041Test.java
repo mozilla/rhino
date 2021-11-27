df023ed121cde630434e1c3040e9ea47f2ec1217
MaxisTekfield <max.tekfield@huawei.com>
2021-11-27 20:01:54 +0800

    Fix issue #1041. Handles {0} quantifier with max of zero correctly.

---
+++ testsrc/org/mozilla/javascript/tests/Issue1041Test.java
@@ -0,0 +1,28 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.javascript.tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.ScriptableObject;
+
+public class Issue1041Test {
+    @Test
+    public void testQuantifierWithMax0() {
+        Context cx = Context.enter();
+        try {
+            ScriptableObject scope = cx.initStandardObjects();
+            Boolean result =
+                    (Boolean) cx.evaluateString(scope, "/ab{0}c/.test('abc')", "<eval>", 1, null);
+            Assert.assertFalse(result);
+
+            result = (Boolean) cx.evaluateString(scope, "/ab{0}c/.test('ac')", "<eval>", 1, null);
+            Assert.assertTrue(result);
+        } finally {
+            Context.exit();
+        }
+    }
+}
