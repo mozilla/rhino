2b9633a4b1f859d0d594e3167da5eba1ad7be1f0
Ronald Brill <rbri@rbri.de>
2021-12-20 22:35:52 +0100

    fiy toLocaleLowerCase and toLocaleUpperCase when called with one paramter                     {

--- testsrc/org/mozilla/javascript/tests/NativeStringTest.java
+++ testsrc/org/mozilla/javascript/tests/NativeStringTest.java
@@ -5,25 +5,32 @@
 /** */
 package org.mozilla.javascript.tests;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Locale;
+import org.junit.Test;
 import org.mozilla.javascript.Scriptable;
 
-/** @author Marc Guillemot */
-public class NativeStringTest extends TestCase {
+/**
+ * @author Marc Guillemot
+ * @author Ronald Brill
+ */
+public class NativeStringTest {
 
     /**
      * Test for bug #492359 https://bugzilla.mozilla.org/show_bug.cgi?id=492359 Calling generic
      * String or Array functions without arguments was causing ArrayIndexOutOfBoundsException in
      * 1.7R2
      */
-    public void testtoLowerCaseApply() {
+    @Test
+    public void toLowerCaseApply() {
         assertEvaluates("hello", "var x = String.toLowerCase; x.apply('HELLO')");
         assertEvaluates(
                 "hello",
                 "String.toLowerCase('HELLO')"); // first patch proposed to #492359 was breaking this
     }
 
-    private void assertEvaluates(final Object expected, final String source) {
+    private static void assertEvaluates(final Object expected, final String source) {
         Utils.runWithAllOptimizationLevels(
                 cx -> {
                     final Scriptable scope = cx.initStandardObjects();
@@ -31,5 +38,71 @@ public class NativeStringTest extends TestCase {
                     assertEquals(expected, rep);
                     return null;
                 });
+    }
+
+    @Test
+    public void toLocaleLowerCase() {
+        String js = "'\\u0130'.toLocaleLowerCase()";
+
+        Utils.runWithAllOptimizationLevels(
+                cx -> {
+                    final Scriptable scope = cx.initStandardObjects();
+                    cx.setLocale(new Locale("en"));
+
+                    final Object rep = cx.evaluateString(scope, js, "test.js", 0, null);
+                    assertEquals("\u0069\u0307", rep);
+                    return null;
+                });
+
+        Utils.runWithAllOptimizationLevels(
+                cx -> {
+                    final Scriptable scope = cx.initStandardObjects();
+                    cx.setLocale(new Locale("tr"));
+
+                    final Object rep = cx.evaluateString(scope, js, "test.js", 0, null);
+                    assertEquals("\u0069", rep);
+                    return null;
+                });
+    }
+
+    @Test
+    public void toLocaleLowerCaseParam() {
+        assertEvaluates("\u0069\u0307", "'\\u0130'.toLocaleLowerCase('en')");
+        assertEvaluates("\u0069", "'\\u0130'.toLocaleLowerCase('tr')");
+
+        assertEvaluates("\u0069\u0307", "'\\u0130'.toLocaleLowerCase('Absurdistan')");
+    }
+
+    @Test
+    public void toLocaleUpperCase() {
+        String js = "'\\u0069'.toLocaleUpperCase()";
+
+        Utils.runWithAllOptimizationLevels(
+                cx -> {
+                    final Scriptable scope = cx.initStandardObjects();
+                    cx.setLocale(new Locale("en"));
+
+                    final Object rep = cx.evaluateString(scope, js, "test.js", 0, null);
+                    assertEquals("\u0049", rep);
+                    return null;
+                });
+
+        Utils.runWithAllOptimizationLevels(
+                cx -> {
+                    final Scriptable scope = cx.initStandardObjects();
+                    cx.setLocale(new Locale("tr"));
+
+                    final Object rep = cx.evaluateString(scope, js, "test.js", 0, null);
+                    assertEquals("\u0130", rep);
+                    return null;
+                });
+    }
+
+    @Test
+    public void toLocaleUpperCaseParam() {
+        assertEvaluates("\u0049", "'\\u0069'.toLocaleUpperCase('en')");
+        assertEvaluates("\u0130", "'\\u0069'.toLocaleUpperCase('tr')");
+
+        assertEvaluates("\u0049", "'\\u0069'.toLocaleUpperCase('Absurdistan')");
     }
 }
