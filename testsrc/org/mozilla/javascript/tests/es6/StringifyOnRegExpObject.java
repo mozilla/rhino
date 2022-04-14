29e0f08c6fc5ff35d39498fd9e84d64f65367e7a
Ronald Brill <rbri@rbri.de>
2022-04-14 16:54:01 +0200

    move es6 tests to the right place

--- testsrc/org/mozilla/javascript/tests/es6/StringifyOnRegExpObject.java
+++ testsrc/org/mozilla/javascript/tests/es6/StringifyOnRegExpObject.java
@@ -2,13 +2,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package org.mozilla.javascript.tests;
+package org.mozilla.javascript.tests.es6;
 
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.drivers.LanguageVersion;
 import org.mozilla.javascript.drivers.RhinoTest;
 import org.mozilla.javascript.drivers.ScriptTestsBase;
 
-@RhinoTest("testsrc/jstests/stringify-on-regexp-object.js")
+@RhinoTest("testsrc/jstests/es6/stringify-on-regexp-object.js")
 @LanguageVersion(Context.VERSION_ES6)
 public class StringifyOnRegExpObject extends ScriptTestsBase {}
--- testsrc/org/mozilla/javascript/tests/es6/StringifyXmlTest.java
+++ testsrc/org/mozilla/javascript/tests/es6/StringifyXmlTest.java
@@ -1,10 +1,10 @@
-package org.mozilla.javascript.tests;
+package org.mozilla.javascript.tests.es6;
 
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.drivers.LanguageVersion;
 import org.mozilla.javascript.drivers.RhinoTest;
 import org.mozilla.javascript.drivers.ScriptTestsBase;
 
-@RhinoTest("testsrc/jstests/stringify-java-objects.js")
+@RhinoTest("testsrc/jstests/es6/stringify-xml.js")
 @LanguageVersion(Context.VERSION_ES6)
-public class StringifyJavaObjectsTest extends ScriptTestsBase {}
+public class StringifyXmlTest extends ScriptTestsBase {}
