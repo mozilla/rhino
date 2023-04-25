6387bac4281ba1c4a8050cd055071615593d214c
RBRi <rbri@rbri.de>
2023-04-25 23:51:14 +0200

    More consistent test optimization levels (#1317)
    Ensure that more tests check multiple optimization levels and that they do it in a consistent way.
    
    * use Utils.runWithAllOptimizationLevels for the ES6 tests
    * use Utils.runWithAllOptimizationLevels for the ES2022 tests
    * support the system property TEST_OPTLEVEL for more tests
    use Utils.runWithAllOptimizationLevels at more places
    * spotless

--- testsrc/org/mozilla/javascript/tests/es6/PromiseTest.java
+++ testsrc/org/mozilla/javascript/tests/es6/PromiseTest.java
@@ -9,62 +9,62 @@ package org.mozilla.javascript.tests.es6;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.ScriptableObject;
 import org.mozilla.javascript.TopLevel;
+import org.mozilla.javascript.tests.Utils;
 
 public class PromiseTest {
 
-    private Context cx;
-    private ScriptableObject scope;
-
-    @Before
-    public void setUp() {
-        cx = Context.enter();
-        cx.setLanguageVersion(Context.VERSION_ES6);
-        scope = cx.initSafeStandardObjects(new TopLevel(), false);
-    }
-
-    @After
-    public void tearDown() {
-        Context.exit();
-    }
-
     @Test
     public void ctorCallableThis() {
-        Object result =
-                cx.evaluateString(
-                        scope,
-                        "  var r = '';"
-                                + "  var p = new Promise(function(resolve, reject) {\n"
-                                + "      r += this;\n"
-                                + "    });\n"
-                                + "  r += ' done';\n"
-                                + "  r;",
-                        "test",
-                        1,
-                        null);
-        assertEquals("[object global] done", result);
+        Utils.runWithAllOptimizationLevels(
+                cx -> {
+                    cx.setLanguageVersion(Context.VERSION_ES6);
+                    ScriptableObject scope = cx.initStandardObjects(new TopLevel(), false);
+
+                    Object result =
+                            cx.evaluateString(
+                                    scope,
+                                    "  var r = '';"
+                                            + "  var p = new Promise(function(resolve, reject) {\n"
+                                            + "      r += this;\n"
+                                            + "    });\n"
+                                            + "  r += ' done';\n"
+                                            + "  r;",
+                                    "test",
+                                    1,
+                                    null);
+                    assertEquals("[object global] done", result);
+
+                    return null;
+                });
     }
 
     @Test
     public void ctorCallableThisStrict() {
-        Object result =
-                cx.evaluateString(
-                        scope,
-                        "'use strict';"
-                                + "  var r = '';"
-                                + "  var p = new Promise(function(resolve, reject) {\n"
-                                + "      r += this === undefined;\n"
-                                + "    });\n"
-                                + "  r += ' done';\n"
-                                + "  r;",
-                        "test",
-                        1,
-                        null);
-        assertEquals("true done", result);
+        Utils.runWithAllOptimizationLevels(
+                cx -> {
+                    cx.setLanguageVersion(Context.VERSION_ES6);
+                    ScriptableObject scope = cx.initStandardObjects();
+
+                    Object result =
+                            cx.evaluateString(
+                                    scope,
+                                    "'use strict';"
+                                            + "  var r = '';"
+                                            + "  var p = new Promise(function(resolve, reject) {\n"
+                                            + "      r += this === undefined;\n"
+                                            + "    });\n"
+                                            + "  r += ' done';\n"
+                                            + "  r;",
+                                    "test",
+                                    1,
+                                    null);
+                    assertEquals("true done", result);
+
+                    return null;
+                });
     }
 }
