4f0c81e7517fbcb8dd62be87895cdf1c97e94e74
Ronald Brill <rbri@rbri.de>
2023-05-31 13:09:12 +0200

    some code cleanup

--- testsrc/org/mozilla/javascript/tests/es2019/ArrayFlatTest.java
+++ testsrc/org/mozilla/javascript/tests/es2019/ArrayFlatTest.java
@@ -2,13 +2,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package org.mozilla.javascript.tests.harmony;
+package org.mozilla.javascript.tests.es2019;
 
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.drivers.LanguageVersion;
 import org.mozilla.javascript.drivers.RhinoTest;
 import org.mozilla.javascript.drivers.ScriptTestsBase;
 
-@RhinoTest("testsrc/jstests/harmony/array-flat.js")
+@RhinoTest("testsrc/jstests/es2019/array-flat.js")
 @LanguageVersion(Context.VERSION_ES6)
 public class ArrayFlatTest extends ScriptTestsBase {}
