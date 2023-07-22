894268cfc17cfd4328846c6aed4a51e5ff68c74c
noga <nabarun.mondal@gmail.com>
2023-07-22 15:05:05 +0530

    Revert "Breaking the build by adding proper test for the print() statement."
    This reverts commit 109ab9b6324c2acb8b8d94437061e01cd94db325.

--- testsrc/org/mozilla/javascript/tests/scriptengine/BuiltinsTest.java
+++ testsrc/org/mozilla/javascript/tests/scriptengine/BuiltinsTest.java
@@ -2,18 +2,12 @@ package org.mozilla.javascript.tests.scriptengine;
 
 import static org.junit.Assert.*;
 
-import java.io.PrintStream;
 import java.io.StringWriter;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 import javax.script.SimpleScriptContext;
-
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -37,24 +31,8 @@ public class BuiltinsTest {
     }
 
     @Test
-    public void printStdout() throws Exception {
-        // only way to test these are redirections and then prayers
-        // this was not true earlier
-        final String content = "Hello, World!";
-        final String tmpFile = "out.txt" ;
-        final Path filePath = Paths.get(tmpFile);
-        if ( Files.exists( filePath )){
-            Files.delete(filePath);
-        }
-        PrintStream o = new PrintStream(tmpFile);
-        PrintStream console = System.out;
-        System.setOut(o);
-        engine.eval("print('" + content + "');");
-        System.setOut(console);
-        Assert.assertTrue( Files.exists(filePath));
-        // now read the content of the file ?
-        String writtenContent = new String(Files. readAllBytes(filePath));
-        Assert.assertEquals( content + "\n",  writtenContent);
+    public void printStdout() throws ScriptException {
+        engine.eval("print('Hello, World!');");
     }
 
     @Test
