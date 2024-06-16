25aaaf61fdc478eb5f5dbedeaa6e9a2ac8a0a28e
Greg Brail <gbrail@users.noreply.github.com>
2024-06-16 11:39:32 -0700

    Rename the main module and all-in-one JAR
    The main module is now named "rhino", and exports the
    "org.mozilla.rhino" package.
    
    The "rhino-all" module builds the "rhino-all.jar", which contains all
    the modules except the ScriptEngine (like the old "rhino.jar") and is
    sufficient to run Rhino using "java -jar".

--- rhino-xml/src/main/java/module-info.java
+++ rhino-xml/src/main/java/module-info.java
@@ -1,6 +1,6 @@
 module org.mozilla.javascript.xml {
     exports org.mozilla.javascript.xmlimpl;
 
-    requires org.mozilla.rhino.runtime;
+    requires org.mozilla.rhino;
     requires java.xml;
 }
