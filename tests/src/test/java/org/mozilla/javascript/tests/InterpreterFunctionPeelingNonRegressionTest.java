mozilla/rhino

commit 955af1565bd440b4954a7cbd3db4278b669302e7

More idiomatic loop, without continues and unconditional break at bottom.

rhino/src/main/java/org/mozilla/javascript/Interpreter.java
@@ -1774,23 +1774,21 @@ public final class Interpreter extends Icode implements Evaluator {
                                         calleeScope =
                                                 ScriptableObject.getTopLevelScope(frame.scope);
                                     }
-                                    // Iteratively peel known function types that can be reduced:
-                                    // arrows, lambdas, bound functions, call/apply, and
-                                    // no-such-method-handler in order to make a best-effort to keep
-                                    // them in this interpreter loop so continuations keep working.
-                                    // The loop initializer and condition are formulated so that
-                                    // they short-circuit the loop if the function is already an
-                                    // interpreted function, which should be the majority of cases.
+                                    // Iteratively reduce known function types: arrows, lambdas,
+                                    // bound functions, call/apply, and no-such-method-handler in
+                                    // order to make a best-effort to keep them in this interpreter
+                                    // loop so continuations keep working. The loop initializer and
+                                    // condition are formulated so that they short-circuit the loop
+                                    // if the function is already an interpreted function, which
+                                    // should be the majority of cases.
                                     for (boolean notInt = !(fun instanceof InterpretedFunction);
                                             notInt; ) {
                                         if (fun instanceof ArrowFunction) {
                                             ArrowFunction afun = (ArrowFunction) fun;
                                             fun = afun.getTargetFunction();
                                             funThisObj = afun.getCallThis(cx);
-                                            continue;
                                         } else if (fun instanceof LambdaFunction) {
                                             fun = ((LambdaFunction) fun).getTarget();
-                                            continue;
                                         } else if (fun instanceof BoundFunction) {
                                             BoundFunction bfun = (BoundFunction) fun;
                                             fun = bfun.getTargetFunction();
@@ -1818,7 +1816,6 @@ public final class Interpreter extends Icode implements Evaluator {
                                                         boundArgs, 0, stack, stackTop + 2, blen);
                                                 indexReg += blen;
                                             }
-                                            continue;
                                         } else if (fun instanceof IdFunctionObject) {
                                             IdFunctionObject ifun = (IdFunctionObject) fun;
                                             // Bug 405654 -- make the best effort to keep
@@ -1878,7 +1875,10 @@ public final class Interpreter extends Icode implements Evaluator {
                                                         indexReg--;
                                                     }
                                                 }
-                                                continue;
+                                            } else {
+                                                // Some other IdFunctionObject we don't know how to
+                                                // reduce.
+                                                break;
                                             }
                                         } else if (fun instanceof NoSuchMethodShim) {
                                             NoSuchMethodShim nsmfun = (NoSuchMethodShim) fun;
@@ -1895,15 +1895,13 @@ public final class Interpreter extends Icode implements Evaluator {
                                             stack[stackTop + 3] =
                                                     cx.newArray(calleeScope, elements);
                                             indexReg = 2;
-                                            continue;
                                         } else if (fun == null) {
                                             throw ScriptRuntime.notFunctionError(null, null);
+                                        } else {
+                                            // Current function is something that we can't reduce
+                                            // further.
+                                            break;
                                         }
-
-                                        // We reached here without any of the continue statements
-                                        // triggering. Current function is something that we can't
-                                        // peel back further.
-                                        break;
                                     }
 
                                     if (fun instanceof InterpretedFunction) {

commit c1575189e461bce3149ada61535685cd3a87d015

Fixed a bug with interpreter peeling introduced in #1510

rhino/src/main/java/org/mozilla/javascript/Interpreter.java
@@ -1784,6 +1784,8 @@ public final class Interpreter extends Icode implements Evaluator {
                                             ArrowFunction afun = (ArrowFunction) fun;
                                             fun = afun.getTargetFunction();
                                             funThisObj = afun.getCallThis(cx);
+                                        } else if (fun instanceof LambdaConstructor) {
+                                            break;
                                         } else if (fun instanceof LambdaFunction) {
                                             fun = ((LambdaFunction) fun).getTarget();
                                         } else if (fun instanceof BoundFunction) {

tests/src/test/java/org/mozilla/javascript/tests/InterpreterFunctionPeelingNonRegressionTest.java
@@ -1,0 +1,86 @@
+package org.mozilla.javascript.tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.mozilla.javascript.LambdaConstructor.CONSTRUCTOR_FUNCTION;
+
+import org.junit.Test;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.LambdaConstructor;
+import org.mozilla.javascript.LambdaFunction;
+import org.mozilla.javascript.ScriptRuntime;
+import org.mozilla.javascript.Scriptable;
+import org.mozilla.javascript.ScriptableObject;
+
+public class InterpreterFunctionPeelingNonRegressionTest {
+    @Test
+    public void testLambdaFunction() {
+        try (var cx = Context.enter()) {
+            cx.setOptimizationLevel(-1);
+            Scriptable scope = cx.initStandardObjects();
+
+            LambdaFunction makePerson =
+                    new LambdaFunction(
+                            scope,
+                            "makePerson",
+                            1,
+                            (cx1, scope1, thisObj, args) ->
+                                    new Person(ScriptRuntime.toString(args[0])));
+            scope.put("makePerson", scope, makePerson);
+
+            String source =
+                    "function testLambdaFunction() {\n"
+                            + "   return makePerson('Andrea');\n"
+                            + "}\n"
+                            + "testLambdaFunction().name";
+            Object value = cx.evaluateString(scope, source, "test", 1, null);
+            assertEquals("Andrea", value);
+        }
+    }
+
+    @Test
+    public void testLambdaConstructor() {
+        try (var cx = Context.enter()) {
+            cx.setOptimizationLevel(-1);
+            Scriptable scope = cx.initStandardObjects();
+
+            LambdaConstructor personCtor =
+                    new LambdaConstructor(
+                            scope,
+                            "Person",
+                            1,
+                            CONSTRUCTOR_FUNCTION,
+                            (cx1, scope1, args) -> new Person(ScriptRuntime.toString(args[0])));
+            scope.put("Person", scope, personCtor);
+
+            String source =
+                    "function testLambdaConstructor() {\n"
+                            + "  return Person('Andrea');\n"
+                            + "}\n"
+                            + "testLambdaConstructor().name";
+            Object value = cx.evaluateString(scope, source, "test", 1, null);
+            assertEquals("Andrea", value);
+        }
+    }
+
+    static class Person extends ScriptableObject {
+        private final String name;
+
+        public Person(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String getClassName() {
+            return "Person";
+        }
+
+        @Override
+        public Object get(String name, Scriptable start) {
+            if (name.equals("name")) {
+                return this.name;
+            } else {
+                return super.get(name, start);
+            }
+        }
+    }
+}