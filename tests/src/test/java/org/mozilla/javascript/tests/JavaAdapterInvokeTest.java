1c765271d237f9808d77fe533999dee9074173f2
Roland Praml <roland.praml@foconis.de>
2024-08-22 08:13:35 +0200

    Refactored test

--- tests/src/test/java/org/mozilla/javascript/tests/JavaAdapterInvokeTest.java
+++ tests/src/test/java/org/mozilla/javascript/tests/JavaAdapterInvokeTest.java
@@ -1,27 +1,10 @@
 package org.mozilla.javascript.tests;
 
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import org.mozilla.javascript.Context;
 import org.mozilla.javascript.Scriptable;
 
 public class JavaAdapterInvokeTest {
-    Context cx = null;
-    Scriptable topScope = null;
-
-    @Before
-    public void enterContext() {
-        cx = Context.enter();
-        cx.setOptimizationLevel(-1);
-        topScope = cx.initStandardObjects();
-    }
-
-    @After
-    public void exitContext() {
-        Context.exit();
-    }
 
     public interface AdapterInterface {
         int m1(int i);
@@ -41,8 +24,15 @@ public class JavaAdapterInvokeTest {
         }
     }
 
+    /**
+     * This test creates a new {@link AdapterClass} and passes a javascript object with two
+     * functions (m1,m2) to the constructor.
+     *
+     * <p>It is expected, that rhino creates an {@link AdapterInterface}, that delegates the calls
+     * to m1/m2 back to the javascript implementation.
+     */
     @Test
-    public void testInvoke() throws NoSuchMethodException {
+    public void testInvoke() {
         String testCode =
                 "'use strict'\n"
                         + "var impl = {"
@@ -53,13 +43,18 @@ public class JavaAdapterInvokeTest {
                         + AdapterClass.class.getName()
                         + "(impl)\n"
                         + "adapter.doIt(42)";
-
-        Number result = (Number) cx.evaluateString(topScope, testCode, "", 1, null);
-        Assert.assertEquals(50, result.intValue());
+        Utils.runWithAllOptimizationLevels(
+                cx -> {
+                    final Scriptable scope = cx.initStandardObjects();
+                    Number result = (Number) cx.evaluateString(scope, testCode, "", 1, null);
+                    Assert.assertEquals(50, result.intValue());
+                    return null;
+                });
     }
 
+    /** Similar to {@link #testInvoke()}, but we use a javascript object, created from prototype. */
     @Test
-    public void testInvokeWithPrototype() throws NoSuchMethodException {
+    public void testInvokeWithPrototype() {
         String testCode =
                 "'use strict'\n"
                         + "function Obj() {}\n"
@@ -71,41 +66,63 @@ public class JavaAdapterInvokeTest {
                         + "(impl)\n"
                         + "adapter.doIt(42)";
 
-        Number result = (Number) cx.evaluateString(topScope, testCode, "", 1, null);
-        Assert.assertEquals(50, result.intValue());
+        Utils.runWithAllOptimizationLevels(
+                cx -> {
+                    final Scriptable scope = cx.initStandardObjects();
+                    Number result = (Number) cx.evaluateString(scope, testCode, "", 1, null);
+                    Assert.assertEquals(50, result.intValue());
+                    return null;
+                });
     }
 
+    /**
+     * Test uses some value in the method that are either constructed in ctor or in prototype. This
+     * test fails before #1453
+     */
     @Test
-    public void testInvokeWithPrototypeAndCtor() throws NoSuchMethodException {
+    public void testInvokeWithPrototypeAndObjs() {
         String testCode =
                 "'use strict'\n"
                         + "function Obj() { this.myObj = {one: 1} }\n"
+                        + "Obj.prototype.seven = 7\n"
                         + "Obj.prototype.m1 = function(i) { return i + this.myObj.one }\n"
-                        + "Obj.prototype.m2 = function() { return 7 }\n"
+                        + "Obj.prototype.m2 = function() { return this.seven }\n"
                         + "var impl = new Obj()\n"
                         + "adapter = new Packages."
                         + AdapterClass.class.getName()
                         + "(impl)\n"
                         + "adapter.doIt(42)";
 
-        Number result = (Number) cx.evaluateString(topScope, testCode, "", 1, null);
-        Assert.assertEquals(50, result.intValue());
+        Utils.runWithAllOptimizationLevels(
+                cx -> {
+                    final Scriptable scope = cx.initStandardObjects();
+                    Number result = (Number) cx.evaluateString(scope, testCode, "", 1, null);
+                    Assert.assertEquals(50, result.intValue());
+                    return null;
+                });
     }
 
+    /** Equivalent javascript code of {@link #testInvokeWithPrototypeAndObjs()} */
     @Test
-    public void testInvokeJsOnly() throws NoSuchMethodException {
+    public void testInvokeJsOnly() {
         String testCode =
                 "'use strict'\n"
                         + "function Obj() { this.myObj = {one: 1} }\n"
+                        + "Obj.prototype.seven = 7\n"
                         + "Obj.prototype.m1 = function(i) { return i + this.myObj.one }\n"
-                        + "Obj.prototype.m2 = function() { return 7 }\n"
+                        + "Obj.prototype.m2 = function() { return this.seven }\n"
                         + "function Adapter(adapter) { this.adapter = adapter }\n"
                         + "Adapter.prototype.doIt = function(i) { return this.adapter.m1(i) + this.adapter.m2() }\n"
                         + "var impl = new Obj()\n"
                         + "adapter = new Adapter(impl)\n"
                         + "adapter.doIt(42)";
 
-        Number result = (Number) cx.evaluateString(topScope, testCode, "", 1, null);
-        Assert.assertEquals(50, result.intValue());
+        Utils.runWithAllOptimizationLevels(
+                cx -> {
+                    final Scriptable scope = cx.initStandardObjects();
+                    Number result = (Number) cx.evaluateString(scope, testCode, "", 1, null);
+                    Assert.assertEquals(50, result.intValue());
+                    return null;
+                });
     }
 }
