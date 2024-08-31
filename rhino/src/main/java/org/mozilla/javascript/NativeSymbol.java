a6c36685517b36da833e5e1319af866e943d870e
Tony Germano <tony@germano.name>
2024-08-30 22:40:18 -0400

    Sort well-known Symbol definitions

--- rhino/src/main/java/org/mozilla/javascript/NativeSymbol.java
+++ rhino/src/main/java/org/mozilla/javascript/NativeSymbol.java
@@ -32,18 +32,18 @@ public class NativeSymbol extends IdScriptableObject implements Symbol {
 
         cx.putThreadLocal(CONSTRUCTOR_SLOT, Boolean.TRUE);
         try {
-            createStandardSymbol(cx, scope, ctor, "iterator", SymbolKey.ITERATOR);
-            createStandardSymbol(cx, scope, ctor, "species", SymbolKey.SPECIES);
-            createStandardSymbol(cx, scope, ctor, "toStringTag", SymbolKey.TO_STRING_TAG);
             createStandardSymbol(cx, scope, ctor, "hasInstance", SymbolKey.HAS_INSTANCE);
             createStandardSymbol(
                     cx, scope, ctor, "isConcatSpreadable", SymbolKey.IS_CONCAT_SPREADABLE);
             createStandardSymbol(cx, scope, ctor, "isRegExp", SymbolKey.IS_REGEXP);
-            createStandardSymbol(cx, scope, ctor, "toPrimitive", SymbolKey.TO_PRIMITIVE);
+            createStandardSymbol(cx, scope, ctor, "iterator", SymbolKey.ITERATOR);
             createStandardSymbol(cx, scope, ctor, "match", SymbolKey.MATCH);
             createStandardSymbol(cx, scope, ctor, "replace", SymbolKey.REPLACE);
             createStandardSymbol(cx, scope, ctor, "search", SymbolKey.SEARCH);
+            createStandardSymbol(cx, scope, ctor, "species", SymbolKey.SPECIES);
             createStandardSymbol(cx, scope, ctor, "split", SymbolKey.SPLIT);
+            createStandardSymbol(cx, scope, ctor, "toPrimitive", SymbolKey.TO_PRIMITIVE);
+            createStandardSymbol(cx, scope, ctor, "toStringTag", SymbolKey.TO_STRING_TAG);
             createStandardSymbol(cx, scope, ctor, "unscopables", SymbolKey.UNSCOPABLES);
 
         } finally {
