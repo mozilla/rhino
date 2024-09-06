5af151e9aed08e60be7f4415c2a03b5002740c45
Roland Praml <roland.praml@foconis.de>
2024-09-06 16:11:20 +0200

    Provided additional tests

--- tests/src/test/java/org/mozilla/javascript/tests/OverloadTestVarArgs.java
+++ tests/src/test/java/org/mozilla/javascript/tests/OverloadTestVarArgs.java
@@ -14,56 +14,83 @@ import org.mozilla.javascript.Scriptable;
 public class OverloadTestVarArgs {
 
     public String args(String arg1) {
-        return "arg1";
+        return "args(String arg1)";
+    }
+
+    public String args(String arg1, int integer) {
+        return "args(String arg1, int integer)";
     }
 
     public String args(String arg1, String arg2) {
-        return "arg1 + arg2";
+        return "args(String arg1, String arg2)";
     }
 
     public String args(String arg1, String... args) {
-        return "arg1 + args";
+        return "args(String arg1, String... args)";
     }
 
     public String args2(String arg1, String... args) {
-        return "arg1 + args";
+        return "args2(String arg1, String... args)";
     }
 
     public String args2(String arg1, String arg2) {
-        return "arg1 + arg2";
+        return "args2(String arg1, String arg2)";
     }
 
     public String args2(String arg1) {
-        return "arg1";
+        return "args2(String arg1)";
     }
 
+    public String args3(String arg1, int... integers) {
+        return "args3(String arg1, int... integers)";
+    }
+
+    public String args3(String arg1, String... strings) {
+        return "args3(String arg1, String... strings)";
+    }
+
+    public String args3(String arg1, String arg2) {
+        return "args3(String arg1, String arg2)";
+    }
+
+    /** ensures, that js + java produces the same result */
     @Test
-    public void argsTestJavaReference() {
-        // this is java reference
-        assertEquals("arg1", this.args("foo"));
-        assertEquals("arg1 + arg2", this.args("foo", "bar"));
-        assertEquals("arg1 + args", this.args("foo", "bar", "baz"));
+    public void argsTest() {
+        /*	assertEvaluates("args(String arg1)", this.args("foo"), "self.args('foo');");
+        assertEvaluates("args(String arg1, String arg2)", this.args("foo", "bar"), "self.args('foo', 'bar');");
+        assertEvaluates("args(String arg1, String... args)",
+        this.args("foo", "bar", "baz"), "self.args('foo', 'bar', 'baz');");
+        assertEvaluates("args(String arg1, int integer)" ,this.args("foo", 1), "self.args('foo', 1);");
+
+        assertEvaluates("args2(String arg1)", this.args2("foo"), "self.args2('foo');");
+        assertEvaluates("args2(String arg1, String arg2)", this.args2("foo", "bar"), "self.args2('foo', 'bar');");
+        assertEvaluates("args2(String arg1, String... args)", this.args2("foo", "bar", "baz"), "self.args2('foo', 'bar', 'baz');");
+
+        assertEvaluates("args3(String arg1, String arg2)", this.args3("foo", "bar"), "self.args3('foo', 'bar');");
+        assertEvaluates("args3(String arg1, int... integers)", this.args3("foo", 1), "self.args3('foo', 1);");
+        int[] intArr = new int[]{1, 2, 3};
+        assertEvaluates("args3(String arg1, int... integers)", this.args3("foo", intArr), "self.args3('foo', 1);", intArr);*/
+
+        String[] stringArr = new String[] {"foo", "bar", "baz"};
+        assertEvaluates(
+                "args3(String arg1, String... strings)",
+                this.args3("foo", stringArr),
+                "self.args3('foo', 1);",
+                stringArr);
     }
 
-    @Test
-    public void argsTestJs() {
-        assertEvaluates("arg1", "self.args('foo');");
-        assertEvaluates("arg1 + arg2", "self.args('foo', 'bar');");
-        assertEvaluates("arg1 + args", "self.args('foo', 'bar', 'baz');");
+    private void assertEvaluates(final Object expected, String javaResult, final String source) {
+        assertEvaluates(expected, javaResult, source, null);
     }
 
-    @Test
-    public void args2TestJs() {
-        assertEvaluates("arg1", "self.args2('foo');");
-        assertEvaluates("arg1 + arg2", "self.args2('foo', 'bar');");
-        assertEvaluates("arg1 + args", "self.args2('foo', 'bar', 'baz');");
-    }
-
-    private void assertEvaluates(final Object expected, final String source) {
+    private void assertEvaluates(
+            final Object expected, String javaResult, final String source, Object arg) {
+        assertEquals(expected, javaResult);
         Utils.runWithAllOptimizationLevels(
                 cx -> {
                     final Scriptable scope = cx.initStandardObjects();
                     scope.put("self", scope, this);
+                    scope.put("arg", scope, arg);
                     final Object rep = cx.evaluateString(scope, source, "test.js", 0, null);
                     assertEquals(expected, Context.jsToJava(rep, String.class));
                     return null;
