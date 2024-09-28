f92263f54033feb811f27ab3b5d0770dd62e1233
naijun0403 <naijun0403@gmail.com>
2024-09-28 21:15:30 +0900

    chore: apply format

--- rhino/src/main/java/org/mozilla/javascript/ScriptRuntime.java
+++ rhino/src/main/java/org/mozilla/javascript/ScriptRuntime.java
@@ -1053,7 +1053,8 @@ public class ScriptRuntime {
         if (Undefined.isUndefined(obj)) return "[object Undefined]";
 
         Object tagValue = ScriptableObject.getProperty(obj, SymbolKey.TO_STRING_TAG);
-        // Note: Scriptable.NOT_FOUND is not a CharSequence, so we don't need to explicitly check for it
+        // Note: Scriptable.NOT_FOUND is not a CharSequence, so we don't need to explicitly check
+        // for it
         if (tagValue instanceof CharSequence) {
             return "[object " + tagValue + "]";
         }
