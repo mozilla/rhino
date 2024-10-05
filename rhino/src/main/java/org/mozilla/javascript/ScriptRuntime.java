mozilla/rhino

commit 4e0b69b5c6728302567f39f90838313775f71a7a

improved ScriptRuntime.toPrimitive() (taken from #1611 done by @tonygermano) (#1661)
* this is an excerpt of from PR #1611 done by @tonygermano (commit 9664e77, e17cc25)

* more standard compliant implementation of ScriptRuntime.toPrimitive();
  * have not used the Optional for more backward compatibility
  * have changed the code a bit to be more in sync with the spec (explicit if stmt's)
* improved support for Symbol/BigInt in equals and compare
* adjusted based on retests with real browsers

rhino/src/main/java/org/mozilla/javascript/ScriptRuntime.java
@@ -3547,16 +3547,53 @@ public class ScriptRuntime {
         return -val.doubleValue();
     }
 
-    public static Object toPrimitive(Object val) {
-        return toPrimitive(val, null);
+    public static Object toPrimitive(Object input) {
+        return toPrimitive(input, null);
     }
 
-    public static Object toPrimitive(Object val, Class<?> typeHint) {
-        if (!(val instanceof Scriptable)) {
-            return val;
+    /**
+     * 1. If input is an Object, then a. Let exoticToPrim be ? GetMethod(input, @@toPrimitive). b.
+     * If exoticToPrim is not undefined, then i. If preferredType is not present, then 1. Let hint
+     * be "default". ii. Else if preferredType is string, then 1. Let hint be "string". iii. Else,
+     * 1. Assert: preferredType is number. 2. Let hint be "number". iv. Let result be ?
+     * Call(exoticToPrim, input, « hint »). v. If result is not an Object, return result. vi. Throw
+     * a TypeError exception. c. If preferredType is not present, let preferredType be number. d.
+     * Return ? OrdinaryToPrimitive(input, preferredType). 2. Return input.
+     *
+     * @param input
+     * @param preferredType
+     * @return
+     * @see <a href="https://262.ecma-international.org/15.0/index.html#sec-toprimitive"></a>
+     */
+    public static Object toPrimitive(Object input, Class<?> preferredType) {
+        if (!isObject(input)) {
+            return input;
         }
-        Scriptable s = (Scriptable) val;
-        Object result = s.getDefaultValue(typeHint);
+        final Scriptable s = (Scriptable) input;
+        final Object exoticToPrim = ScriptableObject.getProperty(s, SymbolKey.TO_PRIMITIVE);
+        if (exoticToPrim instanceof Function) {
+            final Function func = (Function) exoticToPrim;
+            final Context cx = Context.getCurrentContext();
+            final Scriptable scope = func.getParentScope();
+            final String hint;
+            if (preferredType == null) {
+                hint = "default";
+            } else if (StringClass == preferredType) {
+                hint = "string";
+            } else {
+                hint = "number";
+            }
+            final Object result = func.call(cx, scope, s, new Object[] {hint});
+            if (isObject(result)) {
+                throw typeErrorById("msg.cant.convert.to.primitive");
+            }
+            return result;
+        }
+        if (!Undefined.isUndefined(exoticToPrim) && exoticToPrim != Scriptable.NOT_FOUND) {
+            throw notFunctionError(exoticToPrim);
+        }
+        final Class<?> defaultValueHint = preferredType == null ? preferredType : NumberClass;
+        final Object result = s.getDefaultValue(defaultValueHint);
         if ((result instanceof Scriptable) && !isSymbol(result))
             throw typeErrorById("msg.bad.default.value");
         return result;
@@ -3599,6 +3636,8 @@ public class ScriptRuntime {
                 }
             }
             return eqNumber(b ? 1.0 : 0.0, y);
+        } else if (isSymbol(x) && isObject(y)) {
+            return eq(x, toPrimitive(y));
         } else if (x instanceof Scriptable) {
             if (x instanceof Delegator) {
                 x = ((Delegator) x).getDelegee();
@@ -3612,7 +3651,9 @@ public class ScriptRuntime {
             if (y instanceof Delegator && ((Delegator) y).getDelegee() == x) {
                 return true;
             }
-
+            if (isSymbol(y) && isObject(x)) {
+                return eq(toPrimitive(x), y);
+            }
             if (y instanceof Scriptable) {
                 if (x instanceof ScriptableObject) {
                     Object test = ((ScriptableObject) x).equivalentValues(y);
@@ -3951,18 +3992,33 @@ public class ScriptRuntime {
         if (val1 instanceof Number && val2 instanceof Number) {
             return compare((Number) val1, (Number) val2, op);
         } else {
-            if ((val1 instanceof Symbol) || (val2 instanceof Symbol)) {
+            if (isSymbol(val1) || isSymbol(val2)) {
                 throw typeErrorById("msg.compare.symbol");
             }
-            if (val1 instanceof Scriptable) {
-                val1 = ((Scriptable) val1).getDefaultValue(NumberClass);
+            val1 = toPrimitive(val1, NumberClass);
+            val2 = toPrimitive(val2, NumberClass);
+
+            if (val1 instanceof CharSequence) {
+                if (val2 instanceof CharSequence) {
+                    return compareTo(val1.toString(), val2.toString(), op);
+                }
+
+                if (val2 instanceof BigInteger) {
+                    try {
+                        return compareTo(toBigInt(val1.toString()), (BigInteger) val2, op);
+                    } catch (EcmaError e) {
+                        return false;
+                    }
+                }
             }
-            if (val2 instanceof Scriptable) {
-                val2 = ((Scriptable) val2).getDefaultValue(NumberClass);
+            if (val1 instanceof BigInteger && val2 instanceof CharSequence) {
+                try {
+                    return compareTo((BigInteger) val1, toBigInt(val2.toString()), op);
+                } catch (EcmaError e) {
+                    return false;
+                }
             }
-            if (val1 instanceof CharSequence && val2 instanceof CharSequence) {
-                return compareTo(val1.toString(), val2.toString(), op);
-            }
+
             return compare(toNumeric(val1), toNumeric(val2), op);
         }
     }

tests/testsrc/test262.properties
@@ -4604,13 +4604,7 @@ language/expressions/does-not-equals 0/38 (0.0%)
 
 ~language/expressions/dynamic-import
 
-language/expressions/equals 6/47 (12.77%)
-    coerce-symbol-to-prim-err.js
-    coerce-symbol-to-prim-invocation.js
-    coerce-symbol-to-prim-return-obj.js
-    coerce-symbol-to-prim-return-prim.js
-    get-symbol-to-prim-err.js
-    to-prim-hint.js
+language/expressions/equals 0/47 (0.0%)
 
 language/expressions/exponentiation 3/44 (6.82%)
     bigint-toprimitive.js
@@ -4979,12 +4973,9 @@ language/expressions/generators 194/290 (66.9%)
     yield-star-after-newline.js
     yield-star-before-newline.js
 
-language/expressions/greater-than 2/49 (4.08%)
-    bigint-and-incomparable-string.js
-    bigint-and-string.js
+language/expressions/greater-than 0/49 (0.0%)
 
-language/expressions/greater-than-or-equal 1/43 (2.33%)
-    bigint-and-incomparable-string.js
+language/expressions/greater-than-or-equal 0/43 (0.0%)
 
 language/expressions/grouping 0/9 (0.0%)
 
@@ -5027,12 +5018,9 @@ language/expressions/left-shift 4/45 (8.89%)
     bigint-wrapped-values.js
     order-of-evaluation.js
 
-language/expressions/less-than 1/45 (2.22%)
-    bigint-and-incomparable-string.js
+language/expressions/less-than 0/45 (0.0%)
 
-language/expressions/less-than-or-equal 2/47 (4.26%)
-    bigint-and-incomparable-string.js
-    bigint-and-string.js
+language/expressions/less-than-or-equal 0/47 (0.0%)
 
 language/expressions/logical-and 1/18 (5.56%)
     tco-right.js {unsupported: [tail-call-optimization]}

rhino/src/main/resources/org/mozilla/javascript/resources/Messages.properties
@@ -674,6 +674,9 @@ msg.bigint.negative.exponent = \
 msg.bigint.out.of.range.arithmetic = \
     BigInt is too large.
 
+msg.cant.convert.to.primitive = \
+    Cannot convert object to primitive value.
+
 # ScriptableObject
 msg.default.value =\
     Cannot find default value for object.

tests/testsrc/jstests/harmony/v8-symbols.js
@@ -169,8 +169,8 @@ function TestEquality() {
     assertTrue(symbols[i] == symbols[i])
     assertFalse(symbols[i] === Object(symbols[i]))
     assertFalse(Object(symbols[i]) === symbols[i])
-    assertFalse(symbols[i] == Object(symbols[i]))
-    assertFalse(Object(symbols[i]) == symbols[i])
+    assertTrue(symbols[i] == Object(symbols[i]))
+    assertTrue(Object(symbols[i]) == symbols[i])
     assertTrue(symbols[i] === symbols[i].valueOf())
     assertTrue(symbols[i].valueOf() === symbols[i])
     assertTrue(symbols[i] == symbols[i].valueOf())

commit a2e6272f4dc9d8be584a4a4c7b39396e93d3865a

ScriptRuntime#toString(java.lang.Object) uses toPrimitive() fix s stupid bug in the toPrimitive() implementation introduced in #1661

rhino/src/main/java/org/mozilla/javascript/ScriptRuntime.java
@@ -1014,24 +1014,22 @@ public class ScriptRuntime {
                 return val.toString();
             }
             if (val instanceof BigInteger) {
-                return val.toString();
+                return ((BigInteger) val).toString(10);
             }
             if (val instanceof Number) {
                 // XXX should we just teach NativeNumber.stringValue()
                 // about Numbers?
                 return numberToString(((Number) val).doubleValue(), 10);
             }
-            if (val instanceof Symbol) {
+            if (val instanceof Boolean) {
+                return val.toString();
+            }
+            if (isSymbol(val)) {
                 throw typeErrorById("msg.not.a.string");
             }
-            if (val instanceof Scriptable) {
-                val = ((Scriptable) val).getDefaultValue(StringClass);
-                if ((val instanceof Scriptable) && !isSymbol(val)) {
-                    throw errorWithClassName("msg.primitive.expected", val);
-                }
-                continue;
-            }
-            return val.toString();
+            // Assert: val is an Object
+            val = toPrimitive(val, StringClass);
+            // Assert: val is a primitive
         }
     }
 
@@ -3581,7 +3579,7 @@ public class ScriptRuntime {
                 && !Undefined.isUndefined(exoticToPrim)) {
             throw notFunctionError(exoticToPrim);
         }
-        final Class<?> defaultValueHint = preferredType == null ? preferredType : NumberClass;
+        final Class<?> defaultValueHint = preferredType == null ? NumberClass : preferredType;
         final Object result = s.getDefaultValue(defaultValueHint);
         if ((result instanceof Scriptable) && !isSymbol(result))
             throw typeErrorById("msg.bad.default.value");

tests/testsrc/test262.properties
@@ -1063,11 +1063,9 @@ built-ins/Math 51/327 (15.6%)
 
 built-ins/NaN 0/6 (0.0%)
 
-built-ins/NativeErrors 31/123 (25.2%)
+built-ins/NativeErrors 29/123 (23.58%)
     AggregateError/errors-iterabletolist-failures.js
     AggregateError/is-a-constructor.js {unsupported: [Reflect.construct]}
-    AggregateError/message-tostring-abrupt.js
-    AggregateError/message-tostring-abrupt-symbol.js
     AggregateError/newtarget-proto-custom.js {unsupported: [Reflect.construct]}
     AggregateError/newtarget-proto-fallback.js
     AggregateError/proto-from-ctor-realm.js {unsupported: [Reflect]}
@@ -1118,7 +1116,7 @@ built-ins/Number 24/335 (7.16%)
     S9.3.1_A3_T1_U180E.js {unsupported: [u180e]}
     S9.3.1_A3_T2_U180E.js {unsupported: [u180e]}
 
-built-ins/Object 217/3408 (6.37%)
+built-ins/Object 216/3408 (6.34%)
     assign/assignment-to-readonly-property-of-target-must-throw-a-typeerror-exception.js
     assign/not-a-constructor.js {unsupported: [Reflect.construct]}
     assign/source-own-prop-desc-missing.js {unsupported: [Proxy]}
@@ -1188,7 +1186,6 @@ built-ins/Object 217/3408 (6.37%)
     freeze/throws-when-false.js
     freeze/typedarray-backed-by-resizable-buffer.js {unsupported: [resizable-arraybuffer]}
     fromEntries/not-a-constructor.js {unsupported: [Reflect.construct]}
-    fromEntries/to-property-key.js
     fromEntries/uses-keys-not-iterator.js
     getOwnPropertyDescriptors/not-a-constructor.js {unsupported: [Reflect.construct]}
     getOwnPropertyDescriptors/observable-operations.js {unsupported: [Proxy]}
@@ -2184,7 +2181,7 @@ built-ins/SetIteratorPrototype 0/11 (0.0%)
 
 ~built-ins/SharedArrayBuffer
 
-built-ins/String 125/1182 (10.58%)
+built-ins/String 116/1182 (9.81%)
     fromCharCode/not-a-constructor.js {unsupported: [Reflect.construct]}
     fromCodePoint/not-a-constructor.js {unsupported: [Reflect.construct]}
     prototype/charAt/not-a-constructor.js {unsupported: [Reflect.construct]}
@@ -2198,9 +2195,6 @@ built-ins/String 125/1182 (10.58%)
     prototype/indexOf/not-a-constructor.js {unsupported: [Reflect.construct]}
     prototype/indexOf/position-tointeger-errors.js
     prototype/indexOf/position-tointeger-wrapped-values.js
-    prototype/indexOf/searchstring-tostring-bigint.js
-    prototype/indexOf/searchstring-tostring-errors.js
-    prototype/indexOf/searchstring-tostring-toprimitive.js
     prototype/indexOf/searchstring-tostring-wrapped-values.js
     prototype/isWellFormed/not-a-constructor.js {unsupported: [Reflect.construct]}
     prototype/isWellFormed/to-string-primitive.js
@@ -2223,7 +2217,6 @@ built-ins/String 125/1182 (10.58%)
     prototype/replaceAll/not-a-constructor.js {unsupported: [Reflect.construct]}
     prototype/replaceAll/replaceValue-call-each-match-position.js
     prototype/replaceAll/replaceValue-call-matching-empty.js
-    prototype/replaceAll/replaceValue-value-tostring.js
     prototype/replaceAll/searchValue-flags-no-g-throws.js
     prototype/replaceAll/searchValue-flags-null-undefined-throws.js
     prototype/replaceAll/searchValue-flags-toString-abrupt.js
@@ -2236,7 +2229,6 @@ built-ins/String 125/1182 (10.58%)
     prototype/replaceAll/searchValue-replacer-RegExp-call.js {unsupported: [class]}
     prototype/replaceAll/searchValue-replacer-RegExp-call-fn.js {unsupported: [class]}
     prototype/replaceAll/searchValue-tostring-regexp.js
-    prototype/replaceAll/this-tostring.js
     prototype/replace/cstm-replace-get-err.js
     prototype/replace/cstm-replace-invocation.js
     prototype/replace/not-a-constructor.js {unsupported: [Reflect.construct]}
@@ -2272,16 +2264,12 @@ built-ins/String 125/1182 (10.58%)
     prototype/toWellFormed/to-string-primitive.js
     prototype/trimEnd/not-a-constructor.js {unsupported: [Reflect.construct]}
     prototype/trimEnd/this-value-object-toprimitive-call-err.js
-    prototype/trimEnd/this-value-object-toprimitive-meth-err.js
     prototype/trimEnd/this-value-object-toprimitive-meth-priority.js
-    prototype/trimEnd/this-value-object-toprimitive-returns-object-err.js
     prototype/trimEnd/this-value-object-tostring-meth-priority.js
     prototype/trimEnd/this-value-object-valueof-meth-priority.js
     prototype/trimStart/not-a-constructor.js {unsupported: [Reflect.construct]}
     prototype/trimStart/this-value-object-toprimitive-call-err.js
-    prototype/trimStart/this-value-object-toprimitive-meth-err.js
     prototype/trimStart/this-value-object-toprimitive-meth-priority.js
-    prototype/trimStart/this-value-object-toprimitive-returns-object-err.js
     prototype/trimStart/this-value-object-tostring-meth-priority.js
     prototype/trimStart/this-value-object-valueof-meth-priority.js
     prototype/trim/not-a-constructor.js {unsupported: [Reflect.construct]}