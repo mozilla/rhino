740299852fad3ba1a9415785cd92741c327bac2f
Ronald Brill <rbri@rbri.de>
2024-11-07 14:04:00 +0100

    make sure, equivalentValues is used if available

---
+++ rhino/src/test/java/org/mozilla/javascript/tests/ScriptRuntimeEquivalentValuesTest.java
@@ -0,0 +1,86 @@
+package org.mozilla.javascript.tests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.mozilla.javascript.*;
+import org.mozilla.javascript.annotations.JSConstructor;
+
+/**
+ * Test cases for the {@link ScriptRuntime} support for ScriptableObject#equivalentValues(Object)
+ * method.
+ *
+ * @author Ronald Brill
+ */
+public class ScriptRuntimeEquivalentValuesTest {
+
+    @Test
+    public void equivalentValuesUndefined() throws Exception {
+        Utils.runWithAllOptimizationLevels(
+                cx -> {
+                    final Scriptable scope = cx.initStandardObjects();
+                    try {
+                        ScriptableObject.defineClass(scope, EquivalentTesterObject.class);
+                    } catch (Exception e) {
+                    }
+
+                    Object result =
+                            cx.evaluateString(
+                                    scope,
+                                    "var o = new EquivalentTesterObject();"
+                                            + "'' + (o == undefined) + ' ' + (undefined == o)",
+                                    "test",
+                                    1,
+                                    null);
+                    assertEquals("" + cx.getOptimizationLevel(), "true true", result);
+
+                    return null;
+                });
+    }
+
+    @Test
+    public void equivalentValuesNull() throws Exception {
+        Utils.runWithAllOptimizationLevels(
+                cx -> {
+                    final Scriptable scope = cx.initStandardObjects();
+                    try {
+                        ScriptableObject.defineClass(scope, EquivalentTesterObject.class);
+                    } catch (Exception e) {
+                    }
+
+                    Object result =
+                            cx.evaluateString(
+                                    scope,
+                                    "var o = new EquivalentTesterObject();"
+                                            + "'' + (o == null) + ' ' + (null == o)",
+                                    "test",
+                                    1,
+                                    null);
+                    assertEquals("" + cx.getOptimizationLevel(), "true true", result);
+
+                    return null;
+                });
+    }
+
+    public static class EquivalentTesterObject extends ScriptableObject {
+
+        public EquivalentTesterObject() {}
+
+        @Override
+        public String getClassName() {
+            return "EquivalentTesterObject";
+        }
+
+        @JSConstructor
+        public void jsConstructorMethod() {}
+
+        @Override
+        protected Object equivalentValues(final Object value) {
+            if (value == null || Undefined.isUndefined(value)) {
+                return Boolean.TRUE;
+            }
+
+            return super.equivalentValues(value);
+        }
+    }
+}
