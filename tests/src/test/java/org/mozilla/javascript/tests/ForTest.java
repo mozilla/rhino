79cf572db0f65e7b40f211970def9c872dd4880a
Ronald Brill <rbri@rbri.de>
2024-12-01 12:25:49 +0100

    implement some test cases

---
+++ tests/src/test/java/org/mozilla/javascript/tests/ForTest.java
@@ -0,0 +1,17 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.javascript.tests;
+
+import org.junit.Test;
+import org.mozilla.javascript.Undefined;
+
+public class ForTest {
+
+    /** Test for issue #645. There was a NPE. */
+    @Test
+    public void forInit() {
+        Utils.assertWithAllOptimizationLevels(Undefined.instance, "for(({});;){ break; }");
+    }
+}
---
+++ tests/src/test/java/org/mozilla/javascript/tests/es6/ArrayDestructuringTest.java
@@ -0,0 +1,30 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.javascript.tests.es6;
+
+import org.junit.Test;
+import org.mozilla.javascript.tests.Utils;
+
+public class ArrayDestructuringTest {
+
+    /** Test for issue #662. There was a ClassCastException. */
+    @Test
+    public void strangeCase() {
+        Utils.assertWithAllOptimizationLevels("", "var a = ''; [a.x] = ''; a");
+    }
+
+    /** Test for issue #662. */
+    @Test
+    public void strangeCase2() {
+        Utils.assertWithAllOptimizationLevels("", "[1..h]=''");
+    }
+
+    /** Test for issue #662. */
+    @Test
+    public void strangeCase3() {
+        Utils.assertWithAllOptimizationLevels(
+                123, "[0..toString.h] = [123]; Number.prototype.toString.h");
+    }
+}
