d69afa8aa8d03d70e945d9fd07d218c7d790b521
nabacg <nabacg@gmail.com>
2024-12-02 12:48:36 +0000

    adding PropertyTest redefinePropertyWithThreadSafeSlotMap as a repro case of Dead Lock that can occure when new SlotMap.compute method is used with ThreadSafeSlotMapContainer

--- tests/src/test/java/org/mozilla/javascript/tests/es6/PropertyTest.java
+++ tests/src/test/java/org/mozilla/javascript/tests/es6/PropertyTest.java
@@ -1,12 +1,19 @@
 package org.mozilla.javascript.tests.es6;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.mozilla.javascript.tests.Utils.DEFAULT_OPT_LEVELS;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.lang.reflect.Method;
 import org.junit.Test;
 import org.mozilla.javascript.Context;
+import org.mozilla.javascript.ContextFactory;
+import org.mozilla.javascript.Scriptable;
 import org.mozilla.javascript.ScriptableObject;
 import org.mozilla.javascript.tests.Utils;
+import org.mozilla.javascript.tools.shell.Global;
 
 public class PropertyTest {
 
@@ -118,6 +125,59 @@ public class PropertyTest {
 
                     return null;
                 });
+
+
+
+    }
+
+    @Test
+    public void redefinePropertyWithThreadSafeSlotMap() {
+
+        final ContextFactory factory =
+                new ContextFactory() {
+                    @Override
+                    protected boolean hasFeature(Context cx, int featureIndex) {
+                        switch (featureIndex) {
+                            case Context.FEATURE_THREAD_SAFE_OBJECTS:
+                                return true;
+                            default:
+                                return super.hasFeature(cx, featureIndex);
+                        }
+                    }
+                };
+
+        try (Context cx = factory.enterContext()) {
+            cx.setLanguageVersion(Context.VERSION_ES6);
+            ScriptableObject scope = cx.initStandardObjects();
+
+            final String expected = "undefined - true - true | function - function";
+
+            final String script =
+                    "Object.defineProperty(MyHostObject, 'foo', { enumerable: !0, configurable: !0, set: function() { return !0 }});\n"
+                            + "var desc = Object.getOwnPropertyDescriptor(MyHostObject, 'foo');"
+                            + "var result = '' + desc.writable + ' - ' + desc.configurable + ' - ' + desc.enumerable;"
+                            + "result = result + ' | ' + typeof desc.get + ' - ' + typeof desc.set;"
+                            + "result;";
+
+            try {
+                final MyHostObject myHostObject = new MyHostObject();
+
+                // define custom getter method
+                final Method getter = MyHostObject.class.getMethod("getFoo");
+                final Method setter = MyHostObject.class.getMethod("setFoo", String.class);
+                myHostObject.defineProperty(
+                        "foo", null, getter, setter, ScriptableObject.EMPTY);
+                scope.put("MyHostObject", scope, myHostObject);
+            } catch (Exception e) {
+            }
+
+            final String result =
+                    (String) cx.evaluateString(scope, script, "myScript", 1, null);
+
+            assertEquals(expected, result);
+
+        }
+
     }
 
     public static class MyHostObject extends ScriptableObject {
