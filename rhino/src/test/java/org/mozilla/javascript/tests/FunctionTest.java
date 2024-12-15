ec46b2072d4c8faa25f1b2ee5e66bc5245581fdb
Ronald Brill <rbri@rbri.de>
2024-12-15 19:01:02 +0100

    simplify tests

--- rhino/src/test/java/org/mozilla/javascript/tests/FunctionTest.java
+++ rhino/src/test/java/org/mozilla/javascript/tests/FunctionTest.java
@@ -5,10 +5,7 @@
 /** */
 package org.mozilla.javascript.tests;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
-import org.mozilla.javascript.Scriptable;
 
 /**
  * Unit tests for Function.
