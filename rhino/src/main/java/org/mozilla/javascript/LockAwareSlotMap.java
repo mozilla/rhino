5b1569b806c695971aa4a526433e8a115b62c125
duncan.macgregor <duncan.macgregor@servicenow.com>
2024-12-29 20:06:05 +0000

    Factor out container classes as they are no longer required.

--- rhino/src/main/java/org/mozilla/javascript/LockAwareSlotMap.java
+++ rhino/src/main/java/org/mozilla/javascript/LockAwareSlotMap.java
@@ -1,10 +1,9 @@
 package org.mozilla.javascript;
 
 /**
- * Extends {@link SlotMap} with a set of "WithLock" methods. which
- * will not acquire the lock. These should only be used internally by
- * implementation, or by other {@link SlotMap}s which share the same
- * lock.
+ * Extends {@link SlotMap} with a set of "WithLock" methods. which will not acquire the lock. These
+ * should only be used internally by implementation, or by other {@link SlotMap}s which share the
+ * same lock.
  */
 interface LockAwareSlotMap extends SlotMap {
     /** The equivalent of {@link SlotMap#size()}. */
@@ -19,7 +18,10 @@ interface LockAwareSlotMap extends SlotMap {
     /** The equivalent of {@link SlotMap#query(Object, int)}. */
     Slot queryWithLock(Object key, int index);
 
-    /** The equivalent of {@link SlotMap#compute(SlotMapOwner, Object, int, org.mozilla.javascript.SlotMap.SlotComputer)}. */
+    /**
+     * The equivalent of {@link SlotMap#compute(SlotMapOwner, Object, int,
+     * org.mozilla.javascript.SlotMap.SlotComputer)}.
+     */
     <S extends Slot> S computeWithLock(
             SlotMapOwner owner, Object key, int index, SlotComputer<S> compute);
 
