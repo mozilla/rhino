8a6c7e5ac7e466ca7a602d6814e9db6f883614a5
Roland Praml <roland.praml@foconis.de>
2025-02-05 10:01:37 +0100

    Simplified test and removed some config files

--- rhino/src/test/java/org/mozilla/javascript/tests/RhinoPropertiesTest.java
+++ rhino/src/test/java/org/mozilla/javascript/tests/RhinoPropertiesTest.java
@@ -1,7 +1,7 @@
 package org.mozilla.javascript.tests;
 
+import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 
 import java.io.File;
 import java.util.Map;
@@ -23,88 +23,101 @@ import org.mozilla.javascript.config.RhinoPropertiesLoader;
  */
 public class RhinoPropertiesTest {
 
+    public enum TestEnum {
+        valueA,
+        VALUEB,
+        valuec
+    }
+
     /** This loader is used for all tests in this maven module! */
     public static class Loader implements RhinoPropertiesLoader {
 
-        public static enum TestEnum {
-            valueA,
-            VALUEB,
-            valuec
-        }
-
+        /**
+         * this loader will add some explicitly typed values to the configuration. After that, it
+         * will also load the defaults.
+         */
         @Override
         public void load(RhinoProperties properties) {
 
-            properties.addConfig(Map.of("foo.bar", "baz1"));
+            properties.addConfig(Map.of("test.foo.bar", "baz1"));
             properties.addConfig(
                     Map.of(
-                            "foo.bar", "baz2",
-                            "foo.someBoolean1", "true",
-                            "foo.someBoolean2", "TRUE",
-                            "foo.someBoolean3", "1",
-                            "foo.someInteger1", "42",
-                            "foo.someInteger2", 42,
-                            "foo.someAValue", true,
-                            "FOO_SOME_BVALUE", 42,
-                            "FOO_ENUM1", "valuea"));
+                            "test.foo.bar", "baz2",
+                            "test.foo.someBoolean1", "true",
+                            "test.foo.someBoolean2", "TRUE",
+                            "test.foo.someBoolean3", "1",
+                            "test.foo.someInteger1", "42",
+                            "test.foo.someInteger2", 42,
+                            "test.foo.someAValue", true,
+                            "TEST_FOO_SOME_BVALUE", 42,
+                            "TEST_FOO_ENUM1", "valuea"));
             properties.addConfig(
                     Map.of(
-                            "FOO_ENUM1", "valuea",
-                            "FOO_ENUM2", "VALUEA",
-                            "FOO_ENUM3", "VALUEB",
-                            "FOO_ENUM4", "VALUEC",
-                            "FOO_ENUM5", "VALUED"));
-            // load module independent file.
-            properties.loadFromFile(new File("rhino-config-for-this-module.config"));
+                            "TEST_FOO_ENUM1", "valuea",
+                            "TEST_FOO_ENUM2", "VALUEA",
+                            "TEST_FOO_ENUM3", "VALUEB",
+                            "TEST_FOO_ENUM4", "VALUEC",
+                            "TEST_FOO_ENUM5", "VALUED"));
+            // also load defaults, so that users can use rhino-test.config here!
+            properties.loadDefaults();
         }
     }
 
     /** Tests the loader initialization and all methods in RhinoConfig. */
     @Test
     void testLoaderInit() {
+        // test if correct files are loaded:
+        assertFalse(RhinoConfig.get("test.rhino-explicit.loaded", false));
+        assertFalse(RhinoConfig.get("test.file.rhino-config.loaded", false));
+
+        assertTrue(RhinoConfig.get("test.file.rhino-test-config.loaded", false));
+        assertTrue(RhinoConfig.get("test.cp.rhino-config.loaded", false));
+        assertTrue(RhinoConfig.get("test.cp.rhino-config.loaded", false));
+
         // override test
-        assertEquals("baz2", RhinoConfig.get("foo.bar"));
+        assertEquals("baz2", RhinoConfig.get("test.foo.bar"));
 
         // normal getters (with correct type)
-        assertEquals(true, RhinoConfig.get("foo.someBoolean1", false));
-        assertEquals(true, RhinoConfig.get("foo.someBoolean2", false));
-        assertEquals(false, RhinoConfig.get("foo.someBoolean3", false));
-        assertEquals(false, RhinoConfig.get("foo.someOtherBoolean", false));
+        assertEquals(true, RhinoConfig.get("test.foo.someBoolean1", false));
+        assertEquals(true, RhinoConfig.get("test.foo.someBoolean2", false));
+        assertEquals(false, RhinoConfig.get("test.foo.someBoolean3", false));
+        assertEquals(false, RhinoConfig.get("test.foo.someOtherBoolean", false));
 
         // integer test
-        assertEquals(42, RhinoConfig.get("foo.someInteger1", 21));
-        assertEquals(42, RhinoConfig.get("foo.someInteger2", 21));
-        assertEquals(21, RhinoConfig.get("foo.someOtherInteger", 21));
+        assertEquals(42, RhinoConfig.get("test.foo.someInteger1", 21));
+        assertEquals(42, RhinoConfig.get("test.foo.someInteger2", 21));
+        assertEquals(21, RhinoConfig.get("test.foo.someOtherInteger", 21));
 
         // camelCase-Tests
-        assertEquals(true, RhinoConfig.get("foo.someAValue", false));
-        assertEquals(42, RhinoConfig.get("foo.someBValue", 21));
+        assertEquals(true, RhinoConfig.get("test.foo.someAValue", false));
+        assertEquals(42, RhinoConfig.get("test.foo.someBValue", 21));
 
         // enums
-        assertEquals(Loader.TestEnum.valueA, RhinoConfig.get("foo.enum1", Loader.TestEnum.VALUEB));
-        assertEquals(Loader.TestEnum.valueA, RhinoConfig.get("foo.enum2", Loader.TestEnum.VALUEB));
-        assertEquals(Loader.TestEnum.VALUEB, RhinoConfig.get("foo.enum3", Loader.TestEnum.valueA));
-        assertEquals(Loader.TestEnum.valuec, RhinoConfig.get("foo.enum4", Loader.TestEnum.valueA));
-        assertEquals(Loader.TestEnum.valueA, RhinoConfig.get("foo.enum5", Loader.TestEnum.valueA));
-        assertEquals(Loader.TestEnum.valueA, RhinoConfig.get("foo.enum6", Loader.TestEnum.valueA));
+        assertEquals(TestEnum.valueA, RhinoConfig.get("test.foo.enum1", TestEnum.VALUEB));
+        assertEquals(TestEnum.valueA, RhinoConfig.get("test.foo.enum2", TestEnum.VALUEB));
+        assertEquals(TestEnum.VALUEB, RhinoConfig.get("test.foo.enum3", TestEnum.valueA));
+        assertEquals(TestEnum.valuec, RhinoConfig.get("test.foo.enum4", TestEnum.valueA));
+        assertEquals(TestEnum.valueA, RhinoConfig.get("test.foo.enum5", TestEnum.valueA));
+        assertEquals(TestEnum.valueA, RhinoConfig.get("test.foo.enum6", TestEnum.valueA));
     }
 
     /** Tests explicit loading a file from classpath. */
     @Test
     void testClasspathLoad() {
         RhinoProperties properties = new RhinoProperties();
-        properties.loadFromClasspath(getClass().getClassLoader(), "rhino-testcase.config");
-        assertEquals("value1", properties.get("testconfig.foo"));
-        assertEquals("value2", properties.get("testconfig.bar"));
+        properties.loadFromClasspath(getClass().getClassLoader(), "rhino-explicit.config");
+        assertEquals("true", properties.get("test.rhino-explicit.loaded"));
+        assertEquals("value1", properties.get("test.config.foo"));
+        assertEquals("value2", properties.get("test.config.bar"));
     }
 
     /** Tests explicit loading a file. */
     @Test
     void testFileLoad() {
         RhinoProperties properties = new RhinoProperties();
-        properties.loadFromFile(new File("src/test/resources/rhino-testcase.config"));
-        assertEquals("value1", properties.get("testconfig.foo"));
-        assertEquals("value2", properties.get("testconfig.bar"));
+        properties.loadFromFile(new File("src/test/resources/rhino-explicit.config"));
+        assertEquals("value1", properties.get("test.config.foo"));
+        assertEquals("value2", properties.get("test.config.bar"));
     }
 
     @Test
@@ -113,9 +126,9 @@ public class RhinoPropertiesTest {
         try {
             RhinoProperties properties = new RhinoProperties();
             properties.loadDefaults();
-            assertEquals("value3", properties.get("testconfig.foo"));
-            assertEquals("value4-mod", properties.get("testconfig.bar"));
-            assertEquals("value5", properties.get("testconfig.baz"));
+            assertEquals("value3", properties.get("test.config.foo"));
+            assertEquals("value4-mod", properties.get("test.config.bar"));
+            assertEquals("value5", properties.get("test.config.baz"));
             assertEquals("value6", properties.get("some.system.value"));
         } finally {
             System.clearProperty("some.system.value");
