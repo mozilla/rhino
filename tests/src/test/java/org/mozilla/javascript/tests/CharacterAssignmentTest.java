bebf546a59bdf4c9709e6bde8eeb17b446bca75c
Jakob Silbereisen <jakob.silbereisen@foconis.de>
2025-02-14 16:18:00 +0100

    Failing Test

---
+++ tests/src/test/java/org/mozilla/javascript/tests/CharacterAssignmentTest.java
@@ -0,0 +1,48 @@
+package org.mozilla.javascript.tests;
+
+import org.junit.Test;
+import org.mozilla.javascript.tools.shell.Global;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Jakob Silbereisen, Foconis Analytics GmbH
+ */
+public class CharacterAssignmentTest {
+
+	public static class X {
+		public Character bigCharacter;
+		public char primitiveCharacter;
+		public X() {}
+		public X(char c) {
+			bigCharacter = c;
+			primitiveCharacter = c;
+		}
+	}
+
+	@Test
+	public void assignCharacterTypesFromNumbers() {
+		Utils.runWithAllModes(
+				cx -> {
+					final Global scope = new Global();
+					scope.init(cx);
+					X x1 = new X(), x2 = new X();
+					scope.put("x1", scope, x1);
+					scope.put("x2", scope, x2);
+					scope.put("src", scope, new X('X'));
+					Object ret =
+							cx.evaluateString(
+									scope,
+									"x1.bigCharacter = 65;"
+									+ "x1.primitiveCharacter = 65;"
+									+ "x2.bigCharacter = src.primitiveCharacter;",
+									"myScript.js",
+									1,
+									null);
+					assertEquals('A', x1.bigCharacter);
+					assertEquals('A', x1.primitiveCharacter);
+					assertEquals('X', x2.bigCharacter);
+					return null;
+				});
+	}
+}
