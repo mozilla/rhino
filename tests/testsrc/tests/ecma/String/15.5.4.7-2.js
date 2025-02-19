1799fbef4a615fb51cfe115728217f8c5b3741e7
Greg Brail <gbrail@users.noreply.github.com>
2025-02-18 22:04:02 -0800

    Fix obsolete tests

--- tests/testsrc/tests/ecma/String/15.5.4.7-2.js
+++ tests/testsrc/tests/ecma/String/15.5.4.7-2.js
@@ -54,8 +54,9 @@ writeHeaderToLog( SECTION + " "+ TITLE);
 
 
 new TestCase( SECTION, "String.prototype.lastIndexOf.length",           1,          String.prototype.lastIndexOf.length );
-new TestCase( SECTION, "delete String.prototype.lastIndexOf.length",    false,      delete String.prototype.lastIndexOf.length );
-new TestCase( SECTION, "delete String.prototype.lastIndexOf.length; String.prototype.lastIndexOf.length",   1,  eval("delete String.prototype.lastIndexOf.length; String.prototype.lastIndexOf.length" ) );
+// These are no longer valid ECMAScript tests
+//new TestCase( SECTION, "delete String.prototype.lastIndexOf.length",    false,      delete String.prototype.lastIndexOf.length );
+//new TestCase( SECTION, "delete String.prototype.lastIndexOf.length; String.prototype.lastIndexOf.length",   1,  eval("delete String.prototype.lastIndexOf.length; String.prototype.lastIndexOf.length" ) );
 
 new TestCase( SECTION, "var s = new String(''); s.lastIndexOf('', 0)",          LastIndexOf("","",0),  eval("var s = new String(''); s.lastIndexOf('', 0)") );
 new TestCase( SECTION, "var s = new String(''); s.lastIndexOf('')",             LastIndexOf("",""),  eval("var s = new String(''); s.lastIndexOf('')") );
