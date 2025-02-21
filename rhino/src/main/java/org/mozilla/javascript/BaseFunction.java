e08f7e04c5576a9f12df880ea18e520fdf636e4f
duncan.macgregor <duncan.macgregor@servicenow.com>
2025-02-21 10:32:01 +0000

    Make built-in function source spec compliant.

--- rhino/src/main/java/org/mozilla/javascript/BaseFunction.java
+++ rhino/src/main/java/org/mozilla/javascript/BaseFunction.java
@@ -505,9 +505,7 @@ public class BaseFunction extends IdScriptableObject implements Function {
             sb.append(getFunctionName());
             sb.append("() {\n\t");
         }
-        sb.append("[native code, arity=");
-        sb.append(getArity());
-        sb.append("]\n");
+        sb.append("[native code]\n");
         if (!justbody) {
             sb.append("}\n");
         }
