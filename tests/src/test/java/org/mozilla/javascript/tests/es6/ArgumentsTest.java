111792cb3811bd5da930a15e3168b25563059f27
Ronald Brill <rbri@rbri.de>
2025-03-15 15:14:30 +0100

    use always the same ThrowTypeError object for Arguments.caller and Arguments.callee

--- tests/src/test/java/org/mozilla/javascript/tests/es6/ArgumentsTest.java
+++ tests/src/test/java/org/mozilla/javascript/tests/es6/ArgumentsTest.java
@@ -46,4 +46,64 @@ public class ArgumentsTest {
 
         Utils.assertWithAllModes_ES6("1235", code);
     }
+
+    @Test
+    public void argumentsCallee() {
+        String code =
+                "\"use strict\";\n"
+                        + "function foo() {\n"
+                        + "  let desc = Object.getOwnPropertyDescriptor(arguments, 'callee');\n"
+                        + "  var res = '';\n"
+                        + "  res += typeof desc.get + '/' + desc.get.name;\n"
+                        + "  res += ' | '\n"
+                        + "  res += typeof desc.set + '/' + desc.set.name;\n"
+                        + "  res += ' | '\n"
+                        + "  res += desc.get === desc.set;\n"
+                        + "  return res;\n"
+                        + "}\n"
+                        + "foo()\n";
+
+        Utils.assertWithAllModes_ES6("function/ | function/ | true", code);
+    }
+
+    @Test
+    public void argumentsCalleeDifferentFunctions() {
+        String code =
+                "\"use strict\";\n"
+                        + "function foo1() {\n"
+                        + "  return Object.getOwnPropertyDescriptor(arguments, 'callee');\n"
+                        + "}\n"
+                        + "function foo2() {\n"
+                        + "  return Object.getOwnPropertyDescriptor(arguments, 'callee');\n"
+                        + "}\n"
+                        + "let desc1 = foo1();\n"
+                        + "let desc2 = foo2();\n"
+                        + "let res = '';\n"
+                        + "res += desc1.get === desc2.get;\n"
+                        + "res += ' | '\n"
+                        + "res += desc1.set === desc2.set;\n";
+
+        Utils.assertWithAllModes_ES6("true | true", code);
+    }
+
+    @Test
+    public void argumentsCaller() {
+        // this no longer works in real browsers because
+        // Object.getOwnPropertyDescriptor(arguments, "caller") returns undefined
+        String code =
+                "\"use strict\";\n"
+                        + "function foo() {\n"
+                        + "  let desc = Object.getOwnPropertyDescriptor(arguments, 'caller');\n"
+                        + "  var res = '';\n"
+                        + "  res += typeof desc.get + '/' + desc.get.name;\n"
+                        + "  res += ' | '\n"
+                        + "  res += typeof desc.set + '/' + desc.set.name;\n"
+                        + "  res += ' | '\n"
+                        + "  res += desc.get === desc.set;\n"
+                        + "  return res;\n"
+                        + "}\n"
+                        + "foo()\n";
+
+        Utils.assertWithAllModes_ES6("function/ | function/ | true", code);
+    }
 }
