df4d6d20655dd19c18209763984be9dc30ccd2e7
ZZZank <3410764033@qq.com>
2025-03-15 20:18:17 +0800

    add test

---
+++ tests/src/test/java/org/mozilla/javascript/tests/TypeInfoTest.java
@@ -0,0 +1,123 @@
+package org.mozilla.javascript.tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mozilla.javascript.FunctionObject;
+import org.mozilla.javascript.nat.type.TypeInfo;
+import org.mozilla.javascript.nat.type.VariableTypeInfo;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+/**
+ * @author ZZZank
+ */
+public class TypeInfoTest {
+
+    /**
+     * method name -> method return type
+     */
+    private static final Map<String, TypeInfo> TYPES = new HashMap<>();
+
+    static {
+        for (var method : Typing.class.getMethods()) {
+            var old = TYPES.put(method.getName(), TypeInfo.of(method.getGenericReturnType()));
+            Assert.assertNull(String.format("duplicated method name '%s' in Typing.class", method.getName()), old);
+        }
+    }
+
+    @Test
+    public void rawMap() {
+        var typeInfo = TYPES.get("rawMap");
+        Assert.assertSame(TypeInfo.NONE, typeInfo.param(0));
+        Assert.assertSame(TypeInfo.NONE, typeInfo.param(1));
+    }
+
+    @Test
+    public void paramMap() {
+        var typeInfo = TYPES.get("paramMap");
+        Assert.assertEquals(TypeInfo.STRING, typeInfo.param(0));
+        Assert.assertEquals(TypeInfo.INT, typeInfo.param(1));
+    }
+
+    @Test
+    public void wildcard() {
+        var typeInfo = TYPES.get("wildcard");
+        // wildcard type are converted to its bound
+        Assert.assertSame(Number.class, typeInfo.param(0).asClass());
+    }
+
+    @Test
+    public void variable() {
+        var typeInfo = TYPES.get("variable");
+        var param = typeInfo.param(0);
+        Assert.assertTrue(param instanceof VariableTypeInfo);
+        Assert.assertEquals("E", param.toString());
+        Assert.assertEquals(Number.class, param.asClass());
+    }
+
+    @Test
+    public void primitive() {
+        var typeInfo = TYPES.get("primitive");
+        Assert.assertEquals(TypeInfo.PRIMITIVE_INT, typeInfo);
+    }
+
+    @Test
+    public void primitiveArray() {
+        var typeInfo = TYPES.get("primitiveArray");
+        Assert.assertTrue(typeInfo.isArray());
+        Assert.assertTrue(typeInfo.getComponentType().isPrimitive());
+    }
+
+    @Test
+    public void genericArray() {
+        var typeInfo = TYPES.get("genericArray");
+        Assert.assertTrue(typeInfo.isArray());
+        Assert.assertEquals("T", typeInfo.getComponentType().toString());
+        Assert.assertEquals(Object.class, typeInfo.getComponentType().asClass());
+    }
+
+    @Test
+    public void clazz() {
+        var typeInfo = TYPES.get("clazz");
+        Assert.assertEquals(String.class, typeInfo.asClass());
+        Assert.assertSame(TypeInfo.NONE, typeInfo.getComponentType());
+        Assert.assertEquals(TypeInfo.NONE, typeInfo.param(0));
+    }
+
+    @Test
+    public void functionalInterface() {
+        var typeInfo = TYPES.get("functionalInterface");
+        Assert.assertTrue(typeInfo.isInterface());
+        Assert.assertTrue(typeInfo.isFunctionalInterface());
+    }
+
+    @Test
+    public void typeTag() {
+        for (var typeInfo : TYPES.values()) {
+            Assert.assertEquals(FunctionObject.getTypeTag(typeInfo.asClass()), typeInfo.getTypeTag());
+        }
+    }
+
+    interface Typing {
+        HashMap rawMap();
+
+        HashMap<String, Integer> paramMap();
+
+        Map<? extends Number, String> wildcard();
+
+        <E extends Number> List<E> variable();
+
+        int primitive();
+
+        float[] primitiveArray();
+
+        <T> T[] genericArray(T input);
+
+        String clazz();
+
+        Function functionalInterface();
+    }
+}
