3ed6166c9f2970b3a64cfe5f9e4de76c9f306a6e
andrea.bergia <andrea.bergia@servicenow.com>
2025-06-17 14:36:41 +0200

    Added more tests for undefined
    I've also reorganized the test using a style where each test case
    is wrapped in a named IFFE. I've seen this in some rhino tests and I
    think it's pretty clean - in case of a test failure, the stack trace
    contains the function name, so it helps as documentation.

--- tests/testsrc/jstests/redefined-undefined.js
+++ tests/testsrc/jstests/redefined-undefined.js
@@ -1,6 +1,6 @@
 load("testsrc/assert.js");
 
-(function () {
+(function testRedefineUndefinedInAFunction() {
     var undefined = 10;
     assertEquals(10, undefined);
 
@@ -18,21 +18,45 @@ load("testsrc/assert.js");
     assertEquals(10, x);
 })();
 
-function f(undefined) {
-    return undefined;
-}
+(function undefinedAsArgumentName() {
+    function f(undefined) {
+        return undefined;
+    }
 
-assertEquals(12, f(12));
+    assertEquals(12, f(12));
+})();
 
-var o = { undefined: 42 };
-with (o) {
-    assertEquals(42, undefined);
-}
+(function undefinedInWith() {
+    var o = {undefined: 42};
+    with (o) {
+        assertEquals(42, undefined);
+    }
+})();
 
-try {
-    throw "a";
-} catch (undefined) {
-    assertEquals("a", undefined);
-}
+(function undefinedAsCatchExpression() {
+    try {
+        throw "a";
+    } catch (undefined) {
+        assertEquals("a", undefined);
+    }
+})();
+
+(function undefinedAsFunctionName() {
+    function undefined() {
+        return 10;
+    }
+
+    assertEquals(10, undefined());
+})();
+
+(function undefinedViaDefinePropertyIsIgnored() {
+    Object.defineProperty(this, "undefined", { value: 10 });
+    assertEquals(undefined, undefined);
+})();
+
+(function everythingStillWorks() {
+    assertEquals(typeof undefined, "undefined");
+    assertEquals(undefined, void 0);
+})();
 
 "success";
