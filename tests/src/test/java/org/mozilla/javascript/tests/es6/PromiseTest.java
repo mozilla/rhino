eeed229cd73fddddeb877f1febe792ac9450ca5c
Anivar Aravind <ping@anivar.net>
2025-07-21 05:47:48 +0530

    Implement Promise.withResolvers ES2024 feature
    Adds Promise.withResolvers() static method as specified in ECMAScript 2024.
    This method returns an object containing a new Promise and its resolve/reject functions,
    providing a cleaner alternative to the traditional Promise constructor pattern.
    
    Implementation:
    - Added withResolvers as a static method on Promise constructor
    - Uses Capability class to ensure proper promise construction and prototype chain
    - Returns object with {promise, resolve, reject} properties
    - Follows ECMAScript 2024 specification requirements
    
    Tests:
    - Added comprehensive unit tests to PromiseTest.java
    - Tests verify object structure, types, and functionality
    - Validates promise instanceof Promise and callable functions
    
    Fixes #1544

--- tests/src/test/java/org/mozilla/javascript/tests/es6/PromiseTest.java
+++ tests/src/test/java/org/mozilla/javascript/tests/es6/PromiseTest.java
@@ -36,4 +36,48 @@ public class PromiseTest {
                         + "  r;";
         Utils.assertWithAllModes_ES6("true done", script);
     }
+
+    @Test
+    public void withResolversBasic() {
+        final String script =
+                "var result = Promise.withResolvers();"
+                        + "typeof result === 'object' && "
+                        + "typeof result.promise === 'object' && "
+                        + "typeof result.resolve === 'function' && "
+                        + "typeof result.reject === 'function'";
+        Utils.assertWithAllModes_ES6(true, script);
+    }
+
+    @Test
+    public void withResolversPromiseType() {
+        final String script = "var result = Promise.withResolvers(); result.promise instanceof Promise";
+        Utils.assertWithAllModes_ES6(true, script);
+    }
+
+    @Test
+    public void withResolversObjectKeys() {
+        final String script =
+                "var result = Promise.withResolvers();"
+                        + "var keys = Object.keys(result).sort();"
+                        + "keys.length === 3 && keys[0] === 'promise' && keys[1] === 'reject' && keys[2] === 'resolve'";
+        Utils.assertWithAllModes_ES6(true, script);
+    }
+
+    @Test
+    public void withResolversResolveWorks() {
+        final String script =
+                "var result = Promise.withResolvers();"
+                        + "result.resolve('test');"
+                        + "'success'"; // Just verify the resolve call doesn't throw
+        Utils.assertWithAllModes_ES6("success", script);
+    }
+
+    @Test
+    public void withResolversRejectWorks() {
+        final String script =
+                "var result = Promise.withResolvers();"
+                        + "try { result.reject(new Error('test')); } catch(e) { /* ignore unhandled rejection */ }"
+                        + "'success'"; // Just verify the reject call doesn't throw
+        Utils.assertWithAllModes_ES6("success", script);
+    }
 }
