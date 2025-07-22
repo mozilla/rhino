def73f6422e2e149f1390163a512190172d0f960
Roland Praml <roland.praml@foconis.de>
2025-07-22 10:42:50 +0200

    Pick best implementation from benchmark

--- rhino/src/main/java/org/mozilla/javascript/ContextHolder.java
+++ rhino/src/main/java/org/mozilla/javascript/ContextHolder.java
@@ -1,7 +1,22 @@
 package org.mozilla.javascript;
 
+/* -*- Mode: java; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/**
+ * The ContextHolder encapsulates a ThreadLocal, that holds the current context. Note: former
+ * methods have used an Object[] for performance reasons. This seems to be outdated. See
+ * ContextHolderBenchmark
+ *
+ * @author Roland Praml
+ */
 class ContextHolder {
     private static final ThreadLocal<Context> contextLocal = new ThreadLocal<>();
+
+    private ContextHolder() {}
 
     static void setContext(Context context) {
         contextLocal.set(context);
@@ -11,7 +26,10 @@ class ContextHolder {
         return contextLocal.get();
     }
 
-    public static void clearContext() {
-        contextLocal.remove();
+    static void clearContext() {
+        // do not use contextLocal.remove() here, as this might be much slower, when the same thread
+        // creates a new context.
+        // See ContextHolderBenchmark
+        contextLocal.set(null);
     }
 }
