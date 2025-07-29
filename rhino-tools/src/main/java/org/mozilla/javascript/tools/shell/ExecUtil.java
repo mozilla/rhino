12d6c46a69d0e5f41e3d50660234b7aafa136ff4
Roland Praml <roland.praml@foconis.de>
2025-07-29 08:36:09 +0200

    refactor

--- rhino-tools/src/main/java/org/mozilla/javascript/tools/shell/ExecUtil.java
+++ rhino-tools/src/main/java/org/mozilla/javascript/tools/shell/ExecUtil.java
@@ -1,174 +1,211 @@
 package org.mozilla.javascript.tools.shell;
 
+import org.mozilla.javascript.Context;
+
+import java.io.Closeable;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import org.mozilla.javascript.Context;
 
 /**
- * Helper class for Global process execution.
+ * Helper class for <code>global.runCommand</code>.
  *
  * @author Roland Praml, Foconis Analytics GmbH
  */
 class ExecUtil {
-    private ExecUtil() {}
+	private ExecUtil() {
+	}
 
-    private static class PipeThread extends Thread {
 
-        PipeThread(boolean fromProcess, InputStream from, OutputStream to) {
-            setDaemon(true);
-            this.fromProcess = fromProcess;
-            this.from = from;
-            this.to = to;
-        }
+	/**
+	 * Runs the given process using Runtime.exec(). If any of in, out, err is null, the
+	 * corresponding process stream will be closed immediately, otherwise it will be closed as soon
+	 * as all data will be read from/written to process
+	 *
+	 * @return Exit value of process.
+	 * @throws IOException If there was an error executing the process.
+	 */
+	static int runProcess(
+			String[] cmd,
+			String[] environment,
+			File wd,
+			InputStream in,
+			OutputStream out,
+			OutputStream err)
+			throws IOException {
+		Process p = Runtime.getRuntime().exec(cmd, environment, wd);
 
-        @Override
-        public void run() {
-            try {
-                int n;
-                do {
-                    n = read();
-                    if (n < 0) {
-                        break;
-                    }
-                } while (write(n));
-            } catch (IOException ex) {
-                throw Context.throwAsScriptRuntimeEx(ex);
+		try (
+				PipeThread inThread = createToProcessThread(in, p.getOutputStream());
+				PipeThread outThread = createFromProcessthread(p.getInputStream(), out);
+				PipeThread errThread = createFromProcessthread(p.getErrorStream(), err);
+		) {
 
-            } finally {
-                close();
-            }
-        }
+			try {
+				// wait for process completion
+				p.waitFor();
+				stopThread(inThread);
+				stopThread(outThread);
+				stopThread(errThread);
+			} catch (InterruptedException ignore) {
+				Thread.currentThread().interrupt();
+			}
+			return p.exitValue();
+		} finally {
+			p.destroy();
+		}
+	}
 
-        private void close() {
-            try {
-                if (fromProcess) {
-                    from.close();
-                } else {
-                    to.close();
-                }
-            } catch (IOException ex) {
-                // Ignore errors on close. On Windows JVM may throw invalid
-                // refrence exception if process terminates too fast.
-            }
-        }
+	private static PipeThread createToProcessThread(InputStream in, OutputStream out) throws IOException {
+		if (in == null) {
+			out.close();
+			return null;
+		} else {
+			return new ToProcessPipeThread(in, out);
+		}
+	}
 
-        /**
-         * reads the inputStream. If the 'from' is the a process stream (reading stdOut/stdErr from
-         * process), we ignore read-errors (when process may have closed the pipe)
-         *
-         * @return number of read bytes or -1.
-         */
-        private int read() throws IOException {
-            try {
-                return from.read(buffer);
-            } catch (IOException ex) {
-                // Ignore exception as it can be cause by closed pipe
-                if (fromProcess) {
-                    return -1;
-                } else {
-                    throw ex;
-                }
-            }
-        }
+	private static PipeThread createFromProcessthread(InputStream in, OutputStream out) throws IOException {
+		if (out == null) {
+			in.close();
+			return null;
+		} else {
+			return new FromProcessPipeThread(in, out);
+		}
+	}
 
-        /**
-         * reads the inputStream. If the 'to' is the a process stream (writing to stdIn of process),
-         * we ignore write-errors (when process may have closed the pipe)
-         *
-         * @return read bytes or -1.
-         */
-        private boolean write(int n) throws IOException {
-            try {
-                to.write(buffer, 0, n);
-                to.flush();
-            } catch (IOException ex) {
-                if (fromProcess) {
-                    throw ex;
-                } else {
-                    // to is a process stream -> Ignore exception as it can be cause by closed pipe
-                    return false;
-                }
-            }
-            return true;
-        }
+	private static void stopThread(PipeThread thread) throws InterruptedException {
+		if (thread != null) {
+			thread.join();
+		}
+	}
 
-        private static final int SIZE = 4096;
-        private final byte[] buffer = new byte[SIZE];
-        private final boolean fromProcess;
-        private final InputStream from;
-        private final OutputStream to;
-    }
+	private static Throwable handleException(PipeThread thread, Throwable currentException) {
+		if (thread == null || thread.error == null) {
+			return currentException;
+		} else if (currentException == null) {
+			return thread.error;
+		} else {
+			currentException.addSuppressed(thread.error);
+			return currentException;
+		}
+	}
 
-    /**
-     * Runs the given process using Runtime.exec(). If any of in, out, err is null, the
-     * corresponding process stream will be closed immediately, otherwise it will be closed as soon
-     * as all data will be read from/written to process
-     *
-     * @return Exit value of process.
-     * @throws IOException If there was an error executing the process.
-     */
-    static int runProcess(
-            String[] cmd,
-            String[] environment,
-            File wd,
-            InputStream in,
-            OutputStream out,
-            OutputStream err)
-            throws IOException {
-        Process p = Runtime.getRuntime().exec(cmd, environment, wd);
+	/**
+	 * A PipeThread copies data from <code>from</code> to <code>to</code>. There are two implementations which are slightly different in stream closing and exception handling
+	 * <ul>
+	 *     <li><b>FromProcessPipeThread:</b> Reads from the process (stdOut/stdErr)</li>
+	 *     <li><b>ToProcessPipeThread:</b> Writes to the process (stdIn)</li>
+	 * </ul>
+	 */
+	private abstract static class PipeThread extends Thread implements Closeable {
 
-        PipeThread inThread = null;
-        PipeThread outThread = null;
-        PipeThread errThread = null;
-        try {
-            if (in != null) {
-                inThread = new PipeThread(false, in, p.getOutputStream());
-                inThread.start();
-            } else {
-                p.getOutputStream().close();
-            }
+		PipeThread(InputStream from, OutputStream to) {
+			setDaemon(true);
+			this.from = from;
+			this.to = to;
+		}
 
-            if (out != null) {
-                outThread = new PipeThread(true, p.getInputStream(), out);
-                outThread.start();
-            } else {
-                p.getInputStream().close();
-            }
+		@Override
+		public final void run() {
+			try {
+				copyStream();
+				to.flush();
+			} catch (Throwable t) {
+				if (t instanceof InterruptedException) {
+					// ignore
+				} else {
+					error = t;
+				}
+			} finally {
+				try {
+					closeStream();
+				} catch (IOException ignore) {
+					// Ignore errors on close. On Windows JVM may throw invalid
+					// refrence exception if process terminates too fast.
+				}
+			}
+		}
 
-            if (err != null) {
-                errThread = new PipeThread(true, p.getErrorStream(), err);
-                errThread.start();
-            } else {
-                p.getErrorStream().close();
-            }
+		abstract void copyStream() throws IOException;
 
-            try {
-                // wait for process completion
-                p.waitFor();
-            } catch (InterruptedException ignore) {
-                Thread.currentThread().interrupt();
-            }
-            return p.exitValue();
-        } finally {
-            for (; ; ) {
-                try {
-                    if (outThread != null) {
-                        outThread.join();
-                    }
-                    if (inThread != null) {
-                        inThread.join();
-                    }
-                    if (errThread != null) {
-                        errThread.join();
-                    }
-                    break;
-                } catch (InterruptedException ignore) {
-                    Thread.currentThread().interrupt();
-                }
-            }
-            p.destroy();
-        }
-    }
+		abstract void closeStream() throws IOException;
+
+		@Override
+		public void close() {
+			try {
+				join();
+			} catch (InterruptedException e) {
+				// ignore
+			}
+			if (error != null) {
+				Context.throwAsScriptRuntimeEx(error);
+			}
+		}
+
+		private static final int SIZE = 8192; // aligned with the default buffer size in BufferedWriter
+		final byte[] buffer = new byte[SIZE];
+		final OutputStream to;
+		final InputStream from;
+		Throwable error;
+	}
+
+	private static class FromProcessPipeThread extends PipeThread {
+		public FromProcessPipeThread(InputStream from, OutputStream to) {
+			super(from, to);
+		}
+
+		@Override
+		public void copyStream() throws IOException {
+			int n;
+			for (; ; ) {
+				try {
+					n = from.read(buffer);
+				} catch (IOException ex) {
+					// Ignore exception as it can be cause by closed pipe
+					break;
+				}
+				if (n < 0) {
+					break;
+				}
+				to.write(buffer, 0, n);
+			}
+		}
+
+		@Override
+		void closeStream() throws IOException {
+			from.close();
+		}
+	}
+
+	private static class ToProcessPipeThread extends PipeThread {
+		public ToProcessPipeThread(InputStream from, OutputStream to) {
+			super(from, to);
+		}
+
+		@Override
+		public void copyStream() throws IOException {
+			int n;
+			for (; ; ) {
+				n = from.read(buffer);
+				if (n < 0) {
+					break;
+				}
+				try {
+					to.write(buffer, 0, n);
+				} catch (IOException ex) {
+					// Ignore exception as it can be cause by closed pipe
+					break;
+				}
+			}
+		}
+
+		@Override
+		void closeStream() throws IOException {
+			to.close();
+		}
+	}
+
 }
+
