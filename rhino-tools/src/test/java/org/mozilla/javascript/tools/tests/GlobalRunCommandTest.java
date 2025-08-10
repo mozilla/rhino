f80e05f985f0d293e7cb51edbe3c4bde1d74623c
Roland Praml <pram@gmx.de>
2025-08-10 10:03:12 +0200

    Refactored test class

--- rhino-tools/src/test/java/org/mozilla/javascript/tools/tests/GlobalRunCommandTest.java
+++ rhino-tools/src/test/java/org/mozilla/javascript/tools/tests/GlobalRunCommandTest.java
@@ -7,12 +7,14 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.mozilla.javascript.Context;
+import org.mozilla.javascript.WrapFactory;
 import org.mozilla.javascript.WrappedException;
 import org.mozilla.javascript.testutils.Utils;
 import org.mozilla.javascript.tools.shell.Global;
@@ -27,27 +29,42 @@ public class GlobalRunCommandTest {
     private static final boolean isWindows =
             System.getProperty("os.name").toLowerCase().contains("win");
 
-    // https://stackoverflow.com/questions/52330841/command-which-copies-stdin-to-stdout
     private static final String STDIN_TO_STDOUT =
-            isWindows
-                    ? "runCommand('powershell.exe', '-c', '[Console]::OpenStandardInput().CopyTo([Console]::OpenStandardOutput())', { input: stdIn, output: stdOut, err: stdErr })"
-                    : "runCommand('/usr/bin/cat', { input: stdIn, output: stdOut, err: stdErr })";
+            "runCommand('stdinToStdout', { input: stdIn, output: stdOut, err: stdErr, commandExecutor: commandExecutor })";
     private static final String STDIN_TO_STDERR =
-            isWindows
-                    ? "runCommand('powershell.exe', '-c', '[Console]::OpenStandardInput().CopyTo([Console]::OpenStandardError())', { input: stdIn, output: stdOut, err: stdErr })"
-                    : "runCommand('/usr/bin/env', 'bash', '-c', 'cat >&2', { input: stdIn, output: stdOut, err: stdErr })";
+            "runCommand('stdinToStderr', { input: stdIn, output: stdOut, err: stdErr, commandExecutor: commandExecutor })";
+
+    private static Global.CommandExecutor getExecutor() {
+        return isWindows ? new ExecutorWindows() : new ExecutorLinux();
+    }
+
+    /**
+     * returns a new global object with optional installed streams
+     *
+     * @param cx the context
+     * @param streams the streams (stdIn, stdOut, stdErr)
+     */
+    private static Global getGlobal(Context cx, Object... streams) {
+        Global g = new Global(cx);
+        WrapFactory wf = cx.getWrapFactory();
+        g.put("commandExecutor", g, wf.wrap(cx, g, getExecutor(), Global.CommandExecutor.class));
+        if (streams.length >= 1 && streams[0] != null)
+            g.put("stdIn", g, wf.wrap(cx, g, streams[0], streams[0].getClass()));
+        if (streams.length >= 2 && streams[1] != null)
+            g.put("stdOut", g, wf.wrap(cx, g, streams[1], streams[1].getClass()));
+        if (streams.length >= 3 && streams[2] != null)
+            g.put("stdErr", g, wf.wrap(cx, g, streams[2], streams[2].getClass()));
+        return g;
+    }
 
     /** This is a simple 'hello world' test and tests the process return value. */
     @Test
     public void test() {
-        String cmd =
-                isWindows
-                        ? "runCommand('cmd.exe', '/c', 'echo Hello World')"
-                        : "runCommand('/usr/bin/env', 'bash', '-c', 'echo Hello World')";
+        String cmd = "runCommand('helloWorld', { commandExecutor: commandExecutor })";
         Utils.runWithAllModes(
                 cx -> {
                     cx.setLanguageVersion(Context.VERSION_ES6);
-                    var g = new Global(cx);
+                    var g = getGlobal(cx);
                     var result = cx.evaluateString(g, cmd, "test.js", 1, null);
                     assertInstanceOf(Number.class, result);
                     assertEquals(0, ((Number) result).intValue());
@@ -62,20 +79,16 @@ public class GlobalRunCommandTest {
     @Test
     public void testReturnOutput() {
         String cmd =
-                isWindows
-                        ? "var x = { output:'alreadyThere'}; runCommand('cmd.exe', '/c', 'echo Hello World', x); x.output"
-                        : "var x = { output:'alreadyThere'};  runCommand('/usr/bin/env', 'bash', '-c', 'echo Hello World', x); x.output";
+                "var x = { output:'alreadyThere', commandExecutor: commandExecutor };\n"
+                        + "runCommand('helloWorld', x); x.output";
         Utils.runWithAllModes(
                 cx -> {
                     cx.setLanguageVersion(Context.VERSION_ES6);
-                    var g = new Global(cx);
+                    var g = getGlobal(cx);
                     var result = cx.evaluateString(g, cmd, "test.js", 1, null);
                     assertInstanceOf(String.class, result);
-                    if (isWindows) {
-                        assertEquals("alreadyThereHello World\r\n", result);
-                    } else {
-                        assertEquals("alreadyThereHello World\n", result);
-                    }
+                    result = result.toString().replace("\r\n", "\n"); // normalize crlf
+                    assertEquals("alreadyThereHello World\n", result);
                     return null;
                 });
     }
@@ -86,15 +99,12 @@ public class GlobalRunCommandTest {
     @Test
     public void testWithOutputStream() {
         String cmd =
-                isWindows
-                        ? "runCommand('cmd.exe', '/c', 'echo Hello World', { output: stdOut})"
-                        : "runCommand('/usr/bin/env', 'bash', '-c', 'echo Hello World', { output: stdOut})";
+                "runCommand('helloWorld', { output: stdOut, commandExecutor: commandExecutor })";
         Utils.runWithAllModes(
                 cx -> {
                     cx.setLanguageVersion(Context.VERSION_ES6);
-                    var g = new Global(cx);
                     ByteArrayOutputStream stdOut = new ByteArrayOutputStream();
-                    g.put("stdOut", g, cx.getWrapFactory().wrap(cx, g, stdOut, OutputStream.class));
+                    var g = getGlobal(cx, null, stdOut);
                     var result = cx.evaluateString(g, cmd, "test.js", 1, null);
                     assertInstanceOf(Number.class, result);
                     assertEquals(0, ((Number) result).intValue());
@@ -103,11 +113,7 @@ public class GlobalRunCommandTest {
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
-                    if (isWindows) {
-                        assertEquals("Hello World\r\n", stdOut.toString());
-                    } else {
-                        assertEquals("Hello World\n", stdOut.toString());
-                    }
+                    assertEquals("Hello World\n", stdOut.toString().replace("\r\n", "\n"));
                     return null;
                 });
     }
@@ -119,13 +125,10 @@ public class GlobalRunCommandTest {
         Utils.runWithAllModes(
                 cx -> {
                     cx.setLanguageVersion(Context.VERSION_ES6);
-                    var g = new Global(cx);
                     ByteArrayInputStream stdIn = new ByteArrayInputStream("Hello World".getBytes());
                     ByteArrayOutputStream stdOut = new ByteArrayOutputStream();
                     ByteArrayOutputStream stdErr = new ByteArrayOutputStream();
-                    g.put("stdIn", g, cx.getWrapFactory().wrap(cx, g, stdIn, InputStream.class));
-                    g.put("stdOut", g, cx.getWrapFactory().wrap(cx, g, stdOut, OutputStream.class));
-                    g.put("stdErr", g, cx.getWrapFactory().wrap(cx, g, stdErr, OutputStream.class));
+                    var g = getGlobal(cx, stdIn, stdOut, stdErr);
                     cx.evaluateString(g, STDIN_TO_STDOUT, "test.js", 1, null);
                     g.put("stdIn", g, "Some error");
                     cx.evaluateString(g, STDIN_TO_STDERR, "test.js", 1, null);
@@ -153,16 +156,8 @@ public class GlobalRunCommandTest {
         Utils.runWithMode(
                 cx -> {
                     cx.setLanguageVersion(Context.VERSION_ES6);
-                    var g = new Global(cx);
                     FakeOutputStream stdOut = new FakeOutputStream();
-
-                    g.put(
-                            "stdIn",
-                            g,
-                            cx.getWrapFactory()
-                                    .wrap(cx, g, new FakeInputStream(bytes), InputStream.class));
-                    g.put("stdOut", g, cx.getWrapFactory().wrap(cx, g, stdOut, OutputStream.class));
-                    g.put("stdErr", g, cx.getWrapFactory().wrap(cx, g, stdOut, OutputStream.class));
+                    var g = getGlobal(cx, new FakeInputStream(bytes), stdOut, stdOut);
                     var result = cx.evaluateString(g, STDIN_TO_STDOUT, "test.js", 1, null);
                     assertInstanceOf(Number.class, result);
                     assertEquals(0, ((Number) result).intValue());
@@ -183,14 +178,11 @@ public class GlobalRunCommandTest {
      */
     @Test
     public void testTimeout() {
-        String cmd =
-                isWindows
-                        ? "runCommand('ping',{ timeout: 500, args : ['127.0.0.1', '-n', 5] })"
-                        : "runCommand('/usr/bin/sleep',{ timeout: 500, args : [5] })";
+        String cmd = "runCommand('sleep5',{ timeout: 500, commandExecutor: commandExecutor })";
         Utils.runWithAllModes(
                 cx -> {
                     cx.setLanguageVersion(Context.VERSION_ES6);
-                    var g = new Global(cx);
+                    var g = getGlobal(cx);
                     long start = System.currentTimeMillis();
                     var result = cx.evaluateString(g, cmd, "test.js", 1, null);
                     long duration = System.currentTimeMillis() - start;
@@ -211,15 +203,8 @@ public class GlobalRunCommandTest {
         Utils.runWithAllModes(
                 cx -> {
                     cx.setLanguageVersion(Context.VERSION_ES6);
-                    var g = new Global(cx);
                     FakeOutputStream stdOut = new FakeOutputStream();
-                    g.put(
-                            "stdIn",
-                            g,
-                            cx.getWrapFactory()
-                                    .wrap(cx, g, new ThrowingInputStream(), InputStream.class));
-                    g.put("stdOut", g, cx.getWrapFactory().wrap(cx, g, stdOut, OutputStream.class));
-                    g.put("stdErr", g, cx.getWrapFactory().wrap(cx, g, stdOut, OutputStream.class));
+                    var g = getGlobal(cx, new ThrowingInputStream(), stdOut, stdOut);
 
                     assertThrows(
                             WrappedException.class,
@@ -244,19 +229,12 @@ public class GlobalRunCommandTest {
         Utils.runWithAllModes(
                 cx -> {
                     cx.setLanguageVersion(Context.VERSION_ES6);
-                    var g = new Global(cx);
-
-                    g.put("stdIn", g, "hello world");
-                    g.put(
-                            "stdOut",
-                            g,
-                            cx.getWrapFactory()
-                                    .wrap(cx, g, new ThrowingOutputStream(), OutputStream.class));
-                    g.put(
-                            "stdErr",
-                            g,
-                            cx.getWrapFactory()
-                                    .wrap(cx, g, new ThrowingOutputStream(), OutputStream.class));
+                    var g =
+                            getGlobal(
+                                    cx,
+                                    "hello world",
+                                    new ThrowingOutputStream(),
+                                    new ThrowingOutputStream());
 
                     assertThrows(
                             WrappedException.class,
@@ -294,22 +272,12 @@ public class GlobalRunCommandTest {
         Utils.runWithAllModes(
                 cx -> {
                     cx.setLanguageVersion(Context.VERSION_ES6);
-                    var g = new Global(cx);
                     FakeOutputStream stdOut = new FakeOutputStream();
                     FakeOutputStream stdErr = new FakeOutputStream();
-                    g.put(
-                            "stdIn",
-                            g,
-                            cx.getWrapFactory()
-                                    .wrap(cx, g, new FakeInputStream(tenGig), InputStream.class));
-                    g.put("stdOut", g, cx.getWrapFactory().wrap(cx, g, stdOut, OutputStream.class));
-                    g.put("stdErr", g, cx.getWrapFactory().wrap(cx, g, stdErr, OutputStream.class));
+                    var g = getGlobal(cx, new FakeInputStream(tenGig), stdOut, stdErr);
                     cx.evaluateString(g, STDIN_TO_STDOUT, "test.js", 1, null);
-                    g.put(
-                            "stdIn",
-                            g,
-                            cx.getWrapFactory()
-                                    .wrap(cx, g, new FakeInputStream(tenGig), InputStream.class));
+
+                    g = getGlobal(cx, new FakeInputStream(tenGig), stdOut, stdErr);
                     cx.evaluateString(g, STDIN_TO_STDERR, "test.js", 1, null);
 
                     try {
@@ -324,6 +292,7 @@ public class GlobalRunCommandTest {
                 });
     }
 
+    /** Stream produces a stream returning 'bytes' zeros. */
     static class FakeInputStream extends InputStream {
         long bytes;
 
@@ -356,6 +325,7 @@ public class GlobalRunCommandTest {
         }
     }
 
+    /** Stream counts the received bytes. */
     static class FakeOutputStream extends OutputStream {
         long bytes;
 
@@ -375,6 +345,7 @@ public class GlobalRunCommandTest {
         }
     }
 
+    /** Stream throws exception on read */
     static class ThrowingInputStream extends InputStream {
 
         @Override
@@ -383,11 +354,76 @@ public class GlobalRunCommandTest {
         }
     }
 
+    /** Stream throws exception on write */
     static class ThrowingOutputStream extends OutputStream {
 
         @Override
         public void write(int b) throws IOException {
             throw new IOException("ThrowingInputStream");
+        }
+    }
+
+    /**
+     * executor for linux based OS. It translates the command aliases to a specific linux command.
+     */
+    private static class ExecutorLinux implements Global.CommandExecutor {
+        @Override
+        public Process exec(String[] cmdarray, String[] envp, File dir) throws IOException {
+            switch (cmdarray[0]) {
+                case "helloWorld":
+                    cmdarray = new String[] {"/usr/bin/env", "bash", "-c", "echo Hello World"};
+                    break;
+                case "stdinToStdout":
+                    cmdarray = new String[] {"/usr/bin/cat"};
+                    break;
+                case "stdinToStderr":
+                    cmdarray = new String[] {"/usr/bin/env", "bash", "-c", "cat >&2"};
+                    break;
+                case "sleep5":
+                    cmdarray = new String[] {"/usr/bin/sleep", "5"};
+                    break;
+                default:
+                    throw new IllegalArgumentException(cmdarray[0]);
+            }
+            return Runtime.getRuntime().exec(cmdarray, envp, dir);
+        }
+    }
+
+    /**
+     * executor for windows based OS. It translates the command aliases to a specific linux command.
+     */
+    private static class ExecutorWindows implements Global.CommandExecutor {
+        @Override
+        public Process exec(String[] cmdarray, String[] envp, File dir) throws IOException {
+            switch (cmdarray[0]) {
+                case "helloWorld":
+                    cmdarray = new String[] {"cmd.exe", "/c", "echo Hello World"};
+                    break;
+                case "stdinToStdout":
+                    // https://stackoverflow.com/questions/52330841/command-which-copies-stdin-to-stdout
+                    cmdarray =
+                            new String[] {
+                                "powershell.exe",
+                                "-c",
+                                "[Console]::OpenStandardInput().CopyTo([Console]::OpenStandardOutput())"
+                            };
+                    break;
+                case "stdinToStderr":
+                    // https://stackoverflow.com/questions/52330841/command-which-copies-stdin-to-stdout
+                    cmdarray =
+                            new String[] {
+                                "powershell.exe",
+                                "-c",
+                                "[Console]::OpenStandardInput().CopyTo([Console]::OpenStandardError())"
+                            };
+                    break;
+                case "sleep5":
+                    cmdarray = new String[] {"ping", "127.0.0.1", "-n", "5"};
+                    break;
+                default:
+                    throw new IllegalArgumentException(cmdarray[0]);
+            }
+            return Runtime.getRuntime().exec(cmdarray, envp, dir);
         }
     }
 }
