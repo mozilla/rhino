8466f390c4b24675c6bbe617b75ea7ff065821a3
Roland Praml <pram@gmx.de>
2025-08-24 17:51:12 +0200

    Add real testcase

--- it-android/src/androidTest/java/org/mozilla/javascript/android/test/RhinoTest.java
+++ it-android/src/androidTest/java/org/mozilla/javascript/android/test/RhinoTest.java
@@ -1,16 +1,45 @@
 package org.mozilla.javascript.android.test;
 
+import static org.junit.Assert.assertTrue;
+
+import android.content.Context;
 import android.util.Log;
+
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mozilla.javascript.android.TestCase;
 
-@RunWith(AndroidJUnit4.class)
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@RunWith(Parameterized.class)
 public class RhinoTest {
+
+
+    @Parameterized.Parameter(value = 0)
+    public TestCase testCase;
+
 
     @Test
     public void test() {
-        Log.i("HelloWorldTest", "hello world from JUnit test with log");
-        System.out.println("hello world from JUnit test with sysout");
+        String s= testCase.run();
+        Log.i(testCase.toString(), s);
+        assertTrue(s.contains("success"));
+    }
+
+    @Parameterized.Parameters(name = "{index}, js={0}")
+    public static Collection<Object[]> suiteValues() throws IOException {
+        List<Object[]> result = new ArrayList<Object[]>();
+        Context androidContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        for (TestCase testCase : TestCase.getTestCases(androidContext)) {
+            result.add(new Object[]{testCase});
+        }
+        return result;
     }
 }
--- it-android/src/main/java/org/mozilla/javascript/android/MainActivity.java
+++ it-android/src/main/java/org/mozilla/javascript/android/MainActivity.java
@@ -3,43 +3,42 @@ package org.mozilla.javascript.android;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.List;
 import java.util.Map;
+
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.Scriptable;
 
 public class MainActivity extends Activity {
 
     private int k() {
-        return Math.min(3,1);
+        return Math.min(3, 1);
     }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Map<String, String> tests = TestFinder.listFiles(this);
         StringBuilder sb = new StringBuilder();
-        System.out.println("Warmup");
-        long start = System.currentTimeMillis();
-        int j=0;
-        for (int i = 0; i < 1_000_000; i++) {
-            j+= k();
-        }
+        try {
+            List<TestCase> tests = TestCase.getTestCases(this);
+            for (TestCase test : tests) {
+                if (sb.length() != 0) {
+                    sb.append('\n');
+                }
+                try {
+                    sb.append(test.toString() + ":" + test.run());
 
-        start = System.currentTimeMillis()-start;
-        sb.append(j + ": Took  " + start + " ms");
-        System.out.println(sb.toString());
-        tests.forEach(
-                (name, js) -> {
-                    System.out.println("Executing " + name);
-                    try (Context cx = Context.enter()) {
-                        Scriptable scope = cx.initStandardObjects();
-                        String jsCode = "3+5";
-                        Object result = cx.evaluateString(scope, js, name, 1, null);
-                        sb.append(name).append(":").append(result).append("\n");
-                    } catch (Exception e) {
-                        sb.append(name).append(":").append(e.getMessage()).append("\n");
-                    }
-                });
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    sb.append(test.toString() + ": FAIL" + e.getMessage());
+                }
+            }
+        } catch (IOException e) {
+            sb.append(e);
+        }
 
         TextView label = new TextView(this);
         label.setText(sb.toString());
