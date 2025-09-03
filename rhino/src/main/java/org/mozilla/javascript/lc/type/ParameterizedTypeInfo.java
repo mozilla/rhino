208cee3599282bf76f569cdb25947f9a34aef45a
ZZZank <3410764033@qq.com>
2025-09-03 21:09:37 +0800

    add `ParameterizedTypeInfo.extractConsolidationMapping()` for stage-2 type consolidation

--- rhino/src/main/java/org/mozilla/javascript/lc/type/ParameterizedTypeInfo.java
+++ rhino/src/main/java/org/mozilla/javascript/lc/type/ParameterizedTypeInfo.java
@@ -1,7 +1,10 @@
 package org.mozilla.javascript.lc.type;
 
 import java.lang.reflect.ParameterizedType;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import org.mozilla.javascript.FunctionObject;
 import org.mozilla.javascript.Scriptable;
 
@@ -27,6 +30,43 @@ public interface ParameterizedTypeInfo extends TypeInfo {
     default TypeInfo param(int index) {
         final var params = params();
         return index >= 0 && index < params.size() ? params.get(index) : TypeInfo.NONE;
+    }
+
+    /**
+     * Extract consolidation mapping based on {@link #params()} and {@link Class#getTypeParameters()}
+     * <p>
+     * Example: {@code type} is {@code List<String>}, calling {@snippet :
+     *  ((ParameterizedTypeInfo) type).extractConsolidationMapping(TypeInfoFactory.GLOBAL)
+     * } will give {@code E -> String}, where the {@code E} the type variable declared by {@link List}
+     */
+    default Map<VariableTypeInfo, TypeInfo> extractConsolidationMapping(TypeInfoFactory factory) {
+        var typeVariables = this.asClass().getTypeParameters();
+        var actualParams = this.params();
+
+        var len = typeVariables.length;
+        if (len != actualParams.size()) {
+            throw new IllegalStateException(String.format(
+                "Expecting %s type params for class '%s', but got %s",
+                len,
+                this.asClass().getName(),
+                actualParams.size()
+            ));
+        } else if (len == 0) {
+            throw new IllegalStateException(String.format(
+                "Base class '%s' is not a generic class",
+                this.asClass().getName()
+            ));
+        }
+
+        if (len == 1) {
+            return Map.of((VariableTypeInfo) factory.create(typeVariables[0]), actualParams.get(0));
+        }
+
+        var mapping = new HashMap<VariableTypeInfo, TypeInfo>();
+        for (int i = 0; i < len; i++) {
+            mapping.put((VariableTypeInfo) factory.create(typeVariables[i]), actualParams.get(i));
+        }
+        return mapping;
     }
 
     @Override
