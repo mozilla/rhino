2d79ddd7f841fd416e3cf25d9092dd5db39c781a
ZZZank <3410764033@qq.com>
2025-09-03 22:09:42 +0800

    add test for generic access for `List<String>`-like object

--- tests/src/test/java/org/mozilla/javascript/tests/type_info/TypeConsolidationApplyTest.java
+++ tests/src/test/java/org/mozilla/javascript/tests/type_info/TypeConsolidationApplyTest.java
@@ -12,36 +12,84 @@ import org.mozilla.javascript.ScriptableObject;
  */
 public class TypeConsolidationApplyTest {
 
+    /**
+     * Test for {@code SomeClass<SomeParamHere>}
+     */
     @ParameterizedTest
     @ValueSource(booleans = {true, false})
-    public void test(boolean interpretMode) {
+    public void testGeneric(boolean interpretMode) {
         try (var cx = ContextFactory.getGlobal().enterContext()) {
             cx.setInterpretedMode(interpretMode);
             var scope = cx.initStandardObjects();
 
-            var bridge = new StringImpl();
-            ScriptableObject.putProperty(scope, "bridge", Context.javaToJS(bridge, scope));
+            var o = new TestObj<>();
+            ScriptableObject.putProperty(scope, "bridge", Context.javaToJS(o, scope));
 
-            var script = "bridge.set(3.45);"; // number should be wrapped to string automatically
-            cx.evaluateString(scope, script, "TypeConsolidationApplyTest", 1, null);
+            // Double auto wrapped to String
+            cx.evaluateString(scope, "bridge.forString().set(3.45);", "test", 1, null);
+            Assertions.assertInstanceOf(String.class, o.value);
+            Assertions.assertEquals("3.45", o.value);
 
-            var got = bridge.value;
-            Assertions.assertInstanceOf(String.class, got);
-            Assertions.assertEquals("3.45", got);
+            // Double auto wrapped to Integer
+            cx.evaluateString(scope, "bridge.forInt().set(3.45);", "test", 1, null);
+            Assertions.assertInstanceOf(Integer.class, o.value);
+            Assertions.assertEquals(3, o.value);
+
+            // String auto wrapped to Double
+            cx.evaluateString(scope, "bridge.forDouble().set('3');", "test", 1, null);
+            Assertions.assertInstanceOf(Double.class, o.value);
+            Assertions.assertEquals(3.0, o.value);
         }
     }
 
-    public abstract static class GenericBase<T> {
+    /**
+     * Test for {@code SomeClassImpl extends SomeClass<SomeParamHere>}
+     */
+    @ParameterizedTest
+    @ValueSource(booleans = {true, false})
+    public void testGenericParent(boolean interpretMode) {
+        try (var cx = ContextFactory.getGlobal().enterContext()) {
+            cx.setInterpretedMode(interpretMode);
+            var scope = cx.initStandardObjects();
+
+            var o = new TestObjStr();
+            ScriptableObject.putProperty(scope, "bridge", Context.javaToJS(o, scope));
+
+            // Double auto wrapped to String
+            cx.evaluateString(scope, "bridge.set(3.45);", "test", 1, null);
+            Assertions.assertInstanceOf(String.class, o.value);
+            Assertions.assertEquals("3.45", o.value);
+        }
+    }
+
+    public static class TestObj<T> {
         protected Object value;
 
         public T get() {
-            return (T) value;
+            return cast(value);
         }
 
         public void set(T value) {
             this.value = value;
         }
+
+        public TestObj<String> forString() {
+            return cast(this);
+        }
+
+        public TestObj<Integer> forInt() {
+            return cast(this);
+        }
+
+        public TestObj<Double> forDouble() {
+            return cast(this);
+        }
     }
 
-    public static class StringImpl extends GenericBase<String> {}
+    public static class TestObjStr extends TestObj<String> {}
+
+    @SuppressWarnings("unchecked")
+    private static <T> T cast(Object o) {
+        return (T) o;
+    }
 }
