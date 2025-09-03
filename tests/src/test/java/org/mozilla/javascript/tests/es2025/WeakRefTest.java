53f46fe9c0c607fd81671ed37c58af023be5f426
Anivar A Aravind <ping@anivar.net>
2025-09-03 17:25:26 +0530

    Fix test parameter order and regenerate test262.properties
    - Fix Utils.assertWithAllModes_ES6 parameter order (expected, script)
    - Regenerate test262.properties with WeakRef tests passing
    
    Signed-off-by: Anivar A Aravind <ping@anivar.net>

--- tests/src/test/java/org/mozilla/javascript/tests/es2025/WeakRefTest.java
+++ tests/src/test/java/org/mozilla/javascript/tests/es2025/WeakRefTest.java
@@ -11,14 +11,14 @@ public class WeakRefTest {
 
     private void assertTypeError(String script) {
         Utils.assertWithAllModes_ES6(
-                "try { " + script + "; false; } catch(e) { e instanceof TypeError; }", true);
+                true, "try { " + script + "; false; } catch(e) { e instanceof TypeError; }");
     }
 
     @Test
     public void weakRefConstructor() {
         Utils.assertWithAllModes_ES6(
-                "var obj = { value: 42 }; var ref = new WeakRef(obj); ref instanceof WeakRef",
-                true);
+                true,
+                "var obj = { value: 42 }; var ref = new WeakRef(obj); ref instanceof WeakRef");
     }
 
     @Test
@@ -28,21 +28,21 @@ public class WeakRefTest {
                         + "var ref = new WeakRef(obj); "
                         + "var derefed = ref.deref(); "
                         + "derefed === obj && derefed.value === 42";
-        Utils.assertWithAllModes_ES6(script, true);
+        Utils.assertWithAllModes_ES6(true, script);
     }
 
     @Test
     public void weakRefDerefMultipleTimes() {
         Utils.assertWithAllModes_ES6(
-                "var obj = { value: 42 }; var ref = new WeakRef(obj); ref.deref() === ref.deref()",
-                true);
+                true,
+                "var obj = { value: 42 }; var ref = new WeakRef(obj); ref.deref() === ref.deref()");
     }
 
     @Test
     public void weakRefToStringTag() {
         Utils.assertWithAllModes_ES6(
-                "var ref = new WeakRef({}); Object.prototype.toString.call(ref)",
-                "[object WeakRef]");
+                "[object WeakRef]",
+                "var ref = new WeakRef({}); Object.prototype.toString.call(ref)");
     }
 
     @Test
@@ -78,51 +78,51 @@ public class WeakRefTest {
     @Test
     public void weakRefWithArray() {
         Utils.assertWithAllModes_ES6(
-                "var arr = [1,2,3]; var ref = new WeakRef(arr); ref.deref() === arr", true);
+                true, "var arr = [1,2,3]; var ref = new WeakRef(arr); ref.deref() === arr");
     }
 
     @Test
     public void weakRefWithFunction() {
         Utils.assertWithAllModes_ES6(
-                "var fn = function(){}; var ref = new WeakRef(fn); ref.deref() === fn", true);
+                true, "var fn = function(){}; var ref = new WeakRef(fn); ref.deref() === fn");
     }
 
     @Test
     public void weakRefWithRegExp() {
         Utils.assertWithAllModes_ES6(
-                "var rx = /test/; var ref = new WeakRef(rx); ref.deref() === rx", true);
+                true, "var rx = /test/; var ref = new WeakRef(rx); ref.deref() === rx");
     }
 
     @Test
     public void weakRefTypeofFunction() {
-        Utils.assertWithAllModes_ES6("typeof WeakRef === 'function'", true);
+        Utils.assertWithAllModes_ES6(true, "typeof WeakRef === 'function'");
     }
 
     @Test
     public void weakRefPrototypeDeref() {
-        Utils.assertWithAllModes_ES6("typeof WeakRef.prototype.deref === 'function'", true);
+        Utils.assertWithAllModes_ES6(true, "typeof WeakRef.prototype.deref === 'function'");
     }
 
     @Test
     public void weakRefConstructorLength() {
-        Utils.assertWithAllModes_ES6("WeakRef.length === 1", true);
+        Utils.assertWithAllModes_ES6(true, "WeakRef.length === 1");
     }
 
     @Test
     public void weakRefConstructorName() {
-        Utils.assertWithAllModes_ES6("WeakRef.name === 'WeakRef'", true);
+        Utils.assertWithAllModes_ES6(true, "WeakRef.name === 'WeakRef'");
     }
 
     @Test
     public void weakRefDerefLength() {
-        Utils.assertWithAllModes_ES6("WeakRef.prototype.deref.length === 0", true);
+        Utils.assertWithAllModes_ES6(true, "WeakRef.prototype.deref.length === 0");
     }
 
     @Test
     public void weakRefHasCorrectPrototype() {
         Utils.assertWithAllModes_ES6(
-                "var ref = new WeakRef({}); Object.getPrototypeOf(ref) === WeakRef.prototype",
-                true);
+                true,
+                "var ref = new WeakRef({}); Object.getPrototypeOf(ref) === WeakRef.prototype");
     }
 
     @Test
@@ -138,22 +138,22 @@ public class WeakRefTest {
     @Test
     public void weakRefConstructorPropertyDescriptor() {
         Utils.assertWithAllModes_ES6(
-                "var desc = Object.getOwnPropertyDescriptor(WeakRef.prototype, 'constructor'); desc.writable === true && desc.enumerable === false && desc.configurable === true",
-                true);
+                true,
+                "var desc = Object.getOwnPropertyDescriptor(WeakRef.prototype, 'constructor'); desc.writable === true && desc.enumerable === false && desc.configurable === true");
     }
 
     @Test
     public void weakRefDerefPropertyDescriptor() {
         Utils.assertWithAllModes_ES6(
-                "var desc = Object.getOwnPropertyDescriptor(WeakRef.prototype, 'deref'); desc.writable === true && desc.enumerable === false && desc.configurable === true",
-                true);
+                true,
+                "var desc = Object.getOwnPropertyDescriptor(WeakRef.prototype, 'deref'); desc.writable === true && desc.enumerable === false && desc.configurable === true");
     }
 
     @Test
     public void weakRefToStringTagDescriptor() {
         Utils.assertWithAllModes_ES6(
-                "var desc = Object.getOwnPropertyDescriptor(WeakRef.prototype, Symbol.toStringTag); desc.value === 'WeakRef' && desc.writable === false && desc.enumerable === false && desc.configurable === true",
-                true);
+                true,
+                "var desc = Object.getOwnPropertyDescriptor(WeakRef.prototype, Symbol.toStringTag); desc.value === 'WeakRef' && desc.writable === false && desc.enumerable === false && desc.configurable === true");
     }
 
     @Test
@@ -169,14 +169,14 @@ public class WeakRefTest {
     @Test
     public void weakRefWithManyArguments() {
         Utils.assertWithAllModes_ES6(
-                "var obj = {}; var ref = new WeakRef(obj, 'extra', 'args', 'ignored'); ref.deref() === obj",
-                true);
+                true,
+                "var obj = {}; var ref = new WeakRef(obj, 'extra', 'args', 'ignored'); ref.deref() === obj");
     }
 
     @Test
     public void weakRefDerefWithArguments() {
         Utils.assertWithAllModes_ES6(
-                "var obj = {}; var ref = new WeakRef(obj); ref.deref('arg1', 'arg2') === obj",
-                true);
+                true,
+                "var obj = {}; var ref = new WeakRef(obj); ref.deref('arg1', 'arg2') === obj");
     }
 }
