3b0505cbe00fa851403fb498d104f6476b06b781
vladimir.zhuravlev <vladimir.zhuravlev@servicenow.com>
2025-09-16 14:13:13 -0700

    Spotless fix

--- rhino/src/test/java/org/mozilla/javascript/tests/DuplicateProperties.java
+++ rhino/src/test/java/org/mozilla/javascript/tests/DuplicateProperties.java
@@ -4,16 +4,18 @@ import org.junit.Test;
 import org.mozilla.javascript.testutils.Utils;
 
 public class DuplicateProperties {
-	@Test
-	public void unterminatedCharacterClass() {
-		String script = "(function() {\n" +
-				"'use strict';\n" +
-				"const o = {foo: 1, foo: 2};\n" +
-				"return o.foo\n" +
-				"})();";
+    @Test
+    public void unterminatedCharacterClass() {
+        String script =
+                "(function() {\n"
+                        + "'use strict';\n"
+                        + "const o = {foo: 1, foo: 2};\n"
+                        + "return o.foo\n"
+                        + "})();";
 
-		Utils.assertEvaluatorException_1_8("Property \"foo\" already defined in this object literal", script);
+        Utils.assertEvaluatorException_1_8(
+                "Property \"foo\" already defined in this object literal", script);
 
-		Utils.assertWithAllModes_ES6(2, script);
-	}
+        Utils.assertWithAllModes_ES6(2, script);
+    }
 }
