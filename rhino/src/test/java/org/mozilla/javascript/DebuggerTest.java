277d4fbef889e661dea2517ed5aa2bfc3bcdb54f
Duncan MacGregor <duncan.macgregor@servicenow.com>
2025-10-03 22:43:26 +0100

    Add smoke tests for debugger and class compiler
    * Add a debugger smoke test.
    * Add a `ClassCompiler` smoke test.

---
+++ rhino/src/test/java/org/mozilla/javascript/DebuggerTest.java
@@ -0,0 +1,101 @@
+package org.mozilla.javascript;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mozilla.javascript.debug.DebugFrame;
+import org.mozilla.javascript.debug.DebuggableScript;
+import org.mozilla.javascript.debug.Debugger;
+
+public class DebuggerTest {
+    private static final String TEST_SCRIPT =
+            "function ScriptDebuggerTest() {\n"
+                    + "	this._sInternalAttribute = 'foobar';\n"
+                    + "}\n"
+                    + "\n"
+                    + "ScriptDebuggerTest.prototype.debug = function() {\n"
+                    + "	return \"Debug: \" + this._sInternalAttribute;\n"
+                    + "};\n"
+                    + "\n"
+                    + "new ScriptDebuggerTest().debug()";
+
+    private static final String GLOBAL = "global";
+    private static final String THIS_OBJ_ATTRIBUTE = "_sInternalAttribute";
+    static final int BREAKPOINT_LINE_OUTSIDE_FUNC = 8;
+    static final int BREAKPOINT_LINE_INSIDE_FUNC = 5;
+    static final int NUM_BREAKPOINTS = 2;
+
+    private static class MockDebugger implements Debugger {
+
+        private static MockDebugFrame frame;
+
+        public static synchronized MockDebugFrame getFrame() {
+            if (frame == null) frame = new MockDebugFrame();
+            return frame;
+        }
+
+        @Override
+        public void handleCompilationDone(Context cx, DebuggableScript fnOrScript, String source) {}
+
+        @Override
+        public DebugFrame getFrame(Context cx, DebuggableScript fnOrScript) {
+            return frame;
+        }
+    }
+
+    private static class MockDebugFrame implements DebugFrame {
+
+        private int breakPointsHit = 0;
+
+        public int getBreakPointsHit() {
+            return breakPointsHit;
+        }
+
+        public synchronized void incrementBreakPointsHit() {
+            ++breakPointsHit;
+        }
+
+        @Override
+        public void onEnter(Context cx, Scriptable activation, Scriptable thisObj, Object[] args) {}
+
+        @Override
+        public void onLineChange(Context cx, int line) {
+            // decide to break on a line and evaluate a command similar to what GlideScriptDebugger
+            // does
+            if (line == BREAKPOINT_LINE_INSIDE_FUNC || line == BREAKPOINT_LINE_OUTSIDE_FUNC) {
+                incrementBreakPointsHit();
+            }
+        }
+
+        @Override
+        public void onExceptionThrown(Context cx, Throwable ex) {}
+
+        @Override
+        public void onExit(Context cx, boolean byThrow, Object resultOrException) {}
+
+        @Override
+        public void onDebuggerStatement(Context cx) {}
+    }
+
+    @Test
+    public void testThis() {
+        MockDebugger debugger = new MockDebugger();
+        MockDebugFrame frame = MockDebugger.getFrame();
+        Context cx = Context.enter();
+        Scriptable scope = cx.initSafeStandardObjects();
+        try {
+            cx.setDebugger(debugger, cx);
+            cx.setGeneratingSource(true);
+            cx.setGeneratingDebug(true);
+            cx.setInterpretedMode(true);
+            Script script = cx.compileString(TEST_SCRIPT, "this-klass", 0, null);
+            Object res = script.exec(cx, scope, scope);
+            Assert.assertNotNull(res);
+            if (frame.getBreakPointsHit() != NUM_BREAKPOINTS)
+                Assert.fail("Breakpoints weren't hit");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            Context.exit();
+        }
+    }
+}
