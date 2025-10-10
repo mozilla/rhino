f97ef57d38e0a8b9c0793438e1c4e5896239edbf
Greg Brail <gbrail@users.noreply.github.com>
2025-10-09 21:32:53 -0700

    Use custom string-building logic

--- rhino/src/main/java/org/mozilla/javascript/dtoa/Decimal.java
+++ rhino/src/main/java/org/mozilla/javascript/dtoa/Decimal.java
@@ -5,6 +5,8 @@ public class Decimal {
     private final String digits;
     private final int decimalPoint;
     private final boolean negative;
+    private int length;
+    private char[] buf;
 
     Decimal(String d, int p, boolean n) {
         this.digits = d;
@@ -18,96 +20,113 @@ public class Decimal {
      */
     @Override
     public String toString() {
-        if (decimalPoint > -6 && decimalPoint <= 21) {
+        if (decimalPoint >= -5 && decimalPoint <= 21) {
             return toStringFixed();
         }
         return toStringExponential();
     }
 
     private String toStringFixed() {
-        // Performance notes: as of Java 21, since this method is sensitive:
-        //   * Doing string concatenation right in the code is generally faster if
-        //     all the strings are constants or can be easily calculated (i.e. substring)
-        //   * Using StringBuilder is generally faster if we have to pad
-        //     with an arbitrary number of zeroes.
         int numDigits = digits.length();
         if (numDigits == decimalPoint) {
             if (negative) {
                 return "-" + digits;
             }
             return digits;
-        } else if (decimalPoint <= 0) {
-            // 0.[000]digits
-            StringBuilder s = new StringBuilder(numDigits - decimalPoint + 3);
-            if (negative) {
-                s.append('-');
-            }
-            s.append("0.");
-            makeZeroes(s, -decimalPoint);
-            s.append(digits);
-            trimZeroes(s);
-            return s.toString();
-        } else if (decimalPoint < numDigits) {
-            // dig.its
-            String s;
-            if (negative) {
-                s = "-" + digits.substring(0, decimalPoint) + '.' + digits.substring(decimalPoint);
-            } else {
-                s = digits.substring(0, decimalPoint) + '.' + digits.substring(decimalPoint);
-            }
-            return trimZeroes(s);
-        } else {
-            // digits[000]
-            StringBuilder s = new StringBuilder(numDigits + decimalPoint + 1);
-            if (negative) {
-                s.append('-');
-            }
-            s.append(digits);
-            makeZeroes(s, decimalPoint - numDigits);
-            return s.toString();
         }
+        if (decimalPoint <= 0) {
+            return toStringSmall(numDigits);
+        }
+        if (decimalPoint < numDigits) {
+            return toStringMedium(numDigits);
+        }
+        return toStringBig(numDigits);
+    }
+
+    private String toStringSmall(int numDigits) {
+        // Length for -, 0., extra zeroes, digits
+        int extraZeroes = -decimalPoint;
+        buf = new char[numDigits + extraZeroes + 4];
+        if (negative) {
+            append('-');
+        }
+        append('0');
+        append('.');
+        appendZeroes(extraZeroes);
+        append(digits);
+        trimZeroes();
+        return makeString();
+    }
+
+    private String toStringMedium(int numDigits) {
+        // Length for -, ., digits
+        buf = new char[numDigits + 2];
+        if (negative) {
+            append('-');
+        }
+        append(digits.substring(0, decimalPoint));
+        append('.');
+        append(digits.substring(decimalPoint));
+        trimZeroes();
+        return makeString();
+    }
+
+    private String toStringBig(int numDigits) {
+        int extraZeroes = decimalPoint - numDigits;
+        // Room for -, zeroes, digits
+        buf = new char[numDigits + extraZeroes + 2];
+        if (negative) {
+            append('-');
+        }
+        append(digits);
+        appendZeroes(extraZeroes);
+        return makeString();
     }
 
     private String toStringExponential() {
+        // Length for -, ., e+, three digits
+        buf = new char[digits.length() + 8];
         int scientificExponent = decimalPoint - 1;
-        String exp = scientificExponent >= 0 ? "e+" + scientificExponent : "e" + scientificExponent;
-        String fraction = trimZeroes(digits.substring(1));
         if (negative) {
-            if (fraction.isEmpty()) {
-                return "-" + digits.charAt(0) + exp;
-            }
-            return "-" + digits.charAt(0) + "." + fraction + exp;
+            append('-');
         }
-        if (fraction.isEmpty()) {
-            return digits.charAt(0) + exp;
+        append(digits.charAt(0));
+        append('.');
+        append(digits.substring(1));
+        trimZeroes();
+        append('e');
+        if (scientificExponent >= 0) {
+            append('+');
         }
-        return digits.charAt(0) + "." + fraction + exp;
+        append(Integer.toString(scientificExponent));
+        return makeString();
     }
 
-    private static void makeZeroes(StringBuilder s, int count) {
+    private void appendZeroes(int count) {
         for (int i = 0; i < count; i++) {
-            s.append('0');
+            append('0');
         }
     }
 
-    private static String trimZeroes(String s) {
-        int last = s.length() - 1;
-        while (last >= 0 && s.charAt(last) == '0') {
-            last--;
+    private void trimZeroes() {
+        while (length > 0 && buf[length - 1] == '0') {
+            length--;
         }
-        if (last != s.length() - 1) {
-            return s.substring(0, last + 1);
+        if (length > 0 && buf[length - 1] == '.') {
+            length--;
         }
-        return s;
     }
 
-    private static void trimZeroes(StringBuilder s) {
-        int last = s.length() - 1;
-        while (last >= 0 && s.charAt(last) == '0') {
-            last--;
-        }
-        if (last != s.length() - 1) {
-            s.setLength(last + 1);
-        }
+    private void append(char ch) {
+        buf[length++] = ch;
+    }
+
+    private void append(String s) {
+        s.getChars(0, s.length(), buf, length);
+        length += s.length();
+    }
+
+    private String makeString() {
+        return new String(buf, 0, length);
     }
 }
