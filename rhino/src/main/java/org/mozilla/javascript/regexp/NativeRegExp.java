dd21565c7e0b9c24c345f3455160d7b782183316
Balaji Rao <balaji.rao@servicenow.com>
2025-10-17 13:13:38 +0200

    Case-insensitive matching with anchor

--- rhino/src/main/java/org/mozilla/javascript/regexp/NativeRegExp.java
+++ rhino/src/main/java/org/mozilla/javascript/regexp/NativeRegExp.java
@@ -3601,6 +3601,10 @@ public class NativeRegExp extends IdScriptableObject {
                         if (matchCodePoint == anchorCodePoint) {
                             break;
                         }
+                        if ((gData.regexp.flags & JSREG_FOLD) != 0
+                                && unicodeCaseInsensitiveEquals(matchCodePoint, anchorCodePoint)) {
+                            break;
+                        }
                         charCount = Character.charCount(matchCodePoint);
                     } else {
                         char matchCh = input.charAt(i);
