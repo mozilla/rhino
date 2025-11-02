f7341b47fdc00239ac7b10664e0f4dc9c80edc30
Greg Brail <gbrail@users.noreply.github.com>
2025-11-02 13:46:30 -0500

    Replace number formatting operations on Number
    This replaces the implementations of toExponential, toFixed,
    and toPrecision on the Number class with new implementations that use
    a combination of BigDecimal and the new double formatting code.

--- rhino/src/test/java/org/mozilla/javascript/tests/NumberToStringTest.java
+++ rhino/src/test/java/org/mozilla/javascript/tests/NumberToStringTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.mozilla.javascript.ScriptRuntime;
+import org.mozilla.javascript.dtoa.DecimalFormatter;
 
 public class NumberToStringTest {
 
@@ -189,7 +190,7 @@ public class NumberToStringTest {
     }
 
     /**
-     * Read a list of 10,000 doubles and their toString results as parsed in node.js, and compare
+     * Read a list of 100,000 doubles and their toString results as parsed in node.js, and compare
      * that our results match.
      */
     @Test
@@ -215,14 +216,159 @@ public class NumberToStringTest {
         }
     }
 
-    private String trimTrailingZeroes(String s) {
-        int len = s.length();
-        while (len > 0 && s.charAt(len - 1) == '0') {
-            len--;
+    @Test
+    public void testManyToFixed() throws IOException {
+        try (var is =
+                NumberToStringTest.class.getResourceAsStream(
+                        "/org/mozilla/javascript/tests/to-fixed-numbers.csv")) {
+            assertNotNull(is);
+            var rdr = new BufferedReader(new InputStreamReader(is));
+            int lineNum = 1;
+            for (String line = rdr.readLine(); line != null; line = rdr.readLine()) {
+                String[] fields = line.split(",", 3);
+                // If not two fields, must be a comment
+                if (fields.length != 3) {
+                    continue;
+                }
+                double val = Double.parseDouble(fields[0].trim());
+                int numDigits = Integer.parseInt(fields[1].trim());
+                String expected = fields[2].trim();
+                assertEquals(expected, DecimalFormatter.toFixed(val, numDigits));
+                lineNum++;
+            }
+            assertTrue(lineNum > 1, "Expected at least one data line");
         }
-        if (len != s.length()) {
-            return s.substring(0, len);
+    }
+
+    @Test
+    public void testManyToPrecision() throws IOException {
+        try (var is =
+                NumberToStringTest.class.getResourceAsStream(
+                        "/org/mozilla/javascript/tests/to-precision-numbers.csv")) {
+            assertNotNull(is);
+            var rdr = new BufferedReader(new InputStreamReader(is));
+            int lineNum = 1;
+            for (String line = rdr.readLine(); line != null; line = rdr.readLine()) {
+                String[] fields = line.split(",", 3);
+                // If not two fields, must be a comment
+                if (fields.length != 3) {
+                    continue;
+                }
+                double val = Double.parseDouble(fields[0].trim());
+                int numDigits = Integer.parseInt(fields[1].trim());
+                String expected = fields[2].trim();
+                assertEquals(expected, DecimalFormatter.toPrecision(val, numDigits));
+                lineNum++;
+            }
+            assertTrue(lineNum > 1, "Expected at least one data line");
         }
-        return s;
+    }
+
+    private static final Object[][] CONVERT_TESTS = {
+        // order: source, argument, to exponential, to fixed, to precision
+        {0.0, 0, "0e+0", "0", ""},
+        {0.0, 1, "0.0e+0", "0.0", "0"},
+        {0.0, 2, "0.00e+0", "0.00", "0.0"},
+        {-0, 1, "0.0e+0", "0.0", "0"},
+        {
+            0.0,
+            100,
+            "0.0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000e+0",
+            "0.0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+            "0.000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
+        },
+        {1.0, 0, "1e+0", "1", ""},
+        {1.0, 1, "1.0e+0", "1.0", "1"},
+        {1.0, 10, "1.0000000000e+0", "1.0000000000", "1.000000000"},
+        {2.5, 0, "3e+0", "3", ""},
+        {1.005, 2, "1.00e+0", "1.00", "1.0"},
+        {100.0, 2, "1.00e+2", "100.00", "1.0e+2"},
+        {0.9999, 3, "9.999e-1", "1.000", "1.00"},
+        {0.000001, 1, "1.0e-6", "0.0", "0.000001"},
+        {123.456, 0, "1e+2", "123", ""},
+        {123.456, 1, "1.2e+2", "123.5", "1e+2"},
+        {123.456, 2, "1.23e+2", "123.46", "1.2e+2"},
+        {123.456, 10, "1.2345600000e+2", "123.4560000000", "123.4560000"},
+        {-123.456, 10, "-1.2345600000e+2", "-123.4560000000", "-123.4560000"},
+        {1E23, 4, "1.0000e+23", "1e+23", "1.000e+23"},
+        {100000000000000000000001.0, 5, "1.00000e+23", "1.0000000000000001e+23", "1.0000e+23"},
+        {Math.PI * 100000000000.0, 5, "3.14159e+11", "314159265358.97931", "3.1416e+11"},
+        {
+            Math.PI * 100000000000.0,
+            29,
+            "3.14159265358979309082031250000e+11",
+            "314159265358.97930908203125000000000000000",
+            "314159265358.97930908203125000"
+        },
+        {Math.PI / 100000000000000.0, 4, "3.1416e-14", "0.0000", "3.142e-14"},
+        {5.88E-39, 1, "5.9e-39", "0.0", "6e-39"},
+        {
+            5.88E-39,
+            72,
+            "5.879999999999999682121697891273120514358604938932618538527697337758246910e-39",
+            "0.000000000000000000000000000000000000005879999999999999682121697891273121",
+            "5.87999999999999968212169789127312051435860493893261853852769733775824691e-39"
+        },
+    };
+
+    private static Object[][] getConvertParams() {
+        return CONVERT_TESTS;
+    }
+
+    @ParameterizedTest
+    @MethodSource("getConvertParams")
+    public void testToExponential(
+            double v, int arg, String expected, String ignore1, String ignore2) {
+        assertEquals(expected, DecimalFormatter.toExponential(v, arg));
+    }
+
+    @ParameterizedTest
+    @MethodSource("getConvertParams")
+    public void testToFixed(
+            double v, int fractionDigits, String ignore1, String expected, String ignore2) {
+        assertEquals(expected, DecimalFormatter.toFixed(v, fractionDigits));
+    }
+
+    @ParameterizedTest
+    @MethodSource("getConvertParams")
+    public void testToPrecision(
+            double v, int precision, String ignore1, String ignore2, String expected) {
+        if (precision >= 1) {
+            assertEquals(expected, DecimalFormatter.toPrecision(v, precision));
+        }
+    }
+
+    private static final Object[][] UNDEF_TESTS = {
+        // order: source, to exponential, to fixed, to precision
+        {0.0, "0e+0", "0", "0"},
+        {0.9999, "9.999e-1", "1", "0.9999"},
+        {100.0, "1e+2", "100", "100"},
+        {-123.456, "-1.23456e+2", "-123", "-123.456"},
+        {3.141592653589793, "3.141592653589793e+0", "3", "3.141592653589793"},
+        {1.234567E3, "1.234567e+3", "1235", "1234.567"}
+    };
+
+    private static Object[][] getUndefParams() {
+        return UNDEF_TESTS;
+    }
+
+    @ParameterizedTest
+    @MethodSource("getUndefParams")
+    public void testToExponentialUndef(double v, String expected, String ignore1, String ignore2) {
+        assertEquals(expected, DecimalFormatter.toExponential(v, -1));
+    }
+
+    @ParameterizedTest
+    @MethodSource("getUndefParams")
+    public void testToFixedUndef(double v, String ignore1, String expected, String ignore2) {
+        // An underfined value to toFixed translates to a parameter of 0
+        assertEquals(expected, DecimalFormatter.toFixed(v, 0));
+    }
+
+    @ParameterizedTest
+    @MethodSource("getUndefParams")
+    public void testToPrecisionUndef(double v, String ignore1, String ignore2, String expected) {
+        // An undefined value to toPrecision is the same as toString
+        assertEquals(expected, ScriptRuntime.toString(v));
     }
 }
