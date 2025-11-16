ff86a3330ed2a90b626fd9548a40568e766ab3be
ZZZank <3410764033@qq.com>
2025-11-16 22:06:09 +0800

    expand JavaAdapterTest

--- rhino/src/test/java/org/mozilla/javascript/tests/JavaAdapterTest.java
+++ rhino/src/test/java/org/mozilla/javascript/tests/JavaAdapterTest.java
@@ -1,6 +1,8 @@
 package org.mozilla.javascript.tests;
 
+import java.util.function.Predicate;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mozilla.javascript.Context;
@@ -22,6 +24,10 @@ public class JavaAdapterTest {
         Context.exit();
     }
 
+    private Object eval(String script) {
+        return cx.evaluateString(topScope, script, "JavaAdapterTest", 0, null);
+    }
+
     interface C {
         int methodInC(String str);
     }
@@ -31,17 +37,64 @@ public class JavaAdapterTest {
     public abstract class A implements B {}
 
     @Test
-    public void overrideMethodInMultiLayerInterface() throws NoSuchMethodException {
-        String testCode =
-                "JavaAdapter(Packages." + A.class.getName() + ",{methodInC:function(){}},null)";
-
-        NativeJavaObject adapterObject =
-                (NativeJavaObject) cx.evaluateString(topScope, testCode, "", 1, null);
-
+    public void overrideMethodInMultiLayerInterface() {
         // if the method 'methodInC' is overrided from 'interface C',
         // its signature will be 'public int methodInC(java.lang.String)'  (expected result),
         // otherwise if the method 'methodInC' is newly created by JavaAdapter,
         // its signature will be 'public java.lang.Object methodInC()'
-        adapterObject.unwrap().getClass().getDeclaredMethod("methodInC", String.class);
+        String testCode =
+                "JavaAdapter(Packages."
+                        + A.class.getName()
+                        + ",{methodInC:function(str){ return Number(str); }}"
+                        + ",null)";
+
+        var adapterObject = (NativeJavaObject) eval(testCode);
+        var adapted = (C) adapterObject.unwrap();
+        Assert.assertEquals(123, adapted.methodInC("123"));
+    }
+
+    @Test
+    public void ignoreOriginalArgs() {
+        String testCode =
+                "JavaAdapter(Packages."
+                        + A.class.getName()
+                        + ",{methodInC:function(){ return 42; }}" // args are ignored
+                        + ",null)";
+
+        var adapterObject = (NativeJavaObject) eval(testCode);
+        var adapted = (C) adapterObject.unwrap();
+        Assert.assertEquals(42, adapted.methodInC("whatever string"));
+    }
+
+    public abstract static class StaticA implements B {}
+
+    @Test
+    public void implementStaticClass() {
+        String testCode =
+                "JavaAdapter(Packages."
+                        + StaticA.class.getName()
+                        + ",{methodInC:function(){ return 42; }}"
+                        + ")"; // no args required
+
+        var adapterObject = (NativeJavaObject) eval(testCode);
+        var adapted = (C) adapterObject.unwrap();
+        Assert.assertEquals(42, adapted.methodInC("whatever string"));
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void implementInterface() {
+        String testCode =
+                "JavaAdapter(Packages."
+                        + Predicate.class.getName()
+                        + ",{test:function(obj){ return 'rhino' == obj; }}"
+                        + ")"; // no args required since superclass is Object
+
+        var adapterObject = (NativeJavaObject) eval(testCode);
+        var adapted = (Predicate<Object>) adapterObject.unwrap();
+        Assert.assertTrue(adapted.test("rhino"));
+        Assert.assertFalse(adapted.test("rhizo"));
+        Assert.assertFalse(adapted.test(null));
+        Assert.assertFalse(adapted.test(42));
     }
 }
