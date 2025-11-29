e6ce5a670891cf331bda57826543e129fcee07c3
anivar <ping@anivar.net>
2025-11-29 18:59:12 +0000

    Implement ES2022 RegExp hasIndices (d flag)
    Add full support for ES2022 RegExp d flag which enables hasIndices property
    and indices array in match results.
    
    Changes:
    - Added JSREG_HASINDICES flag constant
    - Implemented RegExp.prototype.hasIndices getter
    - Generate match.indices array with start/end positions for:
      - Overall match (indices[0])
      - Numbered capture groups (indices[1..n])
      - Named capture groups (indices.groups.name)
      - Undefined for unmatched groups
    - Added flag validation for d compatibility
    - Added ES2024 v flag recognition (unicodeSets)
    - Added v flag validation (mutually exclusive with u, incompatible with i)
    
    ES2024 v flag set operations (-- and &&) are recognized but not yet
    implemented. Tests for set operations marked as @Ignore pending comprehensive
    character class parser rewrite.
    
    Tests:
    - 25 comprehensive tests for d flag functionality (all passing)
    - 5 tests for v flag set operations (disabled, requires parser rewrite)

--- tests/src/test/java/org/mozilla/javascript/tests/es2022/RegExpHasIndicesTest.java
+++ tests/src/test/java/org/mozilla/javascript/tests/es2022/RegExpHasIndicesTest.java
@@ -122,4 +122,166 @@ public class RegExpHasIndicesTest {
                         + "}";
         Utils.assertWithAllModes(true, script);
     }
+
+    // ES2022 hasIndices ('d' flag) functionality tests
+
+    @Test
+    public void testIndicesPropertyPresent() {
+        String script =
+                "var re = /test/d;"
+                        + "var match = re.exec('this is a test');"
+                        + "match.indices !== undefined";
+        Utils.assertWithAllModes(true, script);
+    }
+
+    @Test
+    public void testIndicesPropertyAbsentWithoutDFlag() {
+        String script =
+                "var re = /test/;"
+                        + "var match = re.exec('this is a test');"
+                        + "match.indices === undefined";
+        Utils.assertWithAllModes(true, script);
+    }
+
+    @Test
+    public void testIndicesOverallMatch() {
+        String script =
+                "var re = /test/d;"
+                        + "var match = re.exec('this is a test');"
+                        + "match.indices[0][0] === 10 && match.indices[0][1] === 14";
+        Utils.assertWithAllModes(true, script);
+    }
+
+    @Test
+    public void testIndicesCaptureGroups() {
+        String script =
+                "var re = /(\\d{4})-(\\d{2})-(\\d{2})/d;"
+                        + "var match = re.exec('Date: 2025-11-29');"
+                        + "match.indices[1][0] === 6 && match.indices[1][1] === 10 &&"
+                        + "match.indices[2][0] === 11 && match.indices[2][1] === 13 &&"
+                        + "match.indices[3][0] === 14 && match.indices[3][1] === 16";
+        Utils.assertWithAllModes(true, script);
+    }
+
+    @Test
+    public void testIndicesNamedCaptureGroups() {
+        String script =
+                "var re = /(?<year>\\d{4})-(?<month>\\d{2})/d;"
+                        + "var match = re.exec('2025-11');"
+                        + "match.indices.groups.year[0] === 0 && match.indices.groups.year[1] === 4 &&"
+                        + "match.indices.groups.month[0] === 5 && match.indices.groups.month[1] === 7";
+        Utils.assertWithAllModes(true, script);
+    }
+
+    @Test
+    public void testIndicesUndefinedForUnmatchedGroups() {
+        String script =
+                "var re = /(a)|(b)/d;"
+                        + "var match = re.exec('a');"
+                        + "match.indices[1] !== undefined && match.indices[2] === undefined";
+        Utils.assertWithAllModes(true, script);
+    }
+
+    @Test
+    public void testIndicesWithGlobalFlag() {
+        String script =
+                "var re = /(\\d+)/gd;"
+                        + "var str = '1 22 333';"
+                        + "var m1 = re.exec(str);"
+                        + "var m2 = re.exec(str);"
+                        + "var m3 = re.exec(str);"
+                        + "m1.indices[0][0] === 0 && m1.indices[0][1] === 1 &&"
+                        + "m2.indices[0][0] === 2 && m2.indices[0][1] === 4 &&"
+                        + "m3.indices[0][0] === 5 && m3.indices[0][1] === 8";
+        Utils.assertWithAllModes(true, script);
+    }
+
+    @Test
+    public void testIndicesMatchStructure() {
+        String script =
+                "var re = /(test)/d;"
+                        + "var match = re.exec('test');"
+                        + "Array.isArray(match.indices) && "
+                        + "Array.isArray(match.indices[0]) && "
+                        + "Array.isArray(match.indices[1]) && "
+                        + "match.indices[0].length === 2 && "
+                        + "match.indices[1].length === 2";
+        Utils.assertWithAllModes(true, script);
+    }
+
+    @Test
+    public void testIndicesGroupsStructure() {
+        String script =
+                "var re = /(?<name>\\w+)/d;"
+                        + "var match = re.exec('test');"
+                        + "typeof match.indices.groups === 'object' && "
+                        + "Array.isArray(match.indices.groups.name) && "
+                        + "match.indices.groups.name.length === 2";
+        Utils.assertWithAllModes(true, script);
+    }
+
+    // ES2024 unicodeSets ('v' flag) functionality tests
+
+    // TODO: Set operations (-- and &&) require a complete rewrite of the character class parser
+    // to properly handle ES2024 set notation syntax. These tests are disabled pending that work.
+    @org.junit.Ignore(
+            "Set operations not yet implemented - requires character class parser rewrite")
+    @Test
+    public void testVFlagSetSubtraction() {
+        String script =
+                "var re = /[a-z--[aeiou]]/v;"
+                        + "re.test('b') && re.test('z') && !re.test('a') && !re.test('e')";
+        Utils.assertWithAllModes(true, script);
+    }
+
+    @org.junit.Ignore(
+            "Set operations not yet implemented - requires character class parser rewrite")
+    @Test
+    public void testVFlagSetIntersection() {
+        String script =
+                "var re = /[a-z&&[a-m]]/v;"
+                        + "re.test('a') && re.test('m') && !re.test('n') && !re.test('z')";
+        Utils.assertWithAllModes(true, script);
+    }
+
+    @org.junit.Ignore(
+            "Set operations not yet implemented - requires character class parser rewrite")
+    @Test
+    public void testVFlagMultipleOperations() {
+        String script =
+                "var re = /[a-z--[aeiou]--[xyz]]/v;"
+                        + "re.test('b') && !re.test('a') && !re.test('x')";
+        Utils.assertWithAllModes(true, script);
+    }
+
+    @org.junit.Ignore(
+            "Set operations not yet implemented - requires character class parser rewrite")
+    @Test
+    public void testVFlagWithDigits() {
+        String script =
+                "var re = /[\\d--[5-9]]/v;" + "re.test('0') && re.test('4') && !re.test('5')";
+        Utils.assertWithAllModes(true, script);
+    }
+
+    @org.junit.Ignore(
+            "Set operations not yet implemented - requires character class parser rewrite")
+    @Test
+    public void testVFlagComplexExpression() {
+        String script =
+                "var re = /[a-z&&[^aeiou]]/v;" // Letters that are not vowels (consonants)
+                        + "re.test('b') && re.test('z') && !re.test('a') && !re.test('e')";
+        Utils.assertWithAllModes(true, script);
+    }
+
+    @Test
+    public void testVFlagSetOperationWithoutVFlagFails() {
+        String script =
+                "try {"
+                        + "  eval('var re = /[a--b]/;');"
+                        + "  false;"
+                        + "} catch(e) {"
+                        + "  e instanceof SyntaxError;"
+                        + "}";
+        Utils.assertWithAllModes(true, script);
+    }
 }
