mozilla/rhino

commit 88adf9937bde6a1d7108e6ac74e57ea8c688fcf7

Migrated `NativeConsole` away from `IdScriptableObject`
* Migrated `NativeConsole` away from `IdScriptableObject`

---------

Co-authored-by: andrea.bergia <andrea.bergia@servicenow.com>

rhino/src/main/java/org/mozilla/javascript/NativeConsole.java
@@ -14,10 +14,10 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class NativeConsole extends IdScriptableObject {
+public class NativeConsole extends ScriptableObject {
     private static final long serialVersionUID = 5694613212458273057L;
 
-    private static final Object CONSOLE_TAG = "Console";
+    private static final String CLASS_NAME = "Console";
 
     private static final String DEFAULT_LABEL = "default";
 
@@ -48,9 +48,31 @@ public class NativeConsole extends IdScriptableObject {
 
     public static void init(Scriptable scope, boolean sealed, ConsolePrinter printer) {
         NativeConsole obj = new NativeConsole(printer);
-        obj.activatePrototypeMap(MAX_ID);
         obj.setPrototype(getObjectPrototype(scope));
         obj.setParentScope(scope);
+
+        obj.defineProperty(
+                scope, "toSource", 0, NativeConsole::js_toSource, 0, DONTENUM | READONLY);
+        obj.defineBuiltinProperty(
+                scope, "trace", 1, NativeConsole::js_trace, 0, DONTENUM | READONLY);
+        obj.defineBuiltinProperty(
+                scope, "debug", 1, NativeConsole::js_debug, 0, DONTENUM | READONLY);
+        obj.defineBuiltinProperty(scope, "log", 1, NativeConsole::js_log, 0, DONTENUM | READONLY);
+        obj.defineBuiltinProperty(scope, "info", 1, NativeConsole::js_info, 0, DONTENUM | READONLY);
+        obj.defineBuiltinProperty(scope, "warn", 1, NativeConsole::js_warn, 0, DONTENUM | READONLY);
+        obj.defineBuiltinProperty(
+                scope, "error", 1, NativeConsole::js_error, 0, DONTENUM | READONLY);
+        obj.defineBuiltinProperty(
+                scope, "assert", 2, NativeConsole::js_assert, 0, DONTENUM | READONLY);
+        obj.defineBuiltinProperty(
+                scope, "count", 1, NativeConsole::js_count, 0, DONTENUM | READONLY);
+        obj.defineBuiltinProperty(
+                scope, "countReset", 1, NativeConsole::js_countReset, 0, DONTENUM | READONLY);
+        obj.defineBuiltinProperty(scope, "time", 1, NativeConsole::js_time, 0, DONTENUM | READONLY);
+        obj.defineBuiltinProperty(
+                scope, "timeEnd", 1, NativeConsole::js_timeEnd, 0, DONTENUM | READONLY);
+        obj.defineBuiltinProperty(
+                scope, "timeLog", 2, NativeConsole::js_timeLog, 0, DONTENUM | READONLY);
         if (sealed) {
             obj.sealObject();
         }
@@ -63,141 +85,91 @@ public class NativeConsole extends IdScriptableObject {
 
     @Override
     public String getClassName() {
-        return "Console";
+        return CLASS_NAME;
     }
 
-    @Override
-    protected void initPrototypeId(int id) {
-        if (id > LAST_METHOD_ID) {
-            throw new IllegalStateException(String.valueOf(id));
-        }
-
-        String name;
-        int arity;
-        switch (id) {
-            case Id_toSource:
-                arity = 0;
-                name = "toSource";
-                break;
-            case Id_trace:
-                arity = 1;
-                name = "trace";
-                break;
-            case Id_debug:
-                arity = 1;
-                name = "debug";
-                break;
-            case Id_log:
-                arity = 1;
-                name = "log";
-                break;
-            case Id_info:
-                arity = 1;
-                name = "info";
-                break;
-            case Id_warn:
-                arity = 1;
-                name = "warn";
-                break;
-            case Id_error:
-                arity = 1;
-                name = "error";
-                break;
-            case Id_assert:
-                arity = 2;
-                name = "assert";
-                break;
-            case Id_count:
-                arity = 1;
-                name = "count";
-                break;
-            case Id_countReset:
-                arity = 1;
-                name = "countReset";
-                break;
-            case Id_time:
-                arity = 1;
-                name = "time";
-                break;
-            case Id_timeEnd:
-                arity = 1;
-                name = "timeEnd";
-                break;
-            case Id_timeLog:
-                arity = 2;
-                name = "timeLog";
-                break;
-            default:
-                throw new IllegalStateException(String.valueOf(id));
-        }
-        initPrototypeMethod(CONSOLE_TAG, id, name, arity);
+    private static NativeConsole realThis(Scriptable thisObj, String methodName) {
+        return LambdaConstructor.ensureType(thisObj, NativeConsole.class, methodName);
     }
 
-    @Override
-    public Object execIdCall(
-            IdFunctionObject f, Context cx, Scriptable scope, Scriptable thisObj, Object[] args) {
-        if (!f.hasTag(CONSOLE_TAG)) {
-            return super.execIdCall(f, cx, scope, thisObj, args);
-        }
+    private static Object js_toSource(
+            Context cx, Scriptable scope, Scriptable thisObj, Object[] args) {
+        realThis(thisObj, "toSource");
+        return CLASS_NAME;
+    }
 
-        int methodId = f.methodId();
-        switch (methodId) {
-            case Id_toSource:
-                return "Console";
+    private static Object js_trace(
+            Context cx, Scriptable scope, Scriptable thisObj, Object[] args) {
+        var console = realThis(thisObj, "trace");
+        ScriptStackElement[] stack = new EvaluatorException("[object Object]").getScriptStack();
+        console.printer.print(cx, scope, Level.TRACE, args, stack);
+        return Undefined.instance;
+    }
 
-            case Id_trace:
-                {
-                    ScriptStackElement[] stack =
-                            new EvaluatorException("[object Object]").getScriptStack();
-                    printer.print(cx, scope, Level.TRACE, args, stack);
-                    break;
-                }
+    private static Object js_debug(
+            Context cx, Scriptable scope, Scriptable thisObj, Object[] args) {
+        var console = realThis(thisObj, "debug");
+        console.printer.print(cx, scope, Level.DEBUG, args, null);
+        return Undefined.instance;
+    }
 
-            case Id_debug:
-                printer.print(cx, scope, Level.DEBUG, args, null);
-                break;
+    private static Object js_log(Context cx, Scriptable scope, Scriptable thisObj, Object[] args) {
+        var console = realThis(thisObj, "log");
+        console.printer.print(cx, scope, Level.INFO, args, null);
+        return Undefined.instance;
+    }
 
-            case Id_log:
-            case Id_info:
-                printer.print(cx, scope, Level.INFO, args, null);
-                break;
+    private static Object js_info(Context cx, Scriptable scope, Scriptable thisObj, Object[] args) {
+        var console = realThis(thisObj, "info");
+        console.printer.print(cx, scope, Level.INFO, args, null);
+        return Undefined.instance;
+    }
 
-            case Id_warn:
-                printer.print(cx, scope, Level.WARN, args, null);
-                break;
+    private static Object js_warn(Context cx, Scriptable scope, Scriptable thisObj, Object[] args) {
+        var console = realThis(thisObj, "warn");
+        console.printer.print(cx, scope, Level.WARN, args, null);
+        return Undefined.instance;
+    }
 
-            case Id_error:
-                printer.print(cx, scope, Level.ERROR, args, null);
-                break;
+    private static Object js_error(
+            Context cx, Scriptable scope, Scriptable thisObj, Object[] args) {
+        var console = realThis(thisObj, "error");
+        console.printer.print(cx, scope, Level.ERROR, args, null);
+        return Undefined.instance;
+    }
 
-            case Id_assert:
-                jsAssert(cx, scope, args);
-                break;
+    private static Object js_assert(
+            Context cx, Scriptable scope, Scriptable thisObj, Object[] args) {
+        realThis(thisObj, "assert").jsAssert(cx, scope, args);
+        return Undefined.instance;
+    }
 
-            case Id_count:
-                count(cx, scope, args);
-                break;
+    private static Object js_count(
+            Context cx, Scriptable scope, Scriptable thisObj, Object[] args) {
+        realThis(thisObj, "assert").count(cx, scope, args);
+        return Undefined.instance;
+    }
 
-            case Id_countReset:
-                countReset(cx, scope, args);
-                break;
+    private static Object js_countReset(
+            Context cx, Scriptable scope, Scriptable thisObj, Object[] args) {
+        realThis(thisObj, "countReset").countReset(cx, scope, args);
+        return Undefined.instance;
+    }
 
-            case Id_time:
-                time(cx, scope, args);
-                break;
+    private static Object js_time(Context cx, Scriptable scope, Scriptable thisObj, Object[] args) {
+        realThis(thisObj, "time").time(cx, scope, args);
+        return Undefined.instance;
+    }
 
-            case Id_timeEnd:
-                timeEnd(cx, scope, args);
-                break;
+    private static Object js_timeEnd(
+            Context cx, Scriptable scope, Scriptable thisObj, Object[] args) {
+        realThis(thisObj, "timeEnd").timeEnd(cx, scope, args);
+        return Undefined.instance;
+    }
 
-            case Id_timeLog:
-                timeLog(cx, scope, args);
-                break;
-
-            default:
-                throw new IllegalStateException(String.valueOf(methodId));
-        }
-
+    private static Object js_timeLog(
+            Context cx, Scriptable scope, Scriptable thisObj, Object[] args) {
+        realThis(thisObj, "timeLog").timeLog(cx, scope, args);
         return Undefined.instance;
     }
 
@@ -461,70 +433,4 @@ public class NativeConsole extends IdScriptableObject {
     private double nano2Milli(Long nano) {
         return nano / 1000000D;
     }
-
-    @Override
-    protected int findPrototypeId(String s) {
-        int id;
-        switch (s) {
-            case "log":
-                id = Id_log;
-                break;
-            case "info":
-                id = Id_info;
-                break;
-            case "time":
-                id = Id_time;
-                break;
-            case "warn":
-                id = Id_warn;
-                break;
-            case "count":
-                id = Id_count;
-                break;
-            case "debug":
-                id = Id_debug;
-                break;
-            case "error":
-                id = Id_error;
-                break;
-            case "trace":
-                id = Id_trace;
-                break;
-            case "assert":
-                id = Id_assert;
-                break;
-            case "timeEnd":
-                id = Id_timeEnd;
-                break;
-            case "timeLog":
-                id = Id_timeLog;
-                break;
-            case "toSource":
-                id = Id_toSource;
-                break;
-            case "countReset":
-                id = Id_countReset;
-                break;
-            default:
-                id = 0;
-                break;
-        }
-        return id;
-    }
-
-    private static final int Id_toSource = 1,
-            Id_trace = 2,
-            Id_debug = 3,
-            Id_log = 4,
-            Id_info = 5,
-            Id_warn = 6,
-            Id_error = 7,
-            Id_assert = 8,
-            Id_count = 9,
-            Id_countReset = 10,
-            Id_time = 11,
-            Id_timeEnd = 12,
-            Id_timeLog = 13,
-            LAST_METHOD_ID = 13,
-            MAX_ID = 13;
 }

commit d058209cbba3b99e6eebc0bd72511e1ee64a0900

Fix: allow `console` methods to be called without a `this`
This fixes a regression introduced in
88adf9937bde6a1d7108e6ac74e57ea8c688fcf7, and allows code like this:

```js
var fn = console.error;
fn('hello');
```

Closes #2184

rhino/src/main/java/org/mozilla/javascript/NativeConsole.java
@@ -54,25 +54,89 @@ public class NativeConsole extends ScriptableObject {
         obj.defineProperty(
                 scope, "toSource", 0, NativeConsole::js_toSource, 0, DONTENUM | READONLY);
         obj.defineBuiltinProperty(
-                scope, "trace", 1, NativeConsole::js_trace, 0, DONTENUM | READONLY);
+                scope,
+                "trace",
+                1,
+                (cx, callScope, ignoredThis, args) -> obj.js_trace(cx, callScope, args),
+                0,
+                DONTENUM | READONLY);
         obj.defineBuiltinProperty(
-                scope, "debug", 1, NativeConsole::js_debug, 0, DONTENUM | READONLY);
-        obj.defineBuiltinProperty(scope, "log", 1, NativeConsole::js_log, 0, DONTENUM | READONLY);
-        obj.defineBuiltinProperty(scope, "info", 1, NativeConsole::js_info, 0, DONTENUM | READONLY);
-        obj.defineBuiltinProperty(scope, "warn", 1, NativeConsole::js_warn, 0, DONTENUM | READONLY);
+                scope,
+                "debug",
+                1,
+                (cx, callScope, ignoredThis, args) -> obj.js_debug(cx, callScope, args),
+                0,
+                DONTENUM | READONLY);
         obj.defineBuiltinProperty(
-                scope, "error", 1, NativeConsole::js_error, 0, DONTENUM | READONLY);
+                scope,
+                "log",
+                1,
+                (cx, callScope, ignoredThis, args) -> obj.js_log(cx, callScope, args),
+                0,
+                DONTENUM | READONLY);
         obj.defineBuiltinProperty(
-                scope, "assert", 2, NativeConsole::js_assert, 0, DONTENUM | READONLY);
+                scope,
+                "info",
+                1,
+                (cx, callScope, ignoredThis, args) -> obj.js_info(cx, callScope, args),
+                0,
+                DONTENUM | READONLY);
         obj.defineBuiltinProperty(
-                scope, "count", 1, NativeConsole::js_count, 0, DONTENUM | READONLY);
+                scope,
+                "warn",
+                1,
+                (cx, callScope, ignoredThis, args) -> obj.js_warn(cx, callScope, args),
+                0,
+                DONTENUM | READONLY);
         obj.defineBuiltinProperty(
-                scope, "countReset", 1, NativeConsole::js_countReset, 0, DONTENUM | READONLY);
-        obj.defineBuiltinProperty(scope, "time", 1, NativeConsole::js_time, 0, DONTENUM | READONLY);
+                scope,
+                "error",
+                1,
+                (cx, callScope, ignoredThis, args) -> obj.js_error(cx, callScope, args),
+                0,
+                DONTENUM | READONLY);
         obj.defineBuiltinProperty(
-                scope, "timeEnd", 1, NativeConsole::js_timeEnd, 0, DONTENUM | READONLY);
+                scope,
+                "assert",
+                2,
+                (cx, callScope, ignoredThis, args) -> obj.js_assert(cx, callScope, args),
+                0,
+                DONTENUM | READONLY);
         obj.defineBuiltinProperty(
-                scope, "timeLog", 2, NativeConsole::js_timeLog, 0, DONTENUM | READONLY);
+                scope,
+                "count",
+                1,
+                (cx, callScope, ignoredThis, args) -> obj.js_count(cx, callScope, args),
+                0,
+                DONTENUM | READONLY);
+        obj.defineBuiltinProperty(
+                scope,
+                "countReset",
+                1,
+                (cx, callScope, ignoredThis, args) -> obj.js_countReset(cx, callScope, args),
+                0,
+                DONTENUM | READONLY);
+        obj.defineBuiltinProperty(
+                scope,
+                "time",
+                1,
+                (cx, callScope, ignoredThis, args) -> obj.js_time(cx, callScope, args),
+                0,
+                DONTENUM | READONLY);
+        obj.defineBuiltinProperty(
+                scope,
+                "timeEnd",
+                1,
+                (cx, callScope, ignoredThis, args) -> obj.js_timeEnd(cx, callScope, args),
+                0,
+                DONTENUM | READONLY);
+        obj.defineBuiltinProperty(
+                scope,
+                "timeLog",
+                2,
+                (cx, callScope, ignoredThis, args) -> obj.js_timeLog(cx, callScope, args),
+                0,
+                DONTENUM | READONLY);
         if (sealed) {
             obj.sealObject();
         }
@@ -88,88 +152,69 @@ public class NativeConsole extends ScriptableObject {
         return CLASS_NAME;
     }
 
-    private static NativeConsole realThis(Scriptable thisObj, String methodName) {
-        return LambdaConstructor.ensureType(thisObj, NativeConsole.class, methodName);
-    }
-
     private static Object js_toSource(
             Context cx, Scriptable scope, Scriptable thisObj, Object[] args) {
-        realThis(thisObj, "toSource");
         return CLASS_NAME;
     }
 
-    private static Object js_trace(
-            Context cx, Scriptable scope, Scriptable thisObj, Object[] args) {
-        var console = realThis(thisObj, "trace");
+    private Object js_trace(Context cx, Scriptable scope, Object[] args) {
         ScriptStackElement[] stack = new EvaluatorException("[object Object]").getScriptStack();
-        console.printer.print(cx, scope, Level.TRACE, args, stack);
+        printer.print(cx, scope, Level.TRACE, args, stack);
         return Undefined.instance;
     }
 
-    private static Object js_debug(
-            Context cx, Scriptable scope, Scriptable thisObj, Object[] args) {
-        var console = realThis(thisObj, "debug");
-        console.printer.print(cx, scope, Level.DEBUG, args, null);
+    private Object js_debug(Context cx, Scriptable scope, Object[] args) {
+        printer.print(cx, scope, Level.DEBUG, args, null);
         return Undefined.instance;
     }
 
-    private static Object js_log(Context cx, Scriptable scope, Scriptable thisObj, Object[] args) {
-        var console = realThis(thisObj, "log");
-        console.printer.print(cx, scope, Level.INFO, args, null);
+    private Object js_log(Context cx, Scriptable scope, Object[] args) {
+        printer.print(cx, scope, Level.INFO, args, null);
         return Undefined.instance;
     }
 
-    private static Object js_info(Context cx, Scriptable scope, Scriptable thisObj, Object[] args) {
-        var console = realThis(thisObj, "info");
-        console.printer.print(cx, scope, Level.INFO, args, null);
+    private Object js_info(Context cx, Scriptable scope, Object[] args) {
+        printer.print(cx, scope, Level.INFO, args, null);
         return Undefined.instance;
     }
 
-    private static Object js_warn(Context cx, Scriptable scope, Scriptable thisObj, Object[] args) {
-        var console = realThis(thisObj, "warn");
-        console.printer.print(cx, scope, Level.WARN, args, null);
+    private Object js_warn(Context cx, Scriptable scope, Object[] args) {
+        printer.print(cx, scope, Level.WARN, args, null);
         return Undefined.instance;
     }
 
-    private static Object js_error(
-            Context cx, Scriptable scope, Scriptable thisObj, Object[] args) {
-        var console = realThis(thisObj, "error");
-        console.printer.print(cx, scope, Level.ERROR, args, null);
+    private Object js_error(Context cx, Scriptable scope, Object[] args) {
+        printer.print(cx, scope, Level.ERROR, args, null);
         return Undefined.instance;
     }
 
-    private static Object js_assert(
-            Context cx, Scriptable scope, Scriptable thisObj, Object[] args) {
-        realThis(thisObj, "assert").jsAssert(cx, scope, args);
+    private Object js_assert(Context cx, Scriptable scope, Object[] args) {
+        jsAssert(cx, scope, args);
         return Undefined.instance;
     }
 
-    private static Object js_count(
-            Context cx, Scriptable scope, Scriptable thisObj, Object[] args) {
-        realThis(thisObj, "assert").count(cx, scope, args);
+    private Object js_count(Context cx, Scriptable scope, Object[] args) {
+        count(cx, scope, args);
         return Undefined.instance;
     }
 
-    private static Object js_countReset(
-            Context cx, Scriptable scope, Scriptable thisObj, Object[] args) {
-        realThis(thisObj, "countReset").countReset(cx, scope, args);
+    private Object js_countReset(Context cx, Scriptable scope, Object[] args) {
+        countReset(cx, scope, args);
         return Undefined.instance;
     }
 
-    private static Object js_time(Context cx, Scriptable scope, Scriptable thisObj, Object[] args) {
-        realThis(thisObj, "time").time(cx, scope, args);
+    private Object js_time(Context cx, Scriptable scope, Object[] args) {
+        time(cx, scope, args);
         return Undefined.instance;
     }
 
-    private static Object js_timeEnd(
-            Context cx, Scriptable scope, Scriptable thisObj, Object[] args) {
-        realThis(thisObj, "timeEnd").timeEnd(cx, scope, args);
+    private Object js_timeEnd(Context cx, Scriptable scope, Object[] args) {
+        timeEnd(cx, scope, args);
         return Undefined.instance;
     }
 
-    private static Object js_timeLog(
-            Context cx, Scriptable scope, Scriptable thisObj, Object[] args) {
-        realThis(thisObj, "timeLog").timeLog(cx, scope, args);
+    private Object js_timeLog(Context cx, Scriptable scope, Object[] args) {
+        timeLog(cx, scope, args);
         return Undefined.instance;
     }
 

tests/src/test/java/org/mozilla/javascript/tests/NativeConsoleTest.java
@@ -18,6 +18,7 @@ import org.mozilla.javascript.NativeConsole;
 import org.mozilla.javascript.NativeConsole.Level;
 import org.mozilla.javascript.ScriptStackElement;
 import org.mozilla.javascript.Scriptable;
+import org.mozilla.javascript.ScriptableObject;
 import org.mozilla.javascript.SecurityUtilities;
 import org.mozilla.javascript.SymbolKey;
 import org.mozilla.javascript.Undefined;
@@ -341,6 +342,7 @@ public class NativeConsoleTest {
                 "console.log('abc', 123)",
                 Collections.singletonList(new PrinterCall(Level.INFO, new Object[] {"abc", 123})));
         assertPrintMsg("console.log('abc', 123)", "abc 123");
+        assertPrintMsg("fn = console.log; fn('abc', 123)", "abc 123");
 
         assertPrintCalls(
                 "console.trace('abc', 123)",
@@ -352,26 +354,31 @@ public class NativeConsoleTest {
                                     new ScriptStackElement("source", null, 1)
                                 })));
         assertPrintMsg("console.trace('abc', 123)", "abc 123\n@source:1");
+        assertPrintMsg("fn = console.trace; fn('abc', 123)", "abc 123\n@source:1");
 
         assertPrintCalls(
                 "console.debug('abc', 123)",
                 Collections.singletonList(new PrinterCall(Level.DEBUG, new Object[] {"abc", 123})));
         assertPrintMsg("console.debug('abc', 123)", "abc 123");
+        assertPrintMsg("fn = console.debug; fn('abc', 123)", "abc 123");
 
         assertPrintCalls(
                 "console.info('abc', 123)",
                 Collections.singletonList(new PrinterCall(Level.INFO, new Object[] {"abc", 123})));
         assertPrintMsg("console.info('abc', 123)", "abc 123");
+        assertPrintMsg("fn = console.info; fn('abc', 123)", "abc 123");
 
         assertPrintCalls(
                 "console.warn('abc', 123)",
                 Collections.singletonList(new PrinterCall(Level.WARN, new Object[] {"abc", 123})));
         assertPrintMsg("console.warn('abc', 123)", "abc 123");
+        assertPrintMsg("fn = console.warn; fn('abc', 123)", "abc 123");
 
         assertPrintCalls(
                 "console.error('abc', 123)",
                 Collections.singletonList(new PrinterCall(Level.ERROR, new Object[] {"abc", 123})));
         assertPrintMsg("console.error('abc', 123)", "abc 123");
+        assertPrintMsg("fn = console.error; fn('abc', 123)", "abc 123");
     }
 
     @Test
@@ -416,6 +423,7 @@ public class NativeConsoleTest {
     @Test
     public void testAssert() {
         assertPrintCalls("console.assert(true)", Collections.emptyList());
+        assertPrintCalls("fn = console.assert; fn(true)", Collections.emptyList());
 
         assertPrintCalls(
                 "console.assert(false)",
@@ -473,7 +481,9 @@ public class NativeConsoleTest {
                         + "console.countReset('b');\n"
                         + "console.countReset('c');\n"
                         + "console.count('b');\n"
-                        + "console.count();\n",
+                        + "console.count();\n"
+                        + "fn = console.count; fn('d');\n"
+                        + "fn = console.countReset('d');\n",
                 Arrays.asList(
                         new PrinterCall(Level.INFO, new String[] {"default: 1"}),
                         new PrinterCall(Level.INFO, new String[] {"a: 1"}),
@@ -482,7 +492,8 @@ public class NativeConsoleTest {
                         new PrinterCall(Level.INFO, new String[] {"b: 2"}),
                         new PrinterCall(Level.WARN, new String[] {"Count for 'c' does not exist."}),
                         new PrinterCall(Level.INFO, new String[] {"b: 1"}),
-                        new PrinterCall(Level.INFO, new String[] {"default: 3"})));
+                        new PrinterCall(Level.INFO, new String[] {"default: 3"}),
+                        new PrinterCall(Level.INFO, new String[] {"d: 1"})));
     }
 
     @Test
@@ -495,10 +506,10 @@ public class NativeConsoleTest {
                         + "console.timeLog('b');\n"
                         + "console.timeEnd('b');\n"
                         + "console.timeLog('b');\n"
-                        + "console.time('b');\n"
-                        + "console.timeLog('b', 'abc', 123);\n"
-                        + "console.timeLog();\n"
-                        + "console.timeEnd('c');\n",
+                        + "fn = console.time; fn('b');\n"
+                        + "fn = console.timeLog; fn('b', 'abc', 123);\n"
+                        + "fn = console.timeLog; fn();\n"
+                        + "fn = console.timeEnd; fn('c');\n",
                 Arrays.asList(
                         new PrinterCall(
                                 Level.WARN, new Object[] {"Timer 'default' already exists."}),
@@ -544,6 +555,12 @@ public class NativeConsoleTest {
                 "{\"msg\":\"Something is wrong\",\"err\":{\"fileName\":\"source\",\"lineNumber\":2}}");
     }
 
+    @Test
+    public void testToSource() {
+        assertEquals("Console", evaluate("console.toSource()"));
+        assertEquals("Console", evaluate("fn = console.toSource; fn()"));
+    }
+
     private static void assertFormat(Object[] args, String expected) {
         try (Context cx = Context.enter()) {
             Scriptable scope = cx.initStandardObjects();
@@ -571,6 +588,17 @@ public class NativeConsoleTest {
             NativeConsole.init(scope, false, printer);
             cx.evaluateString(scope, source, "source", 1, null);
             printer.assertMsf(expectedMsg);
+        }
+    }
+
+    private static Object evaluate(String js) {
+        DummyConsolePrinter printer = new DummyConsolePrinter();
+
+        try (Context cx = Context.enter()) {
+            cx.setLanguageVersion(Context.VERSION_DEFAULT);
+            ScriptableObject scope = cx.initStandardObjects();
+            NativeConsole.init(scope, false, printer);
+            return cx.evaluateString(scope, js, "source", 1, null);
         }
     }
 }