mozilla/rhino

commit 2ba6e57690494069d78c156e115b92e7f10d5895

Make `switch` a proper `Scope`
* fix for the parser to allow let in switch statement if the switch is the only statement in a for loop
* Made `switch` a proper block for correct scoping of `let`
* Added a suggested test

---------

Co-authored-by: Ronald Brill <rbri@rbri.de>
Co-authored-by: andrea.bergia <andrea.bergia@servicenow.com>

rhino/src/main/java/org/mozilla/javascript/IRFactory.java
@@ -1158,30 +1158,37 @@ public final class IRFactory {
         // instead of:
         //     goto labelDefault;
 
-        Node switchExpr = transform(node.getExpression());
-        node.addChildToBack(switchExpr);
+        Scope block = Scope.splitScope(node);
+        block.setLineColumnNumber(node.getLineno(), node.getColumn());
+        block.addChildToBack(node);
 
-        Node block = new Node(Token.BLOCK, node, node.getLineno(), node.getColumn());
+        parser.pushScope(node);
+        try {
+            Node switchExpr = transform(node.getExpression());
+            node.addChildToBack(switchExpr);
 
-        for (SwitchCase sc : node.getCases()) {
-            AstNode expr = sc.getExpression();
-            Node caseExpr = null;
+            for (SwitchCase sc : node.getCases()) {
+                AstNode expr = sc.getExpression();
+                Node caseExpr = null;
 
-            if (expr != null) {
-                caseExpr = transform(expr);
-            }
-
-            List<AstNode> stmts = sc.getStatements();
-            Node body = new Block();
-            if (stmts != null) {
-                for (AstNode kid : stmts) {
-                    body.addChildToBack(transform(kid));
+                if (expr != null) {
+                    caseExpr = transform(expr);
                 }
+
+                List<AstNode> stmts = sc.getStatements();
+                Node body = new Block();
+                if (stmts != null) {
+                    for (AstNode kid : stmts) {
+                        body.addChildToBack(transform(kid));
+                    }
+                }
+                addSwitchCase(block, caseExpr, body);
             }
-            addSwitchCase(block, caseExpr, body);
+            closeSwitch(block);
+            return block;
+        } finally {
+            parser.popScope();
         }
-        closeSwitch(block);
-        return block;
     }
 
     private Node transformThrow(ThrowStatement node) {

tests/src/test/java/org/mozilla/javascript/tests/es5/LetTest.java
@@ -1,0 +1,97 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+package org.mozilla.javascript.tests.es5;
+
+import org.junit.jupiter.api.Test;
+import org.mozilla.javascript.testutils.Utils;
+
+/** Test for let. */
+class LetTest {
+
+    @Test
+    void simple() {
+        String script =
+                "function f(a,b,c) {\n"
+                        + " let sum = a + b + c;\n"
+                        + " return sum;\n"
+                        + "}\n"
+                        + "f(1, 2, 3);";
+        Utils.assertWithAllModes_1_8(6, script);
+    }
+
+    @Test
+    void switchLet() {
+        String script =
+                "var sum = 0;\n"
+                        + "var t = 1;\n"
+                        + "switch(t) {\n"
+                        + "  case 1:\n"
+                        + "    let s = t + 2;\n"
+                        + "    sum += s + 3;\n"
+                        + "    break;\n"
+                        + "  default:\n"
+                        + "    sum = 7;\n"
+                        + "}\n"
+                        + "sum;";
+        Utils.assertWithAllModes_1_8(6, script);
+    }
+
+    @Test
+    void forSwitchLet() {
+        String script =
+                "  var sum = 0;\n"
+                        + "for (let i = 0; i < 1; i++)\n"
+                        + "  switch (i) {\n"
+                        + "    case 0:\n"
+                        + "      let test = 7;\n"
+                        + "      sum += 4;\n"
+                        + "      break;\n"
+                        + "    }"
+                        + "sum;";
+        Utils.assertWithAllModes_1_8(4, script);
+    }
+
+    @Test
+    void ifSwitchLet() {
+        String script =
+                "var sum = 0;\n"
+                        + "if (sum == 0)\n"
+                        + "  switch (sum) {\n"
+                        + "    case 0:\n"
+                        + "      let test = 7;\n"
+                        + "      sum += 4;\n"
+                        + "      break;\n"
+                        + "    }"
+                        + "sum;";
+        Utils.assertWithAllModes_1_8(4, script);
+    }
+
+    @Test
+    void letInsideBodyOfSwitch() {
+        String script =
+                "switch (0) {\n"
+                        + "  default:\n"
+                        + "    let f;\n"
+                        + "  {\n"
+                        + "  }\n"
+                        + "}\n"
+                        + "\n"
+                        + "typeof f;\n";
+        Utils.assertWithAllModes_1_8("undefined", script);
+    }
+
+    @Test
+    void letInsideBodyOfSwitchES6Strict() {
+        String script =
+                "(function () {\n"
+                        + "  'use strict';\n"
+                        + "  switch (true) {\n"
+                        + "    case true:\n"
+                        + "      let x = 1;\n"
+                        + "      return x;\n"
+                        + "  }\n"
+                        + "})()\n";
+        Utils.assertWithAllModes_ES6(1, script);
+    }
+}

rhino/src/main/java/org/mozilla/javascript/Parser.java
@@ -1481,81 +1481,86 @@ public class Parser {
 
         SwitchStatement pn = new SwitchStatement(pos);
         pn.setLineColumnNumber(lineNumber(), columnNumber());
-        if (mustMatchToken(Token.LP, "msg.no.paren.switch", true)) pn.setLp(ts.tokenBeg - pos);
-
-        AstNode discriminant = expr(false);
-        pn.setExpression(discriminant);
-        enterSwitch(pn);
-
+        pushScope(pn);
         try {
-            if (mustMatchToken(Token.RP, "msg.no.paren.after.switch", true))
-                pn.setRp(ts.tokenBeg - pos);
+            if (mustMatchToken(Token.LP, "msg.no.paren.switch", true)) pn.setLp(ts.tokenBeg - pos);
 
-            mustMatchToken(Token.LC, "msg.no.brace.switch", true);
+            AstNode discriminant = expr(false);
+            pn.setExpression(discriminant);
+            enterSwitch(pn);
 
-            boolean hasDefault = false;
-            int tt;
-            switchLoop:
-            for (; ; ) {
-                tt = nextToken();
-                int casePos = ts.tokenBeg;
-                int caseLineno = lineNumber(), caseColumn = columnNumber();
-                AstNode caseExpression = null;
-                switch (tt) {
-                    case Token.RC:
-                        pn.setLength(ts.tokenEnd - pos);
-                        break switchLoop;
+            try {
+                if (mustMatchToken(Token.RP, "msg.no.paren.after.switch", true))
+                    pn.setRp(ts.tokenBeg - pos);
 
-                    case Token.CASE:
-                        caseExpression = expr(false);
-                        mustMatchToken(Token.COLON, "msg.no.colon.case", true);
-                        break;
+                mustMatchToken(Token.LC, "msg.no.brace.switch", true);
 
-                    case Token.DEFAULT:
-                        if (hasDefault) {
-                            reportError("msg.double.switch.default");
-                        }
-                        hasDefault = true;
-                        mustMatchToken(Token.COLON, "msg.no.colon.case", true);
-                        break;
-                    case Token.COMMENT:
-                        AstNode n = scannedComments.get(scannedComments.size() - 1);
-                        pn.addChild(n);
-                        continue switchLoop;
-                    default:
-                        reportError("msg.bad.switch");
-                        break switchLoop;
-                }
+                boolean hasDefault = false;
+                int tt;
+                switchLoop:
+                for (; ; ) {
+                    tt = nextToken();
+                    int casePos = ts.tokenBeg;
+                    int caseLineno = lineNumber(), caseColumn = columnNumber();
+                    AstNode caseExpression = null;
+                    switch (tt) {
+                        case Token.RC:
+                            pn.setLength(ts.tokenEnd - pos);
+                            break switchLoop;
 
-                SwitchCase caseNode = new SwitchCase(casePos);
-                caseNode.setExpression(caseExpression);
-                caseNode.setLength(ts.tokenEnd - pos); // include colon
-                caseNode.setLineColumnNumber(caseLineno, caseColumn);
+                        case Token.CASE:
+                            caseExpression = expr(false);
+                            mustMatchToken(Token.COLON, "msg.no.colon.case", true);
+                            break;
 
-                while ((tt = peekToken()) != Token.RC
-                        && tt != Token.CASE
-                        && tt != Token.DEFAULT
-                        && tt != Token.EOF) {
-                    if (tt == Token.COMMENT) {
-                        Comment inlineComment = scannedComments.get(scannedComments.size() - 1);
-                        if (caseNode.getInlineComment() == null
-                                && inlineComment.getLineno() == caseNode.getLineno()) {
-                            caseNode.setInlineComment(inlineComment);
-                        } else {
-                            caseNode.addStatement(inlineComment);
-                        }
-                        consumeToken();
-                        continue;
+                        case Token.DEFAULT:
+                            if (hasDefault) {
+                                reportError("msg.double.switch.default");
+                            }
+                            hasDefault = true;
+                            mustMatchToken(Token.COLON, "msg.no.colon.case", true);
+                            break;
+                        case Token.COMMENT:
+                            AstNode n = scannedComments.get(scannedComments.size() - 1);
+                            pn.addChild(n);
+                            continue switchLoop;
+                        default:
+                            reportError("msg.bad.switch");
+                            break switchLoop;
                     }
-                    AstNode nextStmt = statement();
-                    caseNode.addStatement(nextStmt); // updates length
+
+                    SwitchCase caseNode = new SwitchCase(casePos);
+                    caseNode.setExpression(caseExpression);
+                    caseNode.setLength(ts.tokenEnd - pos); // include colon
+                    caseNode.setLineColumnNumber(caseLineno, caseColumn);
+
+                    while ((tt = peekToken()) != Token.RC
+                            && tt != Token.CASE
+                            && tt != Token.DEFAULT
+                            && tt != Token.EOF) {
+                        if (tt == Token.COMMENT) {
+                            Comment inlineComment = scannedComments.get(scannedComments.size() - 1);
+                            if (caseNode.getInlineComment() == null
+                                    && inlineComment.getLineno() == caseNode.getLineno()) {
+                                caseNode.setInlineComment(inlineComment);
+                            } else {
+                                caseNode.addStatement(inlineComment);
+                            }
+                            consumeToken();
+                            continue;
+                        }
+                        AstNode nextStmt = statement();
+                        caseNode.addStatement(nextStmt); // updates length
+                    }
+                    pn.addCase(caseNode);
                 }
-                pn.addCase(caseNode);
+            } finally {
+                exitSwitch();
             }
+            return pn;
         } finally {
-            exitSwitch();
+            popScope();
         }
-        return pn;
     }
 
     private WhileLoop whileLoop() throws IOException {

rhino/src/main/java/org/mozilla/javascript/ast/SwitchStatement.java
@@ -27,7 +27,7 @@ import org.mozilla.javascript.Token;
  * <i>DefaultClause</i> :
  *        <b>default</b> : [StatementList]</pre>
  */
-public class SwitchStatement extends Jump {
+public class SwitchStatement extends Scope {
 
     private static final List<SwitchCase> NO_CASES =
             Collections.unmodifiableList(new ArrayList<>());
@@ -44,13 +44,7 @@ public class SwitchStatement extends Jump {
     public SwitchStatement() {}
 
     public SwitchStatement(int pos) {
-        // can't call super (Jump) for historical reasons
         position = pos;
-    }
-
-    public SwitchStatement(int pos, int len) {
-        position = pos;
-        length = len;
     }
 
     /** Returns the switch discriminant expression */

tests/testsrc/test262.properties
@@ -7111,7 +7111,7 @@ language/statements/let 80/145 (55.17%)
 language/statements/return 1/16 (6.25%)
     tco.js {unsupported: [tail-call-optimization]}
 
-language/statements/switch 62/111 (55.86%)
+language/statements/switch 60/111 (54.05%)
     syntax/redeclaration/async-function-name-redeclaration-attempt-with-async-function.js {unsupported: [async-functions]}
     syntax/redeclaration/async-function-name-redeclaration-attempt-with-async-generator.js {unsupported: [async-iteration, async-functions]}
     syntax/redeclaration/async-function-name-redeclaration-attempt-with-class.js {unsupported: [async-functions]}
@@ -7165,8 +7165,6 @@ language/statements/switch 62/111 (55.86%)
     scope-lex-async-function.js
     scope-lex-async-generator.js
     scope-lex-class.js
-    scope-lex-close-case.js
-    scope-lex-close-dflt.js
     scope-lex-const.js
     scope-lex-generator.js
     scope-lex-open-case.js

commit 752dd925f41a68a382b724a6b5d639068170e73f

Fix variable resolution in switch statements with let declarations
Fixed the regression introduced by 2ba6e57, which made `switch`
a proper `Scope`.

The issue was that `Scope.splitScope()` creates an outer block scope
and moves the symbol table from the switch to the block, but doesn't
update the switch's `parentScope` pointer. This breaks the scope chain
used for variable resolution, causing it to skip the block where
symbols are stored.

Fixed by manually fixing the scope chain, and follow the same pattern as
`transformForLoop()` for manually managing `currentScope` instead
of using `pushScope`/`popScope`, since the scope hierarchy is modified
after parsing.

rhino/src/main/java/org/mozilla/javascript/IRFactory.java
@@ -1167,8 +1167,15 @@ public final class IRFactory {
         Scope block = Scope.splitScope(node);
         block.setLineColumnNumber(node.getLineno(), node.getColumn());
         block.addChildToBack(node);
+        node.setParentScope(block);
 
-        parser.pushScope(node);
+        // Can't use pushScope/popScope here since splitScope moves the symbol table
+        // We set currentScope to 'node' (not 'block') so nested scopes can be pushed,
+        // since their parent pointers were set to 'node' during parsing. Variable resolution
+        // works correctly because it walks up the parentScope chain, where node.parentScope =
+        // block.
+        Scope savedScope = parser.currentScope;
+        parser.currentScope = node;
         try {
             Node switchExpr = transform(node.getExpression());
             node.addChildToBack(switchExpr);
@@ -1193,7 +1200,7 @@ public final class IRFactory {
             closeSwitch(block);
             return block;
         } finally {
-            parser.popScope();
+            parser.currentScope = savedScope;
         }
     }
 

tests/src/test/java/org/mozilla/javascript/tests/es5/LetTest.java
@@ -94,4 +94,35 @@ class LetTest {
                         + "})()\n";
         Utils.assertWithAllModes_ES6(1, script);
     }
+
+    @Test
+    void letInsideFunctionSwitchIf() {
+        String script =
+                "(function () {\n"
+                        + "    switch (0) {\n"
+                        + "        default:\n"
+                        + "            let a = 0;\n"
+                        + "            if (a == 0) {\n"
+                        + "                a++;\n"
+                        + "            }\n"
+                        + "            return a;\n"
+                        + "    }\n"
+                        + "})();\n";
+        Utils.assertWithAllModes_1_8(1, script);
+    }
+
+    @Test
+    void letInsideFunctionWhileIf() {
+        String script =
+                "(function () {\n"
+                        + "    while (true) {\n"
+                        + "            let a = 0;\n"
+                        + "            if (a == 0) {\n"
+                        + "                a++;\n"
+                        + "            }\n"
+                        + "            return a;\n"
+                        + "    }\n"
+                        + "})();\n";
+        Utils.assertWithAllModes_1_8(1, script);
+    }
 }