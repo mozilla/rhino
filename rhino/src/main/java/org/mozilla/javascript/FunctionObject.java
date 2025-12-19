111ab8ecbeb9379cfa1ab4ae8d822d3fd174eabf
duncan.macgregor <duncan.macgregor@servicenow.com>
2025-12-19 11:33:46 +0000

    Clean up `ModuleScope`.

--- rhino/src/main/java/org/mozilla/javascript/FunctionObject.java
+++ rhino/src/main/java/org/mozilla/javascript/FunctionObject.java
@@ -14,7 +14,6 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Member;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
-import org.mozilla.javascript.commonjs.module.ModuleScope;
 
 public class FunctionObject extends BaseFunction {
     private static final long serialVersionUID = -5332312783643935019L;
@@ -387,22 +386,8 @@ public class FunctionObject extends BaseFunction {
                     thisObj = ((Delegator) thisObj).getDelegee();
                 }
                 if (!clazz.isInstance(thisObj)) {
-                    boolean compatible = false;
-                    if (thisObj == scope || thisObj instanceof ModuleScope) {
-                        Scriptable parentScope = getDeclarationScope();
-                        if (scope != parentScope) {
-                            // Call with dynamic scope for standalone function,
-                            // use parentScope as thisObj
-                            compatible = clazz.isInstance(parentScope);
-                            if (compatible) {
-                                thisObj = parentScope;
-                            }
-                        }
-                    }
-                    if (!compatible) {
-                        // Couldn't find an object to call this on.
-                        throw ScriptRuntime.typeErrorById("msg.incompat.call", functionName);
-                    }
+                    // Couldn't find an object to call this on.
+                    throw ScriptRuntime.typeErrorById("msg.incompat.call", functionName);
                 }
             }
 
--- rhino/src/main/java/org/mozilla/javascript/commonjs/module/Require.java
+++ rhino/src/main/java/org/mozilla/javascript/commonjs/module/Require.java
@@ -17,6 +17,7 @@ import org.mozilla.javascript.ScriptRuntime;
 import org.mozilla.javascript.Scriptable;
 import org.mozilla.javascript.ScriptableObject;
 import org.mozilla.javascript.TopLevel;
+import org.mozilla.javascript.Undefined;
 
 /**
  * Implements the require() function as defined by <a
@@ -176,12 +177,18 @@ public class Require extends BaseFunction {
         if (args == null || args.length < 1) {
             throw ScriptRuntime.throwError(cx, scope, "require() needs one argument");
         }
+        // Top scope from the point of view of the callee
+        TopLevel topScope = ScriptableObject.getTopLevelScope(scope);
+        if (thisObj == null || Undefined.isUndefined(thisObj)) {
+            thisObj = topScope.getGlobalThis();
+        }
+        ModuleScope moduleScope = ModuleScope.findModuleScope(scope);
 
         String id = (String) Context.jsToJava(args[0], String.class);
         URI uri = null;
         URI base = null;
         if (id.startsWith("./") || id.startsWith("../")) {
-            if (!(thisObj instanceof ModuleScope)) {
+            if (moduleScope == null) {
                 throw ScriptRuntime.throwError(
                         cx,
                         scope,
@@ -190,7 +197,6 @@ public class Require extends BaseFunction {
                                 + "\" when require() is used outside of a module");
             }
 
-            ModuleScope moduleScope = (ModuleScope) thisObj;
             base = moduleScope.getBase();
             URI current = moduleScope.getUri();
             uri = current.resolve(id);
