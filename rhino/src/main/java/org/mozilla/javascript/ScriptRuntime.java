bc2a85b26bdd71ed635f8050f4ad53f8700777c0
duncan.macgregor <duncan.macgregor@servicenow.com>
2026-01-02 18:47:26 +0000

    Fixup after rebase.

--- rhino/src/main/java/org/mozilla/javascript/ScriptRuntime.java
+++ rhino/src/main/java/org/mozilla/javascript/ScriptRuntime.java
@@ -192,7 +192,7 @@ public class ScriptRuntime {
             scope = new TopLevel();
         }
 
-        scope.put("global", scope, scope);
+        scope.put("global", scope, scope.getGlobalThis());
 
         scope.clearCache();
         scope.associateValue(LIBRARY_SCOPE_KEY, scope);
