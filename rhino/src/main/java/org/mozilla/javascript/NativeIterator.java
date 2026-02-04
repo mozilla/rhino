da52f52a55a1107c148ab2181e97e7a99aa4d10a
duncan.macgregor <duncan.macgregor@servicenow.com>
2026-02-04 14:12:02 +0000

    Convert `NativeGenerator` to descriptor.

--- rhino/src/main/java/org/mozilla/javascript/NativeIterator.java
+++ rhino/src/main/java/org/mozilla/javascript/NativeIterator.java
@@ -52,7 +52,7 @@ public final class NativeIterator extends ScriptableObject {
         if (cx.getLanguageVersion() >= Context.VERSION_ES6) {
             ES6Generator.init(scope, sealed);
         } else {
-            NativeGenerator.init(scope, sealed);
+            NativeGenerator.init(cx, scope, sealed);
         }
 
         // StopIteration
