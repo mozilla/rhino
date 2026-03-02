66c23ec7f491c1ecb5f5f2e2ffe54612f2c3156d
duncan.macgregor <duncan.macgregor@servicenow.com>
2026-03-02 23:22:52 +0000

    Refactor away from `WithScope` for catching exceptions.

---
+++ rhino/src/main/java/org/mozilla/javascript/CatchScope.java
@@ -0,0 +1,9 @@
+package org.mozilla.javascript;
+
+public class CatchScope extends DeclarationScope {
+    private static final long serialVersionUID = -7471457301304454454L;
+
+    public CatchScope(VarScope parentScope) {
+        super(parentScope);
+    }
+}
