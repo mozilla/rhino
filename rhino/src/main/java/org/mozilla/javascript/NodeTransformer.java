08aa7eacc9371687d94fb63701bf66f230adea57
duncan.macgregor <duncan.macgregor@servicenow.com>
2026-03-06 09:08:21 +0000

    Await proof of concept implementation.

--- rhino/src/main/java/org/mozilla/javascript/NodeTransformer.java
+++ rhino/src/main/java/org/mozilla/javascript/NodeTransformer.java
@@ -158,6 +158,7 @@ public class NodeTransformer {
 
                 case Token.YIELD:
                 case Token.YIELD_STAR:
+                case Token.AWAIT:
                     ((FunctionNode) tree).addResumptionPoint(node);
                     break;
 
