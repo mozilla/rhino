2202edc308fcaf5a54c4096003f8ba92e96a4a68
Greg Brail <gbrail@users.noreply.github.com>
2026-03-07 16:59:27 -0800

    Move tests to more appropriate directories

--- tests/src/test/java/org/mozilla/javascript/tests/es2024/TypedArrayResizableArrayBufferTest.java
+++ tests/src/test/java/org/mozilla/javascript/tests/es2024/TypedArrayResizableArrayBufferTest.java
@@ -1,10 +1,10 @@
-package org.mozilla.javascript.tests.harmony;
+package org.mozilla.javascript.tests.es2024;
 
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.drivers.LanguageVersion;
 import org.mozilla.javascript.drivers.RhinoTest;
 import org.mozilla.javascript.drivers.ScriptTestsBase;
 
-@RhinoTest("testsrc/jstests/harmony/dataview-resizablearraybuffer.js")
+@RhinoTest("testsrc/jstests/es2024/typedarray-resizablearraybuffer.js")
 @LanguageVersion(Context.VERSION_ES6)
-public class DataViewResizableArrayBufferTest extends ScriptTestsBase {}
+public class TypedArrayResizableArrayBufferTest extends ScriptTestsBase {}
