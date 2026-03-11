c27d6ffb9977bc936a7fc3d26f24959273506227
duncan.macgregor <duncan.macgregor@servicenow.com>
2026-03-11 15:25:50 +0000

    Enable `new.target` in 262 tests.

--- tests/src/test/java/org/mozilla/javascript/tests/Test262SuiteTest.java
+++ tests/src/test/java/org/mozilla/javascript/tests/Test262SuiteTest.java
@@ -118,7 +118,6 @@ public class Test262SuiteTest {
                             "class",
                             "class-fields-private",
                             "class-fields-public",
-                            "new.target",
                             "SharedArrayBuffer",
                             "tail-call-optimization",
                             "Temporal",
