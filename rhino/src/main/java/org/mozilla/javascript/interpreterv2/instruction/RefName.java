92b4499cf5d06c58cf7d5c54c2d5dc23bcd6c53f
duncan.macgregor <duncan.macgregor@servicenow.com>
2026-06-03 14:21:22 +0100

    Ref instructions.

---
+++ rhino/src/main/java/org/mozilla/javascript/interpreterv2/instruction/RefName.java
@@ -0,0 +1,30 @@
+package org.mozilla.javascript.interpreterv2.instruction;
+
+import org.mozilla.javascript.CallFrameV2;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.ScriptRuntime;
+import org.mozilla.javascript.interpreterv2.operand.Operand;
+
+public class RefName extends Instruction {
+    private final Operand name;
+    private final int flags;
+
+    public RefName(Operand name, int flags) {
+        this.name = name;
+        this.flags = flags;
+    }
+
+    @Override
+    public void interpret(Context cx, CallFrameV2 frame) {
+        Object nameValue = name.retrieveAndWrap(cx, frame);
+
+        Object result = ScriptRuntime.nameRef(nameValue, cx, frame.scope, flags);
+        frame.push(result);
+        frame.pc += 1;
+    }
+
+    @Override
+    public int stackChange() {
+        return 1 + name.stackChange();
+    }
+}
