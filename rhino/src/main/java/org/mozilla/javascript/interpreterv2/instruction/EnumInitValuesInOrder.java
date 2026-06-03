353d8e6002170788f585fc7bd5db5905168dafc9
duncan.macgregor <duncan.macgregor@servicenow.com>
2026-06-03 14:48:28 +0100

    Enumeration instructions.

---
+++ rhino/src/main/java/org/mozilla/javascript/interpreterv2/instruction/EnumInitValuesInOrder.java
@@ -0,0 +1,38 @@
+package org.mozilla.javascript.interpreterv2.instruction;
+
+import org.mozilla.javascript.CallFrameV2;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.ScriptRuntime;
+import org.mozilla.javascript.interpreterv2.InstructionFormatter;
+import org.mozilla.javascript.interpreterv2.operand.Operand;
+
+public class EnumInitValuesInOrder extends Instruction {
+    private final Operand obj;
+    private final int index;
+
+    public EnumInitValuesInOrder(Operand obj, int index) {
+        this.obj = obj;
+        this.index = index;
+    }
+
+    @Override
+    public void interpret(Context cx, CallFrameV2 frame) {
+        frame.pc += 1;
+
+        var lhs = obj.retrieveAndWrap(cx, frame);
+        frame.setLocal(
+                index,
+                ScriptRuntime.enumInit(
+                        lhs, cx, frame.scope, ScriptRuntime.ENUMERATE_VALUES_IN_ORDER));
+    }
+
+    @Override
+    public int stackChange() {
+        return obj.stackChange();
+    }
+
+    @Override
+    public String toDebugString() {
+        return InstructionFormatter.formatInstruction(this, "obj", obj, "index", index);
+    }
+}
