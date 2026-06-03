f75138255d0954e331528cc8606400cf4dd3417b
duncan.macgregor <duncan.macgregor@servicenow.com>
2026-06-03 14:11:15 +0100

    Name instructions.

---
+++ rhino/src/main/java/org/mozilla/javascript/interpreterv2/instruction/DelName.java
@@ -0,0 +1,37 @@
+package org.mozilla.javascript.interpreterv2.instruction;
+
+import org.mozilla.javascript.CallFrameV2;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.ScriptRuntime;
+import org.mozilla.javascript.interpreterv2.InstructionFormatter;
+import org.mozilla.javascript.interpreterv2.operand.Operand;
+
+public class DelName extends Instruction {
+    private final Operand lhs;
+    private final Operand rhs;
+
+    public DelName(Operand lhs, Operand rhs) {
+        this.lhs = lhs;
+        this.rhs = rhs;
+    }
+
+    @Override
+    public void interpret(Context cx, CallFrameV2 frame) {
+        frame.pc += 1;
+
+        var rhs = this.rhs.retrieveAndWrap(cx, frame);
+        var lhs = this.lhs.retrieveAndWrap(cx, frame);
+        frame.push(ScriptRuntime.delete(lhs, rhs, cx, frame.scope, true));
+    }
+
+    @Override
+    public int stackChange() {
+        return 1 + lhs.stackChange() + rhs.stackChange();
+    }
+
+    @Override
+    public String toDebugString() {
+        return InstructionFormatter.formatInstruction(
+                this, "lhs", lhs.getClass().getSimpleName(), "rhs", rhs.getClass().getSimpleName());
+    }
+}
