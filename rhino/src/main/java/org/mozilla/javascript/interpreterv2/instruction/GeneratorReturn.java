d2081a241c34d36629ea63ea3999b6a0131afb8e
duncan.macgregor <duncan.macgregor@servicenow.com>
2026-06-03 14:42:08 +0100

    Generator related instructions.

---
+++ rhino/src/main/java/org/mozilla/javascript/interpreterv2/instruction/GeneratorReturn.java
@@ -0,0 +1,48 @@
+package org.mozilla.javascript.interpreterv2.instruction;
+
+import static org.mozilla.javascript.UniqueTag.DOUBLE_MARK;
+
+import org.mozilla.javascript.CallFrameV2;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.JavaScriptException;
+import org.mozilla.javascript.NativeIterator;
+import org.mozilla.javascript.interpreterv2.InstructionFormatter;
+import org.mozilla.javascript.interpreterv2.operand.Operand;
+
+public class GeneratorReturn extends Instruction {
+    private final short lineNumber;
+    private final Operand value;
+
+    public GeneratorReturn(short lineNumber, Operand value) {
+        this.lineNumber = lineNumber;
+        this.value = value;
+    }
+
+    @Override
+    public void interpret(Context cx, CallFrameV2 frame) {
+        frame.pc += 1;
+        frame.frozen = true;
+        value.setResult(cx, frame);
+
+        NativeIterator.StopIteration si =
+                new NativeIterator.StopIteration(
+                        (frame.result == DOUBLE_MARK)
+                                ? Double.valueOf(frame.resultDbl)
+                                : frame.result);
+
+        frame.generatorState.returnedException =
+                new JavaScriptException(si, frame.compilerData.sourceFile, lineNumber);
+        frame.shouldYieldToParent = true;
+    }
+
+    @Override
+    public int stackChange() {
+        return value.stackChange();
+    }
+
+    @Override
+    public String toDebugString() {
+        return InstructionFormatter.formatInstruction(
+                this, "lineNumber", lineNumber, "value", value);
+    }
+}
---
+++ rhino/src/main/java/org/mozilla/javascript/interpreterv2/instruction/ThawFrame.java
@@ -0,0 +1,63 @@
+package org.mozilla.javascript.interpreterv2.instruction;
+
+import org.mozilla.javascript.CallFrameV2;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.JavaScriptException;
+import org.mozilla.javascript.Kit;
+import org.mozilla.javascript.NativeGenerator;
+import org.mozilla.javascript.Scriptable;
+import org.mozilla.javascript.interpreterv2.GeneratorState;
+import org.mozilla.javascript.interpreterv2.InstructionFormatter;
+
+public class ThawFrame extends Instruction {
+    private final boolean isYield;
+    private final short baseLineNumber;
+
+    public ThawFrame(boolean isYield, short baseLineNumber) {
+        this.isYield = isYield;
+        this.baseLineNumber = baseLineNumber;
+    }
+
+    @Override
+    public void interpret(Context cx, CallFrameV2 frame) {
+        frame.pc += 1;
+        Object obj = thawGenerator(frame, frame.generatorState);
+        if (obj != Scriptable.NOT_FOUND) {
+            frame.throwable = obj;
+        }
+    }
+
+    private Object thawGenerator(CallFrameV2 frame, GeneratorState generatorState) {
+        // we are resuming execution
+        frame.frozen = false;
+        int sourceLine = baseLineNumber;
+        if (generatorState.operation == NativeGenerator.GENERATOR_THROW) {
+            // processing a call to <generator>.throw(exception): must
+            // act as if exception was thrown from resumption point.
+            return new JavaScriptException(
+                    generatorState.value, frame.fnOrScript.getDescriptor().getSourceName(), sourceLine);
+        }
+        if (generatorState.operation == NativeGenerator.GENERATOR_CLOSE) {
+            return generatorState.value;
+        }
+        if (generatorState.operation != NativeGenerator.GENERATOR_SEND) throw Kit.codeBug();
+        if (isYield) {
+            frame.push(generatorState.value);
+        }
+        return Scriptable.NOT_FOUND;
+    }
+
+    @Override
+    public int stackChange() {
+        if (isYield) {
+            return 1;
+        }
+        return 0;
+    }
+
+    @Override
+    public String toDebugString() {
+        return InstructionFormatter.formatInstruction(
+                this, "isYield", isYield, "baseLineNumber", baseLineNumber);
+    }
+}
