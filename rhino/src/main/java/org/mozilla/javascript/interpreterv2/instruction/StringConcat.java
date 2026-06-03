b974ce67e11e304f7c105c43500c7600597c340a
duncan.macgregor <duncan.macgregor@servicenow.com>
2026-06-03 13:59:28 +0100

    Arithmetic operation instructions.

---
+++ rhino/src/main/java/org/mozilla/javascript/interpreterv2/instruction/StringConcat.java
@@ -0,0 +1,38 @@
+package org.mozilla.javascript.interpreterv2.instruction;
+
+import org.mozilla.javascript.CallFrameV2;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.ScriptRuntime;
+import org.mozilla.javascript.interpreterv2.InstructionFormatter;
+import org.mozilla.javascript.interpreterv2.operand.Operand;
+
+public class StringConcat extends Instruction {
+    private final Operand lhs;
+    private final Operand rhs;
+
+    public StringConcat(Operand lhs, Operand rhs) {
+        this.lhs = lhs;
+        this.rhs = rhs;
+    }
+
+    @Override
+    public void interpret(Context cx, CallFrameV2 frame) {
+        frame.pc += 1;
+
+        Object rObj = rhs.retrieve(cx, frame);
+        Object lObj = lhs.retrieve(cx, frame);
+
+        Object result = ScriptRuntime.concat(lObj, rObj);
+        frame.push(result);
+    }
+
+    @Override
+    public int stackChange() {
+        return 1 + lhs.stackChange() + rhs.stackChange();
+    }
+
+    @Override
+    public String toDebugString() {
+        return InstructionFormatter.formatInstruction(this, "lhs", lhs, "rhs", rhs);
+    }
+}
