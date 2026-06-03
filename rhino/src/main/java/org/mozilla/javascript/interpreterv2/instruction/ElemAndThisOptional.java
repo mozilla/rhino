bf6922b0131dcd5cf8f5f6241c41a23a0aad421f
duncan.macgregor <duncan.macgregor@servicenow.com>
2026-06-03 15:21:30 +0100

    "... and this" instructions.

---
+++ rhino/src/main/java/org/mozilla/javascript/interpreterv2/instruction/ElemAndThisOptional.java
@@ -0,0 +1,37 @@
+package org.mozilla.javascript.interpreterv2.instruction;
+
+import org.mozilla.javascript.CallFrameV2;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.ScriptRuntime;
+import org.mozilla.javascript.interpreterv2.InstructionFormatter;
+import org.mozilla.javascript.interpreterv2.operand.Operand;
+
+public class ElemAndThisOptional extends Instruction {
+    private final Operand obj;
+    private final Operand id;
+
+    public ElemAndThisOptional(Operand obj, Operand id) {
+        this.obj = obj;
+        this.id = id;
+    }
+
+    @Override
+    public void interpret(Context cx, CallFrameV2 frame) {
+        var id = this.id.retrieveAndWrap(cx, frame);
+        var obj = this.obj.retrieveAndWrap(cx, frame);
+
+        frame.push(ScriptRuntime.getElemAndThisOptional(obj, id, cx, frame.scope));
+        frame.pc += 1;
+    }
+
+    @Override
+    public int stackChange() {
+        return 1 + obj.stackChange() + id.stackChange();
+    }
+
+    @Override
+    public String toDebugString() {
+        return InstructionFormatter.formatInstruction(
+                this, "obj", obj.getClass().getSimpleName(), "id", id.getClass().getSimpleName());
+    }
+}
---
+++ rhino/src/main/java/org/mozilla/javascript/interpreterv2/instruction/ValueAndThisOptional.java
@@ -0,0 +1,32 @@
+package org.mozilla.javascript.interpreterv2.instruction;
+
+import org.mozilla.javascript.CallFrameV2;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.ScriptRuntime;
+import org.mozilla.javascript.interpreterv2.InstructionFormatter;
+import org.mozilla.javascript.interpreterv2.operand.Operand;
+
+public class ValueAndThisOptional extends Instruction {
+    private final Operand value;
+
+    public ValueAndThisOptional(Operand value) {
+        this.value = value;
+    }
+
+    @Override
+    public void interpret(Context cx, CallFrameV2 frame) {
+        var value = this.value.retrieveAndWrap(cx, frame);
+        frame.push(ScriptRuntime.getValueAndThisOptional(value, cx));
+        frame.pc += 1;
+    }
+
+    @Override
+    public int stackChange() {
+        return 1 + value.stackChange();
+    }
+
+    @Override
+    public String toDebugString() {
+        return InstructionFormatter.formatInstruction(this, "value", value);
+    }
+}
