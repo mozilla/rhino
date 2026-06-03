eb1da15e3215659bc4a501a7bd22f13d9ff56e7d
duncan.macgregor <duncan.macgregor@servicenow.com>
2026-06-03 13:59:28 +0100

    Arithmetic operation instructions.

---
+++ rhino/src/main/java/org/mozilla/javascript/interpreterv2/instruction/StringStringAdd.java
@@ -0,0 +1,43 @@
+package org.mozilla.javascript.interpreterv2.instruction;
+
+import org.mozilla.javascript.CallFrameV2;
+import org.mozilla.javascript.ConsString;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.interpreterv2.InstructionFormatter;
+import org.mozilla.javascript.interpreterv2.InstructionSimplification;
+import org.mozilla.javascript.interpreterv2.KnownType;
+import org.mozilla.javascript.interpreterv2.operand.Operand;
+
+public class StringStringAdd extends Instruction {
+    private final Operand lhs;
+    private final Operand rhs;
+
+    public StringStringAdd(Operand lhs, Operand rhs) {
+        this.lhs = lhs;
+        this.rhs = rhs;
+    }
+
+    @Override
+    public void interpret(Context cx, CallFrameV2 frame) {
+        Object rhsValue = rhs.retrieve(cx, frame);
+        Object lhsValue = lhs.retrieve(cx, frame);
+
+        frame.push(new ConsString((CharSequence) lhsValue, (CharSequence) rhsValue));
+        frame.pc += 1;
+    }
+
+    @Override
+    public int stackChange() {
+        return 1 + lhs.stackChange() + rhs.stackChange();
+    }
+
+    @Override
+    public KnownType getKnownType(InstructionSimplification simplifier) {
+        return KnownType.STRING;
+    }
+
+    @Override
+    public String toDebugString() {
+        return InstructionFormatter.formatInstruction(this, "lhs", lhs, "rhs", rhs);
+    }
+}
