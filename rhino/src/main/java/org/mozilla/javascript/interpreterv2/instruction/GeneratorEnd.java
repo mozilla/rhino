83810ffc92296875f0a9b2f8c222edb589177292
duncan.macgregor <duncan.macgregor@servicenow.com>
2026-06-03 14:42:08 +0100

    Generator related instructions.

---
+++ rhino/src/main/java/org/mozilla/javascript/interpreterv2/instruction/GeneratorEnd.java
@@ -0,0 +1,38 @@
+package org.mozilla.javascript.interpreterv2.instruction;
+
+import org.mozilla.javascript.CallFrameV2;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.JavaScriptException;
+import org.mozilla.javascript.NativeIterator;
+import org.mozilla.javascript.interpreterv2.InstructionFormatter;
+
+public class GeneratorEnd extends Instruction {
+    private final short lineNumber;
+
+    public GeneratorEnd(short lineNumber) {
+        this.lineNumber = lineNumber;
+    }
+
+    @Override
+    public void interpret(Context cx, CallFrameV2 frame) {
+        frame.pc += 1;
+        // throw StopIteration
+        frame.frozen = true;
+        frame.generatorState.returnedException =
+                new JavaScriptException(
+                        NativeIterator.getStopIterationObject(frame.scope),
+                        frame.fnOrScript.getDescriptor().getSourceName(),
+                        lineNumber);
+        frame.shouldYieldToParent = true;
+    }
+
+    @Override
+    public int stackChange() {
+        return 0;
+    }
+
+    @Override
+    public String toDebugString() {
+        return InstructionFormatter.formatInstruction(this, "lineNumber", lineNumber);
+    }
+}
