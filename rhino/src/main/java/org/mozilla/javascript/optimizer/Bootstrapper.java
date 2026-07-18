e7cc418b4a6eca6ef75d30547f6526b35bf8fe08
duncan.macgregor <duncan.macgregor@servicenow.com>
2026-07-18 20:46:58 +0100

    Cut down on duplicate MH INDY stuff.

--- rhino/src/main/java/org/mozilla/javascript/optimizer/Bootstrapper.java
+++ rhino/src/main/java/org/mozilla/javascript/optimizer/Bootstrapper.java
@@ -3,11 +3,14 @@ package org.mozilla.javascript.optimizer;
 import java.lang.invoke.CallSite;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
+import java.lang.ref.WeakReference;
 import java.util.Arrays;
+import java.util.WeakHashMap;
 import java.util.regex.Pattern;
 import jdk.dynalink.CallSiteDescriptor;
 import jdk.dynalink.DynamicLinker;
 import jdk.dynalink.DynamicLinkerFactory;
+import jdk.dynalink.NamespaceOperation;
 import jdk.dynalink.Operation;
 import jdk.dynalink.StandardNamespace;
 import jdk.dynalink.StandardOperation;
@@ -70,6 +73,10 @@ public class Bootstrapper {
         linker = factory.createLinker();
     }
 
+    private static final MethodHandles.Lookup LOOKUP = MethodHandles.lookup();
+    private static final WeakHashMap<CallSiteDescriptor, WeakReference<CallSiteDescriptor>>
+            CALLSITE_DESC_CACHE = new WeakHashMap<>();
+
     /**
      * This is the method called by every call site in the bytecode to map it to a method handle.
      */
@@ -77,12 +84,87 @@ public class Bootstrapper {
     public static CallSite bootstrap(MethodHandles.Lookup lookup, String name, MethodType mType)
             throws NoSuchMethodException {
         Operation op = parseOperation(name);
+
         // ChainedCallSite lets a call site have a few options for complex situations.
         // It caches up to eight invocations, so that we can quickly select the best
         // implementation in situations where the same call site is invoked in different
         // contexts.
-        return linker.link(new ChainedCallSite(new CallSiteDescriptor(lookup, op, mType)));
+        return linker.link(
+                new ChainedCallSite(dedupDesc(new CallSiteDescriptor(LOOKUP, op, mType))));
     }
+
+    private static CallSiteDescriptor dedupDesc(CallSiteDescriptor desc) {
+        synchronized (CALLSITE_DESC_CACHE) {
+            return CALLSITE_DESC_CACHE.computeIfAbsent(desc, k -> new WeakReference<>(desc)).get();
+        }
+    }
+
+    private static NamespaceOperation GET_PROOPERTY =
+            StandardOperation.GET.withNamespace(StandardNamespace.PROPERTY);
+    private static NamespaceOperation GETNOWARN_PROPERTY =
+            RhinoOperation.GETNOWARN.withNamespace(StandardNamespace.PROPERTY);
+    private static NamespaceOperation GETSUPER_PROPERTY =
+            RhinoOperation.GETSUPER.withNamespace(StandardNamespace.PROPERTY);
+    private static NamespaceOperation GETWITHTHIS_PROPERTY =
+            RhinoOperation.GETWITHTHIS.withNamespace(StandardNamespace.PROPERTY);
+    private static NamespaceOperation GETWITHTHISOPTIONAL_PROPERTY =
+            RhinoOperation.GETWITHTHISOPTIONAL.withNamespace(StandardNamespace.PROPERTY);
+    private static NamespaceOperation GETELEMENT_PROPERTY =
+            RhinoOperation.GETELEMENT.withNamespace(StandardNamespace.PROPERTY);
+    private static NamespaceOperation GETELEMENTSUPER_PROPERTY =
+            RhinoOperation.GETELEMENTSUPER.withNamespace(StandardNamespace.PROPERTY);
+    private static NamespaceOperation GETINDEX_PROPERTY =
+            RhinoOperation.GETINDEX.withNamespace(StandardNamespace.PROPERTY);
+    private static NamespaceOperation SET_PROPERTY =
+            StandardOperation.SET.withNamespace(StandardNamespace.PROPERTY);
+    private static NamespaceOperation SETSUPER_PROPERTY =
+            RhinoOperation.SETSUPER.withNamespace(StandardNamespace.PROPERTY);
+    private static NamespaceOperation SETELEMENT_PROPERTY =
+            RhinoOperation.SETELEMENT.withNamespace(StandardNamespace.PROPERTY);
+    private static NamespaceOperation SETELEMENTSUPER_PROPERTY =
+            RhinoOperation.SETELEMENTSUPER.withNamespace(StandardNamespace.PROPERTY);
+    private static NamespaceOperation SETINDEX_PROPERTY =
+            RhinoOperation.SETINDEX.withNamespace(StandardNamespace.PROPERTY);
+
+    private static NamespaceOperation BIND_NAME =
+            RhinoOperation.BIND.withNamespace(RhinoNamespace.NAME);
+    private static NamespaceOperation GET_NAME =
+            StandardOperation.GET.withNamespace(RhinoNamespace.NAME);
+    private static NamespaceOperation GETWITHTHIS_NAME =
+            RhinoOperation.GETWITHTHIS.withNamespace(RhinoNamespace.NAME);
+    private static NamespaceOperation GETWITHTHISOPTIONAL_NAME =
+            RhinoOperation.GETWITHTHISOPTIONAL.withNamespace(RhinoNamespace.NAME);
+    private static NamespaceOperation SET_NAME =
+            StandardOperation.SET.withNamespace(RhinoNamespace.NAME);
+    private static NamespaceOperation SETSTRICT_NAME =
+            RhinoOperation.SETSTRICT.withNamespace(RhinoNamespace.NAME);
+    private static NamespaceOperation SETCONST_NAME =
+            RhinoOperation.SETCONST.withNamespace(RhinoNamespace.NAME);
+
+    private static NamespaceOperation ADD_MATH =
+            RhinoOperation.ADD.withNamespace(RhinoNamespace.MATH);
+    private static NamespaceOperation TOBOOLEAN_MATH =
+            RhinoOperation.TOBOOLEAN.withNamespace(RhinoNamespace.MATH);
+    private static NamespaceOperation TOINT32_MATH =
+            RhinoOperation.TOINT32.withNamespace(RhinoNamespace.MATH);
+    private static NamespaceOperation TOUINT32_MATH =
+            RhinoOperation.TOUINT32.withNamespace(RhinoNamespace.MATH);
+    private static NamespaceOperation EQ_MATH =
+            RhinoOperation.EQ.withNamespace(RhinoNamespace.MATH);
+    private static NamespaceOperation SHALLOWEQ_MATH =
+            RhinoOperation.SHALLOWEQ.withNamespace(RhinoNamespace.MATH);
+    private static NamespaceOperation TONUMBER_MATH =
+            RhinoOperation.TONUMBER.withNamespace(RhinoNamespace.MATH);
+    private static NamespaceOperation TONUMERIC_MATH =
+            RhinoOperation.TONUMERIC.withNamespace(RhinoNamespace.MATH);
+    private static NamespaceOperation COMPAREGT_MATH =
+            RhinoOperation.COMPARE_GT.withNamespace(RhinoNamespace.MATH);
+    private static NamespaceOperation COMPARELT_MATH =
+            RhinoOperation.COMPARE_LT.withNamespace(RhinoNamespace.MATH);
+    private static NamespaceOperation COMPAREGE_MATH =
+            RhinoOperation.COMPARE_GE.withNamespace(RhinoNamespace.MATH);
+    private static NamespaceOperation COMPARELE_MATH =
+            RhinoOperation.COMPARE_LE.withNamespace(RhinoNamespace.MATH);
 
     /**
      * Operation names in the bytecode are names like "PROP:GET:[NAME]" and "NAME:BIND:[NAME]". (See
@@ -99,125 +181,97 @@ public class Bootstrapper {
             switch (opName) {
                 case "GET":
                     // Get an object property with a constant name
-                    return StandardOperation.GET
-                            .withNamespace(StandardNamespace.PROPERTY)
-                            .named(getNameSegment(tokens, name, 2));
+                    return GET_PROOPERTY.named(getNameSegment(tokens, name, 2));
                 case "GETNOWARN":
                     // Same with no warning of strict mode
-                    return RhinoOperation.GETNOWARN
-                            .withNamespace(StandardNamespace.PROPERTY)
-                            .named(getNameSegment(tokens, name, 2));
+                    return GETNOWARN_PROPERTY.named(getNameSegment(tokens, name, 2));
                 case "GETSUPER":
                     // Get an object property from super with a constant name
-                    return RhinoOperation.GETSUPER
-                            .withNamespace(StandardNamespace.PROPERTY)
-                            .named(getNameSegment(tokens, name, 2));
+                    return GETSUPER_PROPERTY.named(getNameSegment(tokens, name, 2));
                 case "GETWITHTHIS":
                     // Same but also return "this" so that it is found by "lastStoredScriptable"
-                    return RhinoOperation.GETWITHTHIS
-                            .withNamespace(StandardNamespace.PROPERTY)
-                            .named(getNameSegment(tokens, name, 2));
+                    return GETWITHTHIS_PROPERTY.named(getNameSegment(tokens, name, 2));
                 case "GETWITHTHISOPTIONAL":
                     // Similar to the above, but won't complain if prop is not found
-                    return RhinoOperation.GETWITHTHISOPTIONAL
-                            .withNamespace(StandardNamespace.PROPERTY)
-                            .named(getNameSegment(tokens, name, 2));
+                    return GETWITHTHISOPTIONAL_PROPERTY.named(getNameSegment(tokens, name, 2));
                 case "GETELEMENT":
                     // Get the value of an element from a property that is on the stack,\
                     // as if using "[]" notation. Could be a String, number, or Symbol
-                    return RhinoOperation.GETELEMENT.withNamespace(StandardNamespace.PROPERTY);
+                    return GETELEMENT_PROPERTY;
                 case "GETELEMENTSUPER":
                     // Get the value of an element from a property that is on the stack,\
                     // as if using "[]" notation. Could be a String, number, or Symbol
-                    return RhinoOperation.GETELEMENTSUPER.withNamespace(StandardNamespace.PROPERTY);
+                    return GETELEMENTSUPER_PROPERTY;
                 case "GETINDEX":
                     // Same but the value is definitely a numeric index
-                    return RhinoOperation.GETINDEX.withNamespace(StandardNamespace.PROPERTY);
+                    return GETINDEX_PROPERTY;
                 case "SET":
                     // Set an object property with a constant name
-                    return StandardOperation.SET
-                            .withNamespace(StandardNamespace.PROPERTY)
-                            .named(getNameSegment(tokens, name, 2));
+                    return SET_PROPERTY.named(getNameSegment(tokens, name, 2));
                 case "SETSUPER":
                     // Set an object property in super with a constant name
-                    return RhinoOperation.SETSUPER
-                            .withNamespace(StandardNamespace.PROPERTY)
-                            .named(getNameSegment(tokens, name, 2));
+                    return SETSUPER_PROPERTY.named(getNameSegment(tokens, name, 2));
                 case "SETELEMENT":
                     // Set an object property as if by "[]", with a property on the stack
-                    return RhinoOperation.SETELEMENT.withNamespace(StandardNamespace.PROPERTY);
+                    return SETELEMENT_PROPERTY;
                 case "SETELEMENTSUPER":
                     // Set an object property in super as if by "[]", with a property on the stack
-                    return RhinoOperation.SETELEMENTSUPER.withNamespace(StandardNamespace.PROPERTY);
+                    return SETELEMENTSUPER_PROPERTY;
                 case "SETINDEX":
                     // Same but the property name is definitely a number
-                    return RhinoOperation.SETINDEX.withNamespace(StandardNamespace.PROPERTY);
+                    return SETINDEX_PROPERTY;
             }
         } else if ("NAME".equals(namespaceName)) {
             switch (opName) {
                 case "BIND":
                     // Bind a new variable to the context with a constant name
-                    return RhinoOperation.BIND
-                            .withNamespace(RhinoNamespace.NAME)
-                            .named(getNameSegment(tokens, name, 2));
+                    return BIND_NAME.named(getNameSegment(tokens, name, 2));
                 case "GET":
                     // Get a variable from the context with a constant name
-                    return StandardOperation.GET
-                            .withNamespace(RhinoNamespace.NAME)
-                            .named(getNameSegment(tokens, name, 2));
+                    return GET_NAME.named(getNameSegment(tokens, name, 2));
                 case "GETWITHTHIS":
                     // Same but also return "this" so that it is found by "lastStoredScriptable"
-                    return RhinoOperation.GETWITHTHIS
-                            .withNamespace(RhinoNamespace.NAME)
-                            .named(getNameSegment(tokens, name, 2));
+                    return GETWITHTHIS_NAME.named(getNameSegment(tokens, name, 2));
                 case "GETWITHTHISOPTIONAL":
                     // Similar to the above, but won't complain if prop is not found
-                    return RhinoOperation.GETWITHTHISOPTIONAL
-                            .withNamespace(RhinoNamespace.NAME)
-                            .named(getNameSegment(tokens, name, 2));
+                    return GETWITHTHISOPTIONAL_NAME.named(getNameSegment(tokens, name, 2));
                 case "SET":
                     // Set an object in the context with a constant name
-                    return StandardOperation.SET
-                            .withNamespace(RhinoNamespace.NAME)
-                            .named(getNameSegment(tokens, name, 2));
+                    return SET_NAME.named(getNameSegment(tokens, name, 2));
                 case "SETSTRICT":
                     // Same but implement strict mode checks
-                    return RhinoOperation.SETSTRICT
-                            .withNamespace(RhinoNamespace.NAME)
-                            .named(getNameSegment(tokens, name, 2));
+                    return SETSTRICT_NAME.named(getNameSegment(tokens, name, 2));
                 case "SETCONST":
                     // Same but try to set a constant
-                    return RhinoOperation.SETCONST
-                            .withNamespace(RhinoNamespace.NAME)
-                            .named(getNameSegment(tokens, name, 2));
+                    return SETCONST_NAME.named(getNameSegment(tokens, name, 2));
             }
 
         } else if ("MATH".equals(namespaceName)) {
             switch (opName) {
                 case "ADD":
-                    return RhinoOperation.ADD.withNamespace(RhinoNamespace.MATH);
+                    return ADD_MATH;
                 case "TOBOOLEAN":
-                    return RhinoOperation.TOBOOLEAN.withNamespace(RhinoNamespace.MATH);
+                    return TOBOOLEAN_MATH;
                 case "TOINT32":
-                    return RhinoOperation.TOINT32.withNamespace(RhinoNamespace.MATH);
+                    return TOINT32_MATH;
                 case "TOUINT32":
-                    return RhinoOperation.TOUINT32.withNamespace(RhinoNamespace.MATH);
+                    return TOUINT32_MATH;
                 case "EQ":
-                    return RhinoOperation.EQ.withNamespace(RhinoNamespace.MATH);
+                    return EQ_MATH;
                 case "SHALLOWEQ":
-                    return RhinoOperation.SHALLOWEQ.withNamespace(RhinoNamespace.MATH);
+                    return SHALLOWEQ_MATH;
                 case "TONUMBER":
-                    return RhinoOperation.TONUMBER.withNamespace(RhinoNamespace.MATH);
+                    return TONUMBER_MATH;
                 case "TONUMERIC":
-                    return RhinoOperation.TONUMERIC.withNamespace(RhinoNamespace.MATH);
+                    return TONUMERIC_MATH;
                 case "COMPAREGT":
-                    return RhinoOperation.COMPARE_GT.withNamespace(RhinoNamespace.MATH);
+                    return COMPAREGT_MATH;
                 case "COMPARELT":
-                    return RhinoOperation.COMPARE_LT.withNamespace(RhinoNamespace.MATH);
+                    return COMPARELT_MATH;
                 case "COMPAREGE":
-                    return RhinoOperation.COMPARE_GE.withNamespace(RhinoNamespace.MATH);
+                    return COMPAREGE_MATH;
                 case "COMPARELE":
-                    return RhinoOperation.COMPARE_LE.withNamespace(RhinoNamespace.MATH);
+                    return COMPARELE_MATH;
             }
         }
 
