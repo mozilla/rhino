ee9a950c08199a45ea928a0baee4a00a05a68a39
ZZZank <47418975+ZZZank@users.noreply.github.com>
2026-07-28 16:28:29 +0800

    move modular java check to ReflectUtils

--- rhino/src/main/java/org/mozilla/javascript/lc/ReflectUtils.java
+++ rhino/src/main/java/org/mozilla/javascript/lc/ReflectUtils.java
@@ -9,6 +9,22 @@ import org.mozilla.javascript.lc.type.TypeInfo;
  */
 public abstract class ReflectUtils {
 
+    /**
+     * {@code true} if we are on a "modular" version of Java (Java 11 or up, excluding Android). It does not use the SourceVersion class because this is not present on Android.
+     */
+    public static final boolean IS_MODULAR_JAVA;
+
+    static {
+        boolean isModularJava;
+        try {
+            Class.class.getMethod("getModule");
+            isModularJava = true;
+        } catch (NoSuchMethodException e) {
+            isModularJava = false;
+        }
+        IS_MODULAR_JAVA = isModularJava;
+    }
+
     public static String javaSignature(Class<?> type) {
         int arrayDimension = 0;
         while (type.isArray()) {
